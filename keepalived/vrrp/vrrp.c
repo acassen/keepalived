70b30949580e9bce78831d8a1e48ee458bfdcb41
Vincent Bernat <bernat@luffy.cx>
2009-10-06 13:38:03 +0000

    Keep previous effective VRRP priority on reload.
    When using weighted scripts or interfaces, on reload, the VRRP
    priority is resetted to the default priority for the first packets to
    be sent, until vrrp_update_priority() thread is called.

--- keepalived/vrrp/vrrp.c
+++ keepalived/vrrp/vrrp.c
@@ -1108,6 +1108,7 @@ reset_vrrp_state(vrrp_rt * old_vrrp)
 	vrrp->state = old_vrrp->state;
 	vrrp->init_state = old_vrrp->state;
 	vrrp->wantstate = old_vrrp->state;
+	vrrp->effective_priority = old_vrrp->effective_priority;
 	memcpy(vrrp->ipsecah_counter, old_vrrp->ipsecah_counter, sizeof(seq_counter));
 
 #ifdef _HAVE_IPVS_SYNCD_
