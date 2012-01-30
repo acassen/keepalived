a747cf3f8d60a0a8e0b2fbd8c2e87ae16ca1df63
Vincent Bernat <bernat@luffy.cx>
2012-01-30 14:51:28 +0100

    Fix priority not changing on reload
    In 1.2.2 if a user changes the priority assigned to a vrrp-group and
    reloads the configuration, the old priority will persist. This is
    caused by saving the effective_priority in reset_vrrp_state. While
    this makes sense for the case where a weighted interface or script is
    used, it prevents to change the priority when sync groups are used (no
    thread with vrrp_update_priority() is running in this case). Since
    sync groups do not allow the use of weighted interfaces or scripts,
    the effective priority is not copied over when they are used.
    
    Original patch credit goes to John Southworth (Vyatta).

--- keepalived/vrrp/vrrp.c
+++ keepalived/vrrp/vrrp.c
@@ -1201,7 +1201,8 @@ reset_vrrp_state(vrrp_rt * old_vrrp)
 	vrrp->state = old_vrrp->state;
 	vrrp->init_state = old_vrrp->state;
 	vrrp->wantstate = old_vrrp->state;
-	vrrp->effective_priority = old_vrrp->effective_priority;
+	if (!old_vrrp->sync)
+		vrrp->effective_priority = old_vrrp->effective_priority;
 	memcpy(vrrp->ipsecah_counter, old_vrrp->ipsecah_counter, sizeof(seq_counter));
 
 #ifdef _HAVE_IPVS_SYNCD_
