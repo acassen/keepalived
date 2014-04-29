ffae3aab3665512b7340ff7a17f5073aadf7f901
Jonas Johansson <jonasj76@gmail.com>
2014-04-29 13:38:45 +0200

    vrrp: Fix initial interface status flag value for VMAC interface
    In commit a05a503, "vrrp: Fix sync of interface status flag when using VMAC
    interface", no inital value for the VMAC interface status flag was set. Due
    to that the VMAC interface flags shall follow the base interface, the base
    interface status flags value shall be copied to the VMAC interface status
    flags after the VMAC interface has been created.
    
    Signed-off-by: Jonas Johansson <jonasj76@gmail.com>

--- keepalived/vrrp/vrrp_vmac.c
+++ keepalived/vrrp/vrrp_vmac.c
@@ -195,6 +195,7 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 	if (!ifp)
 		return -1;
 	base_ifindex = vrrp->ifp->ifindex;
+	ifp->flags = vrrp->ifp->flags; /* Copy base interface flags */
 	vrrp->ifp = ifp;
 	vrrp->ifp->base_ifindex = base_ifindex;
 	vrrp->ifp->vmac = 1;
