333c850f898c86f4f14942de1e4535ee27af5dcb
Quentin Armitage <quentin@armitage.org.uk>
2016-01-26 13:30:41 +0000

    Make functions always returning 0 void
    Three functions in utils.c always returned 0, and the calling functions
    weren't checking the return code, since it was pointless, so the functions
    have been changed to be of type void.
    
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- lib/utils.c
+++ lib/utils.c
@@ -231,31 +231,28 @@ inet_stosockaddr(char *ip, char *port, struct sockaddr_storage *addr)
 }
 
 /* IPv4 to sockaddr_storage */
-int
+void
 inet_ip4tosockaddr(struct in_addr *sin_addr, struct sockaddr_storage *addr)
 {
 	struct sockaddr_in *addr4 = (struct sockaddr_in *) addr;
 	addr4->sin_family = AF_INET;
 	addr4->sin_addr = *sin_addr;
-	return 0;
 }
 
 /* IPv6 to sockaddr_storage */
-int
+void
 inet_ip6tosockaddr(struct in6_addr *sin_addr, struct sockaddr_storage *addr)
 {
 	struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) addr;
 	addr6->sin6_family = AF_INET6;
 	addr6->sin6_addr = *sin_addr;
-	return 0;
 }
 
-int
+void
 inet_ip6scopeid(uint32_t scope_id, struct sockaddr_storage *addr)
 {
 	struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) addr;
 	addr6->sin6_scope_id = scope_id;
-	return 0;
 }
 
 /* IP network to string representation */
