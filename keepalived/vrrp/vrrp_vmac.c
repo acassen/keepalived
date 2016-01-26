41426ecd233afee6403ea5211c77545b56f72037
Quentin Armitage <quentin@armitage.org.uk>
2016-01-26 12:12:43 +0000

    Stop having an IPv6 link-local address added based on VMAC mac address
    IPv6 link-local addresses that were based on the virtual MAC address of
    the VMAC interface were being added. RFC5798 para 7.4 states that this
    is not permitted. It also causes duplicate address detection failure,
    since each instance of the virtual router was configuring the same
    IPv6 address on the same subnet.
    
    This commit stops the offending link-local address being addied (or removes
    it if it can't stop it being added), and since VRRP advertisements must
    be sent with the virtual MAC address, but a link-local address for the
    interface, if a link-local address from the underlying interface exists,
    it is added to the VMAC interface, otherwise the MAC address of the
    underlying interface is used to generate a link-local address, which is
    then added.
    
    It wasn't until Linux 3.17 that the IFLA_INET6_ADDR_GEN_MODE netlink
    message was added, via which one can stop a link-local address being
    automatically configured. Therefore, if IFLA_INET6_ADDR_GEN_MODE is not
    supported, the only way to ensure that the problematic link-local
    address is not added is to remove it after the interface is brought up.
    This is not ideal, since there is a small window when the "illegal",
    and possibly duplicate, link-local address exists, but I haven't
    found any other way of doing it for pre 3.17 kernels.
    
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_vmac.c
+++ keepalived/vrrp/vrrp_vmac.c
@@ -22,6 +22,9 @@
 
 /* global include */
 //#include <limits.h>
+#ifdef _HAVE_ADDR_GEN_MODE_
+#include <linux/if_link.h>
+#endif
 
 /* local include */
 #include "vrrp_vmac.h"
@@ -35,6 +38,23 @@
 #ifdef _HAVE_VRRP_VMAC_
 /* private matter */
 static const char *ll_kind = "macvlan";
+
+static void
+make_link_local_address(struct in6_addr* l3_addr, const u_char* ll_addr)
+{
+	l3_addr->s6_addr[0] = 0xfe;
+	l3_addr->s6_addr[1] = 0x80;
+	l3_addr->s6_addr16[1] = 0;
+	l3_addr->s6_addr32[1] = 0;
+	l3_addr->s6_addr[8] = ll_addr[0] | 0x02;
+	l3_addr->s6_addr[9] = ll_addr[1];
+	l3_addr->s6_addr[10] = ll_addr[2];
+	l3_addr->s6_addr[11] = 0xff;
+	l3_addr->s6_addr[12] = 0xfe;
+	l3_addr->s6_addr[13] = ll_addr[3];
+	l3_addr->s6_addr[14] = ll_addr[4];
+	l3_addr->s6_addr[15] = ll_addr[5];
+}
 
 static int
 netlink_link_up(vrrp_t *vrrp)
@@ -175,7 +195,6 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 
 	/*
 	 * Update interface queue and vrrp instance interface binding.
-	 * bring it UP !
 	 */
 	netlink_interface_lookup();
 	ifp = if_get_by_ifname(ifname);
@@ -188,8 +207,6 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 	vrrp->ifp->base_ifindex = base_ifindex;
 	vrrp->ifp->vmac = 1;
 	vrrp->vmac_ifindex = IF_INDEX(vrrp->ifp); /* For use on delete */
-	__set_bit(VRRP_VMAC_UP_BIT, &vrrp->vmac_flags);
-	netlink_link_up(vrrp);
 
 	if (vrrp->family == AF_INET) {
 		/* Set the necessary kernel parameters to make macvlans work for us */
@@ -201,28 +218,88 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 			link_disable_ipv6(ifp);
 	}
 	else {
-		// Add link-local address from underlying interface to vmac if there is one
-		if (base_ifp->sin6_addr.s6_addr32[0]) {
-			struct sockaddr_storage *saddr = &vrrp->saddr;
-			ip_address_t ipaddress;
+		// We don't want a link-local address auto assigned - see RFC5798 paragraph 7.4.
+		// If we have a sufficiently recent kernel, we can stop a link local address
+		// based on the MAC address being automatically assigned. If not, then we have
+		// to delete the generated address after bringing the interface up (see below).
+#ifdef _HAVE_ADDR_GEN_MODE_		// Introduced in Linux 3.17
+		memset(&req, 0, sizeof (req));
+		req.n.nlmsg_len = NLMSG_LENGTH(sizeof (struct ifinfomsg));
+		req.n.nlmsg_flags = NLM_F_REQUEST ;
+		req.n.nlmsg_type = RTM_NEWLINK;
+		req.ifi.ifi_family = AF_UNSPEC;
+		req.ifi.ifi_index = vrrp->vmac_ifindex;
 
-			memset(&ipaddress, 0, sizeof(ipaddress));
+		u_char val = IN6_ADDR_GEN_MODE_NONE;
+		struct rtattr* spec;
 
-			ipaddress.ifp = ifp;
+		spec = NLMSG_TAIL(&req.n);
+		addattr_l(&req.n, sizeof(req), IFLA_AF_SPEC, NULL,0);
+		data = NLMSG_TAIL(&req.n);
+		addattr_l(&req.n, sizeof(req), AF_INET6, NULL,0);
+		addattr_l(&req.n, sizeof(req), IFLA_INET6_ADDR_GEN_MODE, &val, sizeof(val));
+		data->rta_len = (void *)NLMSG_TAIL(&req.n) - (void *)data;
+		spec->rta_len = (void *)NLMSG_TAIL(&req.n) - (void *)spec;
+
+		if (netlink_talk(&nl_cmd, &req.n) < 0)
+			log_message(LOG_INFO, "vmac: Error setting ADDR_GEN_MODE to NONE");
+#endif
+
+		/* Add link-local address from underlying interface to vmac if there is one,
+		 * otherwise construct a link-local address based on underlying interface's
+		 * MAC address.
+		 * This is so that VRRP advertisements will be sent from a non-VIP address, but
+		 * using the VRRP MAC address */
+		struct sockaddr_storage *saddr = &vrrp->saddr;
+		ip_address_t ipaddress;
+
+		memset(&ipaddress, 0, sizeof(ipaddress));
+
+		ipaddress.ifp = ifp;
+		if (base_ifp->sin6_addr.s6_addr32[0])
 			memcpy(&ipaddress.u.sin6_addr, &base_ifp->sin6_addr, sizeof(ipaddress.u.sin6_addr));
-			ipaddress.ifa.ifa_family = AF_INET6;
-			ipaddress.ifa.ifa_prefixlen = 64;
-			ipaddress.ifa.ifa_index = vrrp->vmac_ifindex;
+		else
+			make_link_local_address(&ipaddress.u.sin6_addr, base_ifp->hw_addr);
+		ipaddress.ifa.ifa_family = AF_INET6;
+		ipaddress.ifa.ifa_prefixlen = 64;
+		ipaddress.ifa.ifa_index = vrrp->vmac_ifindex;
 
-			if (netlink_ipaddress(&ipaddress, IPADDRESS_ADD) != 1)
-				log_message(LOG_INFO, "Adding link-local address to vmac failed");
+		if (netlink_ipaddress(&ipaddress, IPADDRESS_ADD) != 1)
+			log_message(LOG_INFO, "Adding link-local address to vmac failed");
 
-			// Save the address as source for vrrp packets
-			inet_ip6tosockaddr(&base_ifp->sin6_addr, saddr);
-			inet_ip6scopeid(IF_INDEX(ifp), saddr);
-		}
+		// Save the address as source for vrrp packets
+		inet_ip6tosockaddr(&base_ifp->sin6_addr, saddr);
+		inet_ip6scopeid(IF_INDEX(ifp), saddr);
+	}
+
+	/* bring it UP ! */
+	__set_bit(VRRP_VMAC_UP_BIT, &vrrp->vmac_flags);
+	netlink_link_up(vrrp);
+
+#ifndef _HAVE_ADDR_GEN_MODE_
+	if(vrrp->family == AF_INET6 ) {
+		/* Delete the automatically created link-local address based on the
+		 * MAC address if we weren't able to configure the interface not to
+		 * create the address (see above).
+		 * This isn't ideal, since the invalid address will exist momentarily,
+		 * but is there any better way to do it? */
+		ip_address_t ipaddress;
+
+		memset(&ipaddress, 0, sizeof(ipaddress));
+
+		ipaddress.ifp = ifp;
+		memcpy(&ipaddress.u.sin6_addr, &base_ifp->sin6_addr, sizeof(ipaddress.u.sin6_addr));
+		make_link_local_address(&ipaddress.u.sin6_addr, ll_addr);
+		ipaddress.ifa.ifa_family = AF_INET6;
+		ipaddress.ifa.ifa_prefixlen = 64;
+		ipaddress.ifa.ifa_index = vrrp->vmac_ifindex;
+
+		if (netlink_ipaddress(&ipaddress, IPADDRESS_DEL) != 1)
+			log_message(LOG_INFO, "Deleting auto link-local address from vmac failed");
 	}
 #endif
+#endif
+
 	return 1;
 }
 
