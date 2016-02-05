acassen/keepalived

commit d3463c27770a9bbbb909fd7a41a3e8f18d403f34

Add ability to use libiptc rather than invoking ip(6)tables
Invoking ip(6)tables has a high overhead, since the process has to be
forked and exec'd, and then it has to read the whole ip(6)tables
filter chain before it makes a single update and commits it back.

Using libiptc avoids the overhead of multiple forks/execs, and also
means that multiple entries can be added/deleted to/from the ip(6)tables
configuration in a single update.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/include/vrrp_iptables.h
@@ -1,0 +1,42 @@
+/*
+ * Soft:        Keepalived is a failover program for the LVS project
+ *              <www.linuxvirtualserver.org>. It monitor & manipulate
+ *              a loadbalanced server pool using multi-layer checks.
+ *
+ * Part:        vrrp_iptables.c include file.
+ *
+ * Author:      Quentin Armitage, <quentin@armitage.org.uk>
+ *
+ *              This program is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *              See the GNU General Public License for more details.
+ *
+ *              This program is free software; you can redistribute it and/or
+ *              modify it under the terms of the GNU General Public License
+ *              as published by the Free Software Foundation; either version
+ *              2 of the License, or (at your option) any later version.
+ *
+ * Copyright (C) 2001-2016 Alexandre Cassen, <acassen@gmail.com>
+ */
+
+#ifndef _VRRP_IPTABLES_H
+#define _VRRP_IPTABLES_H
+
+struct ipt_handle;
+
+#ifdef _HAVE_LIBIPTC_
+
+#include <libiptc/libxtc.h>
+
+#include "vrrp_ipaddress.h"
+
+#define	IPTABLES_MAX_TRIES	3	/* How may times to try adding/deleting when get EAGAIN */
+
+
+struct ipt_handle *iptables_open(void);
+int iptables_close(struct ipt_handle *h);
+int iptables_entry(struct ipt_handle* h, const char* chain_name, int rulenum, const char* target_name, const ip_address_t* src_ip_address, const ip_address_t* dst_ip_address, const char* in_iface, const char* out_iface, uint16_t protocol, uint16_t type, int cmd);
+#endif
+
+#endif

configure
@@ -636,6 +636,7 @@ ADDR_GEN_MODE
 VRRP_VMAC
 IPVS_SYNCD
 KERN
+USE_LIBIPTC
 USE_NL3
 EGREP
 GREP
@@ -701,6 +702,7 @@ enable_fwmark
 enable_snmp
 enable_sha1
 enable_vrrp_auth
+enable_libiptc
 enable_debug
 enable_profile
 '
@@ -1327,6 +1329,7 @@ Optional Features:
   --enable-snmp           compile with SNMP support
   --enable-sha1           compile with SHA1 support
   --disable-vrrp-auth     compile without VRRP authentication
+  --disable-libiptc       compile without libiptc
   --enable-debug          compile with debugging flags
   --enable-profile        compile with profiling flags
 
@@ -3284,6 +3287,11 @@ if test "${enable_vrrp_auth+set}" = set; then :
   enableval=$enable_vrrp_auth;
 fi
 
+# Check whether --enable-libiptc was given.
+if test "${enable_libiptc+set}" = set; then :
+  enableval=$enable_libiptc;
+fi
+
 # Check whether --enable-debug was given.
 if test "${enable_debug+set}" = set; then :
   enableval=$enable_debug;
@@ -4120,6 +4128,58 @@ fi
 
 fi
 
+
+
+
+USE_LIBIPTC=_WITHOUT_LIBIPTC_
+if test "${enable_libiptc}" != "no"; then
+  SAV_LIBS=$LIBS
+  LIBS="$LIBS $(pkg-config --libs --static libiptc)"
+  { $as_echo "$as_me:${as_lineno-$LINENO}: checking for iptc_init in -liptc" >&5
+$as_echo_n "checking for iptc_init in -liptc... " >&6; }
+if ${ac_cv_lib_iptc_iptc_init+:} false; then :
+  $as_echo_n "(cached) " >&6
+else
+  ac_check_lib_save_LIBS=$LIBS
+LIBS="-liptc  $LIBS"
+cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+char iptc_init ();
+int
+main ()
+{
+return iptc_init ();
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_link "$LINENO"; then :
+  ac_cv_lib_iptc_iptc_init=yes
+else
+  ac_cv_lib_iptc_iptc_init=no
+fi
+rm -f core conftest.err conftest.$ac_objext \
+    conftest$ac_exeext conftest.$ac_ext
+LIBS=$ac_check_lib_save_LIBS
+fi
+{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_cv_lib_iptc_iptc_init" >&5
+$as_echo "$ac_cv_lib_iptc_iptc_init" >&6; }
+if test "x$ac_cv_lib_iptc_iptc_init" = xyes; then :
+
+      USE_LIBIPTC=_HAVE_LIBIPTC_
+      SAV_LIBS=$LIBS
+
+fi
+
+  LIBS=$SAV_LIBS
+fi
 
 
 
@@ -6162,4 +6222,9 @@ elif test "${USE_NL3}" = "_HAVE_LIBNL1_"; then
   echo "libnl version            : 1"
 else
   echo "libnl version            : None"
+fi
+if test "${USE_LIBIPTC}" = "_HAVE_LIBIPTC_"; then
+  echo "Use libiptc              : Yes"
+else
+  echo "Use libiptc              : No"
 fi

configure.in
@@ -39,6 +39,8 @@ AC_ARG_ENABLE(sha1,
   [  --enable-sha1           compile with SHA1 support])
 AC_ARG_ENABLE(vrrp-auth,
   [  --disable-vrrp-auth     compile without VRRP authentication])
+AC_ARG_ENABLE(libiptc,
+  [  --disable-libiptc       compile without libiptc])
 AC_ARG_ENABLE(debug,
   [  --enable-debug          compile with debugging flags])
 AC_ARG_ENABLE(profile,
@@ -98,6 +100,22 @@ AC_CHECK_LIB(nl-3, nl_socket_alloc,
   ])
 
 AC_SUBST(USE_NL3)
+
+dnl ----[Check for iptables libraries]----
+USE_LIBIPTC=_WITHOUT_LIBIPTC_
+if test "${enable_libiptc}" != "no"; then
+  SAV_LIBS=$LIBS
+  LIBS="$LIBS $(pkg-config --libs --static libiptc)"
+  AC_CHECK_LIB(iptc, iptc_init,
+    [
+      USE_LIBIPTC=_HAVE_LIBIPTC_
+      SAV_LIBS=$LIBS
+    ],
+    [])
+  LIBS=$SAV_LIBS
+fi
+
+AC_SUBST(USE_LIBIPTC)
 
 dnl ----[ Kernel version check ]----
 CPPFLAGS="$CPPFLAGS -I$kernelinc"
@@ -457,5 +475,10 @@ elif test "${USE_NL3}" = "_HAVE_LIBNL1_"; then
   echo "libnl version            : 1"
 else
   echo "libnl version            : None"
+fi
+if test "${USE_LIBIPTC}" = "_HAVE_LIBIPTC_"; then
+  echo "Use libiptc              : Yes"
+else
+  echo "Use libiptc              : No"
 fi
 dnl ----[ end configure ]---

keepalived/include/ipvswrapper.h
@@ -31,7 +31,7 @@
 #include <arpa/inet.h>
 #include <asm/types.h>
 
-#include <linux/if.h>
+#include <net/if.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/udp.h>
 #include <netinet/tcp.h>

keepalived/include/vrrp_if.h
@@ -25,7 +25,7 @@
 
 /* global includes */
 #include <sys/socket.h>
-#include <linux/if.h>
+#include <net/if.h>
 #include <netinet/in.h>
 
 /* needed to get correct values for SIOC* */

keepalived/include/vrrp_ipaddress.h
@@ -88,16 +88,18 @@ typedef struct _ip_address {
 
 #define IP_ISEQ(X,Y)    (((X) && (Y)) ? ((IP_FAMILY(X) == IP_FAMILY(Y)) ? (IP_IS6(X) ? IP6_ISEQ(X, Y) : IP4_ISEQ(X, Y)) : 0) : (((!(X) && (Y))||((X) && !(Y))) ? 0 : 1))
 
+struct ipt_handle;	// AAGH - TODO
+
 /* prototypes */
 extern int netlink_ipaddress(ip_address_t *, int);
 extern void netlink_iplist(list, int);
-extern void handle_iptable_rule_to_iplist(list, int, char *);
+extern void handle_iptable_rule_to_iplist(struct ipt_handle *, list, int, char *);
 extern void free_ipaddress(void *);
 extern char *ipaddresstos(ip_address_t *);
 extern void dump_ipaddress(void *);
 extern ip_address_t *parse_ipaddress(ip_address_t *, char *, int);
 extern void alloc_ipaddress(list, vector_t *, interface_t *);
-extern void clear_diff_address(list, list);
+extern void clear_diff_address(struct ipt_handle *, list, list);
 extern void clear_diff_saddresses(void);
 
 #endif

keepalived/vrrp/Makefile.in
@@ -7,11 +7,12 @@
 CC	 = @CC@
 SNMP_FLAG = @SNMP_SUPPORT@
 VRRP_AUTH_FLAG = @VRRP_AUTH_SUPPORT@
+LIBIPTC_FLAG = @USE_LIBIPTC@
 NL3_FLAG = @USE_NL3@
 INCLUDES = -I../include -I../../lib
 CFLAGS	 = $(INCLUDES) @CFLAGS@ @CPPFLAGS@ \
 	   -Wall -Wunused -Wstrict-prototypes
-DEFS	 = -D@KERN@ -D@IPVS_SUPPORT@ -D@IPVS_SYNCD@ -D@VRRP_VMAC@ -D@ADDR_GEN_MODE@ -D@SNMP_SUPPORT@ -D@USE_NL3@ @DFLAGS@ -D@VRRP_AUTH_SUPPORT@
+DEFS	 = -D@KERN@ -D@IPVS_SUPPORT@ -D@IPVS_SYNCD@ -D@VRRP_VMAC@ -D@ADDR_GEN_MODE@ -D@SNMP_SUPPORT@ -D@USE_NL3@ @DFLAGS@ -D@VRRP_AUTH_SUPPORT@ -D@USE_LIBIPTC@
 COMPILE	 = $(CC) $(CFLAGS) $(DEFS)
 
 OBJS = 	vrrp_daemon.o vrrp_print.o vrrp_data.o vrrp_parser.o \
@@ -22,6 +23,10 @@ OBJS = 	vrrp_daemon.o vrrp_print.o vrrp_data.o vrrp_parser.o \
 
 ifeq ($(VRRP_AUTH_FLAG),_WITH_VRRP_AUTH_)
   OBJS += vrrp_ipsecah.o
+endif
+
+ifeq ($(LIBIPTC_FLAG),_HAVE_LIBIPTC_)
+  OBJS += vrrp_iptables.o
 endif
 
 ifeq ($(SNMP_FLAG),_WITH_SNMP_)
@@ -82,7 +87,7 @@ vrrp_if.o: vrrp_if.c ../include/vrrp_if.h ../include/vrrp_netlink.h \
   ../../lib/utils.h
 vrrp_ipaddress.o: vrrp_ipaddress.c ../include/vrrp_ipaddress.h ../include/vrrp_netlink.h \
   ../include/vrrp_if.h  ../include/vrrp_data.h ../../lib/memory.h ../../lib/utils.h \
-  ../../lib/bitops.h
+  ../../lib/bitops.h ../include/vrrp_iptables.h
 vrrp_iproute.o: vrrp_iproute.c ../include/vrrp_iproute.h ../include/vrrp_netlink.h \
   ../include/vrrp_if.h  ../include/vrrp_data.h ../../lib/memory.h ../../lib/utils.h
 vrrp_iprule.o: vrrp_iprule.c ../include/vrrp_iprule.h ../include/vrrp_netlink.h \
@@ -98,3 +103,4 @@ vrrp_snmp.o: vrrp_snmp.c ../include/vrrp_snmp.h ../include/vrrp_track.h \
   ../include/vrrp_iprule.h ../include/vrrp.h ../../lib/vector.h ../../lib/list.h ../include/snmp.h \
   ../include/global_data.h ../../lib/logger.h
 vrrp_if_config.o: vrrp_if_config.c ../include/vrrp_if_config.h ../../lib/logger.h
+vrrp_iptables.o: vrrp_iptables.c ../include/vrrp_iptables.h ../include/vrrp_ipaddress.h

keepalived/vrrp/vrrp.c
@@ -39,6 +39,9 @@
 #include "vrrp_index.h"
 #include "vrrp_vmac.h"
 #include "vrrp_if_config.h"
+#ifdef _HAVE_LIBIPTC_
+#include "vrrp_iptables.h"
+#endif
 #ifdef _WITH_SNMP_
 #include "vrrp_snmp.h"
 #endif
@@ -91,18 +94,35 @@ vrrp_handle_iprules(vrrp_t * vrrp, int cmd)
 
 /* add/remove iptable drop rules based on accept mode */
 static void
-vrrp_handle_accept_mode(vrrp_t *vrrp, int cmd, int type)
+vrrp_handle_accept_mode(vrrp_t *vrrp, int cmd)
 {
+#ifdef _HAVE_LIBIPTC_
+	int tries = 0;
+	int res = 0;
+#endif
+	struct ipt_handle *h = NULL;
+
 	if ((vrrp->version == VRRP_VERSION_3) &&
 	    (vrrp->base_priority != VRRP_PRIO_OWNER) &&
 	    !vrrp->accept) {
 		if (debug & 32)
-			log_message(LOG_INFO, "VRRP_Instance(%s) %s protocol %s %s", vrrp->iname,
-				(cmd == IPADDRESS_ADD) ? "setting" : "removing", "iptable drop rule to", (type == VRRP_VIP_TYPE) ? "VIP" : "E-VIP");
+			log_message(LOG_INFO, "VRRP_Instance(%s) %s protocol %s", vrrp->iname,
+				(cmd == IPADDRESS_ADD) ? "setting" : "removing", "iptable drop rule");
 
-		/* As accept is false, add iptable rule to drop packets destinated to VIP */
-		handle_iptable_rule_to_iplist((type == VRRP_VIP_TYPE) ? vrrp->vip : vrrp->evip, cmd, IF_NAME(vrrp->ifp));
-		vrrp->iptable_rules_set = (cmd == IPADDRESS_ADD) ? true : false;
+#ifdef _HAVE_LIBIPTC_
+		do {
+			h = iptables_open();
+#endif
+			/* As accept is false, add iptable rule to drop packets destinated to VIPs and eVIPs */
+			if (!LIST_ISEMPTY(vrrp->vip))
+				handle_iptable_rule_to_iplist(h, vrrp->vip, cmd, IF_NAME(vrrp->ifp));
+			if (!LIST_ISEMPTY(vrrp->evip))
+				handle_iptable_rule_to_iplist(h, vrrp->evip, cmd, IF_NAME(vrrp->ifp));
+#ifdef _HAVE_LIBIPTC_
+			res = iptables_close(h);
+		} while (res == EAGAIN && ++tries < IPTABLES_MAX_TRIES);
+#endif
+		vrrp->iptable_rules_set = (cmd == IPADDRESS_ADD);
 	}
 }
 
@@ -1090,14 +1110,11 @@ vrrp_state_become_master(vrrp_t * vrrp)
 					vrrp->iname, (vrrp->adver_int * 1000) / TIMER_HZ);
 
 	/* add the ip addresses */
-	if (!LIST_ISEMPTY(vrrp->vip)) {
+	if (!LIST_ISEMPTY(vrrp->vip))
 		vrrp_handle_ipaddress(vrrp, IPADDRESS_ADD, VRRP_VIP_TYPE);
-		vrrp_handle_accept_mode(vrrp, IPADDRESS_ADD, VRRP_VIP_TYPE);
-	}
-	if (!LIST_ISEMPTY(vrrp->evip)) {
+	if (!LIST_ISEMPTY(vrrp->evip))
 		vrrp_handle_ipaddress(vrrp, IPADDRESS_ADD, VRRP_EVIP_TYPE);
-		vrrp_handle_accept_mode(vrrp, IPADDRESS_ADD, VRRP_EVIP_TYPE);
-	}
+	vrrp_handle_accept_mode(vrrp, IPADDRESS_ADD);
 	vrrp->vipset = 1;
 
 	/* add virtual routes */
@@ -1193,14 +1210,11 @@ vrrp_restore_interface(vrrp_t * vrrp, int advF)
 	 */
 	if (__test_bit(DONT_RELEASE_VRRP_BIT, &debug) || VRRP_VIP_ISSET(vrrp) ||
 	    __test_bit(RELEASE_VIPS_BIT, &debug)) {
-		if (!LIST_ISEMPTY(vrrp->vip)) {
+		if (!LIST_ISEMPTY(vrrp->vip))
 			vrrp_handle_ipaddress(vrrp, IPADDRESS_DEL, VRRP_VIP_TYPE);
-			vrrp_handle_accept_mode(vrrp, IPADDRESS_DEL, VRRP_VIP_TYPE);
-		}
-		if (!LIST_ISEMPTY(vrrp->evip)) {
+		if (!LIST_ISEMPTY(vrrp->evip))
 			vrrp_handle_ipaddress(vrrp, IPADDRESS_DEL, VRRP_EVIP_TYPE);
-			vrrp_handle_accept_mode(vrrp, IPADDRESS_DEL, VRRP_EVIP_TYPE);
-		}
+		vrrp_handle_accept_mode(vrrp, IPADDRESS_DEL);
 		vrrp->vipset = 0;
 	}
 
@@ -2043,22 +2057,47 @@ vrrp_exist(vrrp_t * old_vrrp)
 
 /* Clear VIP|EVIP not present into the new data */
 static void
-clear_diff_vrrp_vip(vrrp_t * old_vrrp, int type)
+clear_diff_vrrp_vip_list(vrrp_t *vrrp, struct ipt_handle* h, list l, list n)
 {
-	vrrp_t *vrrp = vrrp_exist(old_vrrp);
-	list l = (type == VRRP_VIP_TYPE) ? old_vrrp->vip : old_vrrp->evip;
-	list n = (type == VRRP_VIP_TYPE) ? vrrp->vip : vrrp->evip;
-	clear_diff_address(l, n);
+	clear_diff_address(h, l, n);
+
+	if (LIST_ISEMPTY(n))
+		return;
 
 	/* Clear iptable rule to VIP if needed. */
-	if (!LIST_ISEMPTY(n) && old_vrrp->iptable_rules_set) {
-		if ((vrrp->version == VRRP_VERSION_2) || vrrp->accept ||
-		    (vrrp->base_priority == VRRP_PRIO_OWNER)) {
-			handle_iptable_rule_to_iplist(n, IPADDRESS_DEL, IF_NAME(vrrp->ifp));
-			vrrp->iptable_rules_set = false;
-		} else
-			vrrp->iptable_rules_set = true;
-	}
+	if ((vrrp->version == VRRP_VERSION_2) || vrrp->accept ||
+	    (vrrp->base_priority == VRRP_PRIO_OWNER)) {
+		handle_iptable_rule_to_iplist(h, n, IPADDRESS_DEL, IF_NAME(vrrp->ifp));
+// TODO = is this really false.
+		vrrp->iptable_rules_set = false;
+	} else
+		vrrp->iptable_rules_set = true;
+}
+
+static void
+clear_diff_vrrp_vip(vrrp_t * old_vrrp, int type)
+{
+#ifdef _HAVE_LIBIPTC_
+	int tries = 0;
+	int res = 0;
+#endif
+	struct ipt_handle *h = NULL;
+
+	vrrp_t *vrrp = vrrp_exist(old_vrrp);
+
+	if (!old_vrrp->iptable_rules_set)
+		return;
+
+#ifdef _HAVE_LIBIPTC_
+	do {
+		h = iptables_open();
+#endif
+		clear_diff_vrrp_vip_list(vrrp, h, old_vrrp->vip, vrrp->vip);
+		clear_diff_vrrp_vip_list(vrrp, h, old_vrrp->evip, vrrp->evip);
+#ifdef _HAVE_LIBIPTC_
+		res = iptables_close(h);
+	} while (res == EAGAIN && ++tries < IPTABLES_MAX_TRIES);
+#endif
 }
 
 /* Clear virtual routes not present in the new data */
@@ -2109,14 +2148,11 @@ reset_vrrp_state(vrrp_t * old_vrrp)
 	/* Remember if we had vips up and add new ones if needed */
 	vrrp->vipset = old_vrrp->vipset;
 	if (vrrp->vipset) {
-		if (!LIST_ISEMPTY(vrrp->vip)) {
+		if (!LIST_ISEMPTY(vrrp->vip))
 			vrrp_handle_ipaddress(vrrp, IPADDRESS_ADD, VRRP_VIP_TYPE);
-			vrrp_handle_accept_mode(vrrp, IPADDRESS_ADD, VRRP_VIP_TYPE);
-		}
-		if (!LIST_ISEMPTY(vrrp->evip)) {
+		if (!LIST_ISEMPTY(vrrp->evip))
 			vrrp_handle_ipaddress(vrrp, IPADDRESS_ADD, VRRP_EVIP_TYPE);
-			vrrp_handle_accept_mode(vrrp, IPADDRESS_ADD, VRRP_EVIP_TYPE);
-		}
+		vrrp_handle_accept_mode(vrrp, IPADDRESS_ADD);
 		if (!LIST_ISEMPTY(vrrp->vroutes))
 			vrrp_handle_iproutes(vrrp, IPROUTE_ADD);
 		if (!LIST_ISEMPTY(vrrp->vrules))

commit a50da9b33b74d707678ab8f696ef4e0c0953179c

Fix build breakage when not using libiptc
commit d3463c27 ("Add ability to use libiptc rather than...") broke
build when libiptc is not used due to a misplaced include. Now it
builds fine when --disable-libiptc is specified.

keepalived/include/vrrp_iptables.h
@@ -25,9 +25,9 @@
 
 #ifdef _HAVE_LIBIPTC_
 int load_mod_xt_set(void);
+#include <libiptc/libxtc.h>
 #endif
 
-#include <libiptc/libxtc.h>
 
 #include "vrrp_ipaddress.h"
 