acassen/keepalived

commit 2761edb9b036d334761164c4235eab5b9d6e0014

sctp ad persistent engine support

keepalived/include/check_data.h
@@ -125,6 +125,7 @@ typedef struct _virtual_server {
 	int				ops;
 	char				sched[SCHED_MAX_LENGTH];
 	char				timeout_persistence[MAX_TIMEOUT_LENGTH];
+	char				pe_name[IP_VS_PENAME_MAXLEN];
 	unsigned			loadbalancing_kind;
 	uint32_t			granularity_persistence;
 	char				*virtualhost;

doc/keepalived.conf.SYNOPSIS
@@ -326,9 +326,10 @@ virtual_server group <STRING>      {	# VS group declaration
     lvs_sched rr|wrr|lc|wlc|lblc|sh|dh	# LVS scheduler used
     ops					# Apply One-Packet-Scheduling (only for UDP)
     lvs_method NAT|DR|TUN		# LVS method used
+    persistence_engine <STRING>	        # LVS persistence engine name
     persistence_timeout <INTEGER>	# LVS persistence timeout
     persistence_granularity <NETMASK>	# LVS granularity mask
-    protocol TCP			# Only TCP is implemented
+    protocol TCP|UDP|SCTP               # L4 protocol
     ha_suspend				# If VS IP address is not set, suspend
 					#  healthcheckers activity
     virtualhost <STRING>		# VirtualHost string to use for

keepalived/check/check_data.c
@@ -185,8 +185,12 @@ dump_vs(void *data)
 	if (vs->granularity_persistence)
 		log_message(LOG_INFO, "   persistence granularity = %s",
 		       inet_ntop2(vs->granularity_persistence));
-	log_message(LOG_INFO, "   protocol = %s",
-	       (vs->service_type == IPPROTO_TCP) ? "TCP" : "UDP");
+    if (vs->service_type == IPPROTO_TCP)
+	    log_message(LOG_INFO, "   protocol = TCP");
+    if (vs->service_type == IPPROTO_UDP)
+	    log_message(LOG_INFO, "   protocol = UDP");
+    if (vs->service_type == IPPROTO_SCTP)
+	    log_message(LOG_INFO, "   protocol = SCTP");
 	log_message(LOG_INFO, "   alpha is %s, omega is %s",
 		    vs->alpha ? "ON" : "OFF", vs->omega ? "ON" : "OFF");
 	log_message(LOG_INFO, "   quorum = %lu, hysteresis = %lu", vs->quorum, vs->hysteresis);

keepalived/check/check_parser.c
@@ -142,6 +142,16 @@ pto_handler(vector_t *strvec)
 	memcpy(vs->timeout_persistence, str, size);
 }
 static void
+pengine_handler(vector_t *strvec)
+{
+	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
+	char *str = vector_slot(strvec, 1);
+	int size = sizeof (vs->pe_name);
+
+	strncpy(vs->pe_name, str, size - 1);
+	vs->pe_name[size - 1] = '\0';
+}
+static void
 pgr_handler(vector_t *strvec)
 {
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
@@ -155,7 +165,12 @@ proto_handler(vector_t *strvec)
 {
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
 	char *str = vector_slot(strvec, 1);
-	vs->service_type = (!strcmp(str, "TCP")) ? IPPROTO_TCP : IPPROTO_UDP;
+    if (!strcmp(str, "TCP"))
+        vs->service_type = IPPROTO_TCP;
+    else if (!strcmp(str, "SCTP"))
+        vs->service_type = IPPROTO_SCTP;
+    else
+        vs->service_type = IPPROTO_UDP;
 }
 static void
 hasuspend_handler(vector_t *strvec)
@@ -315,6 +330,7 @@ init_check_keywords(bool active)
 	install_keyword("lvs_sched", &lbalgo_handler);
 	install_keyword("lb_kind", &lbkind_handler);
 	install_keyword("lvs_method", &lbkind_handler);
+	install_keyword("persistence_engine", &pengine_handler);
 	install_keyword("persistence_timeout", &pto_handler);
 	install_keyword("persistence_granularity", &pgr_handler);
 	install_keyword("protocol", &proto_handler);

keepalived/check/ipvswrapper.c
@@ -625,6 +625,8 @@ ipvs_set_rule(int cmd, virtual_server_t * vs, real_server_t * rs)
 		if (vs->granularity_persistence)
 			srule->netmask = vs->granularity_persistence;
 
+	strcpy(srule->pe_name, vs->pe_name);
+
 	/* SVR specific */
 	if (rs) {
 		if (cmd == IP_VS_SO_SET_ADDDEST || cmd == IP_VS_SO_SET_DELDEST ||

commit 4a1d064fbcbc0f9d107fa26200bdecb144853801

Fix building with --disable-lvs
Commit 2761edb9b caused building with --disable-lvs to fail.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/include/check_data.h
@@ -125,7 +125,9 @@ typedef struct _virtual_server {
 	int				ops;
 	char				sched[SCHED_MAX_LENGTH];
 	char				timeout_persistence[MAX_TIMEOUT_LENGTH];
+#ifdef _WITH_LVS_
 	char				pe_name[IP_VS_PENAME_MAXLEN];
+#endif
 	unsigned			loadbalancing_kind;
 	uint32_t			granularity_persistence;
 	char				*virtualhost;