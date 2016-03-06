acassen/keepalived

commit 9ae463e7fe7b94c7a5222191a94192f6008130e5

Generate unique default VMAC interface names
Since the virtual router ID can be duplicated both between IPv4 and IPv6, and
also between different interfaces, the approach of setting a default interface
name as vrrp.VRID could produce duplicate names.

This commit now attempts to use vrrp.VRID, but if that already exists, then it
will try vrrpN.VRID, where N starts from 1 and increases until an unused name
is found (for IPv6 it tries vrrp6.VRID before vrrp1.VRID).

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp.c
@@ -1600,7 +1600,10 @@ shutdown_vrrp_instances(void)
 static int
 vrrp_complete_instance(vrrp_t * vrrp)
 {
+	char ifname[IFNAMSIZ];
+	list l;
 	element e;
+	vrrp_t *vrrp_o;
 	ip_address_t *vip;
 
 	if (vrrp->accept) {
@@ -1653,8 +1656,42 @@ vrrp_complete_instance(vrrp_t * vrrp)
 	vrrp->master_adver_int = vrrp->adver_int;
 
 	/* Set a default interface name for the vmac if needed */
-	if (__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags) && !vrrp->vmac_ifname[0])
-		snprintf(vrrp->vmac_ifname, IFNAMSIZ, "vrrp.%d", vrrp->vrid);
+	if (__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags) && !vrrp->vmac_ifname[0]) {
+		/* The same vrid can be used for both IPv4 and IPv6, and also on multiple underlying
+		 * interfaces. */
+		int num=0;
+		snprintf(ifname, IFNAMSIZ, "vrrp.%d", vrrp->vrid);
+
+		while (true) {
+			l = vrrp_data->vrrp;
+			for (e = LIST_HEAD(l); e; ELEMENT_NEXT(e)) {
+				vrrp_o = ELEMENT_DATA(e);
+				if (!strcmp(vrrp_o->vmac_ifname, ifname))
+					break;
+			}
+
+			/* If there is no VMAC with the name and no existing
+			 * interface with the name, we can use it */
+			if (!e && !if_get_by_ifname(ifname))
+				break;
+
+			/* For IPv6 try vrrp6 as second attempt */
+			if (vrrp->family == AF_INET6) {
+				if (num == 0)
+					num = 6;
+				else if (num == 6)
+					num = 1;
+				else if (++num == 6)
+					num++;
+			}
+			else
+				num++;
+			snprintf(ifname, IFNAMSIZ, "vrrp%d.%d", num, vrrp->vrid);
+		}
+
+		/* We've found a unique name */
+		strncpy(vrrp->vmac_ifname, ifname, IFNAMSIZ);
+	}
 
 	/* Make sure we have an IP address as needed */
 	if (vrrp->saddr.ss_family == AF_UNSPEC) {

commit 65b41efdc8afb506d5a2b23744921341f0376102

Improve and fix finding vmacs left over from previous invocation
When netlink reports a new or existing interface, we can extract
information that allows us to determine if the interface is a macvlan,
and the type (e.g. private). We can then save that in the interface_t
structure, setting the vlan flag, and base ifindex.

When working out the interface name to use for VMAC instances, we can
then check the interfaces which are macvlans to see if any of them
match the vrrp instance in terms of mac address, underlying interface
and inet address family, and if so we can then reuse the macvlan interface.

Commit 9ae463e7f broke the finding of existing interfaces where the
configuration didn't specify the VMAC interface name, and simply created
a new interface. This commits now resolves that.

There is still an issue that if an interface was in MASTER mode when
keepalived terminated, when keepalived restarts it leaves the VIPs and
eVIPS on the interfaces, meaning that keepalived cannot receive VRRP
packets on the interface from the VRRP instance that has taken over, and
it also means that there are duplicate IP addresses on the network.
Another commit will resolve this issue.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/include/vrrp_if.h
@@ -120,6 +120,7 @@ typedef struct _tracked_if {
 extern interface_t *if_get_by_ifindex(const int);
 extern interface_t *base_if_get_by_ifindex(const int);
 extern interface_t *if_get_by_ifname(const char *);
+extern list get_if_list(void);
 extern void if_vmac_reflect_flags(const int, const unsigned long);
 extern int if_linkbeat(const interface_t *);
 extern int if_mii_probe(const char *);

keepalived/include/vrrp_vmac.h
@@ -42,6 +42,9 @@ enum vrrp_vmac_bits {
 	VRRP_VMAC_XMITBASE_BIT = 2,
 };
 
+extern const char * const macvlan_ll_kind;
+extern u_char ll_addr[ETH_ALEN];
+
 /* prototypes */
 extern int netlink_link_add_vmac(vrrp_t *);
 extern int netlink_link_del_vmac(vrrp_t *);

keepalived/vrrp/vrrp.c
@@ -1730,7 +1730,6 @@ static int
 vrrp_complete_instance(vrrp_t * vrrp)
 {
 	char ifname[IFNAMSIZ];
-	list l;
 	element e;
 	vrrp_t *vrrp_o;
 	ip_address_t *vip;
@@ -1738,6 +1737,7 @@ vrrp_complete_instance(vrrp_t * vrrp)
 	int max_addr;
 	int i;
 	element next;
+	interface_t *ifp;
 
 	if (vrrp->family == AF_INET6) {
 		if (vrrp->version == VRRP_VERSION_2 && vrrp->strict_mode) {
@@ -1876,41 +1876,73 @@ vrrp_complete_instance(vrrp_t * vrrp)
 	vrrp->master_adver_int = vrrp->adver_int;
 
 	/* Set a default interface name for the vmac if needed */
-	if (__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags) && !vrrp->vmac_ifname[0]) {
+	if (__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags)) {
 		/* The same vrid can be used for both IPv4 and IPv6, and also on multiple underlying
 		 * interfaces. */
-		int num=0;
-		snprintf(ifname, IFNAMSIZ, "vrrp.%d", vrrp->vrid);
 
-		while (true) {
-			l = vrrp_data->vrrp;
-			for (e = LIST_HEAD(l); e; ELEMENT_NEXT(e)) {
-				vrrp_o = ELEMENT_DATA(e);
-				if (!strcmp(vrrp_o->vmac_ifname, ifname))
+		/* Look to see if an existing interface matches. If so, use that name */
+		list if_list = get_if_list();
+		if (!LIST_ISEMPTY(if_list)) {		/* If the list were empty we would have a real problem! */
+			for (e = LIST_HEAD(if_list); e; ELEMENT_NEXT(e)) {
+				ifp = ELEMENT_DATA(e);
+				/* Check if this interface could be the macvlan for this vrrp */
+				if (ifp->vmac &&
+				    !memcmp(ifp->hw_addr, ll_addr, sizeof(ll_addr) - 2) &&
+				    ((vrrp->family == AF_INET && ifp->hw_addr[sizeof(ll_addr) - 2] == 0x01) ||
+				     (vrrp->family == AF_INET6 && ifp->hw_addr[sizeof(ll_addr) - 2] == 0x02)) &&
+				    ifp->hw_addr[sizeof(ll_addr) - 1] == vrrp->vrid &&
+				    ifp->base_ifindex == vrrp->ifp->ifindex)
+				{
+					log_message(LOG_INFO, "(%s): Found matching interface %s", vrrp->iname, ifp->ifname);
+					if (vrrp->vmac_ifname[0]) {
+						if (strcmp(vrrp->vmac_ifname, ifp->ifname))
+							log_message(LOG_INFO, "(%s): vmac name mismatch %s <=> %s", vrrp->iname, vrrp->vmac_ifname, ifp->ifname);
+					}
+					else
+						strcpy(vrrp->vmac_ifname, ifp->ifname);
+					vrrp->ifp = ifp;
+					vrrp->vmac_ifindex = ifp->ifindex;
+					__set_bit(VRRP_VMAC_UP_BIT, &vrrp->vmac_flags);
+
 					break;
+				}
 			}
-			/* If there is no VMAC with the name and no existing
-			 * interface with the name, we can use it */
-			if (!e && !if_get_by_ifname(ifname))
-				break;
-
-			/* For IPv6 try vrrp6 as second attempt */
-			if (vrrp->family == AF_INET6) {
-				if (num == 0)
-					num = 6;
-				else if (num == 6)
-					num = 1;
-				else if (++num == 6)
-					num++;
-			}
-			else
-				num++;
-
-			snprintf(ifname, IFNAMSIZ, "vrrp%d.%d", num, vrrp->vrid);
 		}
 
-		/* We've found a unique name */
-		strncpy(vrrp->vmac_ifname, ifname, IFNAMSIZ);
+		/* No interface found, find an unused name */
+		if (!vrrp->vmac_ifname[0]) {
+			int num=0;
+			snprintf(ifname, IFNAMSIZ, "vrrp.%d", vrrp->vrid);
+
+			while (true) {
+				for (e = LIST_HEAD(vrrp_data->vrrp); e; ELEMENT_NEXT(e)) {
+					vrrp_o = ELEMENT_DATA(e);
+					if (!strcmp(vrrp_o->vmac_ifname, ifname))
+						break;
+				}
+				/* If there is no VMAC with the name and no existing
+				 * interface with the name, we can use it */
+				if (!e && !if_get_by_ifname(ifname))
+					break;
+
+				/* For IPv6 try vrrp6 as second attempt */
+				if (vrrp->family == AF_INET6) {
+					if (num == 0)
+						num = 6;
+					else if (num == 6)
+						num = 1;
+					else if (++num == 6)
+						num++;
+				}
+				else
+					num++;
+
+				snprintf(ifname, IFNAMSIZ, "vrrp%d.%d", num, vrrp->vrid);
+			}
+
+			/* We've found a unique name */
+			strncpy(vrrp->vmac_ifname, ifname, IFNAMSIZ);
+		}
 	}
 
 	/* Make sure we have an IP address as needed */

keepalived/vrrp/vrrp_if.c
@@ -105,6 +105,13 @@ if_get_by_ifname(const char *ifname)
 	return NULL;
 }
 
+/* Return the interface list itself */
+list
+get_if_list(void)
+{
+	return if_queue;
+}
+
 /*
  * Reflect base interface flags on VMAC interfaces.
  * VMAC interfaces should never update it own flags, only be reflected
@@ -290,7 +297,7 @@ free_if(void *data)
 void
 dump_if(void *data)
 {
-	interface_t *ifp = data;
+	interface_t *ifp = data, *ifp_u;
 	char addr_str[INET6_ADDRSTRLEN];
 
 	log_message(LOG_INFO, "------< NIC >------");
@@ -300,7 +307,7 @@ dump_if(void *data)
 	inet_ntop(AF_INET6, &ifp->sin6_addr, addr_str, sizeof(addr_str));
 	log_message(LOG_INFO, " IPv6 address = %s", addr_str);
 
-	/* FIXME: Harcoded for ethernet */
+	/* FIXME: Hardcoded for ethernet */
 	if (ifp->hw_type == ARPHRD_ETHER)
 		log_message(LOG_INFO, " MAC = %.2x:%.2x:%.2x:%.2x:%.2x:%.2x",
 		       ifp->hw_addr[0], ifp->hw_addr[1], ifp->hw_addr[2]
@@ -328,6 +335,9 @@ dump_if(void *data)
 		log_message(LOG_INFO, " HW Type = UNKNOWN");
 		break;
 	}
+
+	if (ifp->vmac && (ifp_u = if_get_by_ifindex(ifp->base_ifindex)))
+		log_message(LOG_INFO, " VMAC underlying interface = %s", ifp_u->ifname);
 
 	/* MII channel supported ? */
 	if (IF_MII_SUPPORTED(ifp))

keepalived/vrrp/vrrp_netlink.c
@@ -38,6 +38,7 @@
 /* local include */
 #include "check_api.h"
 #include "vrrp_netlink.h"
+#include "vrrp_vmac.h"
 #include "logger.h"
 #include "memory.h"
 #include "scheduler.h"
@@ -295,6 +296,12 @@ parse_rtattr(struct rtattr **tb, int max, struct rtattr *rta, int len)
 			tb[rta->rta_type] = rta;
 		rta = RTA_NEXT(rta, len);
 	}
+}
+
+static void
+parse_rtattr_nested(struct rtattr **tb, int max, struct rtattr *rta)
+{
+        parse_rtattr(tb, max, RTA_DATA(rta), RTA_PAYLOAD(rta));
 }
 
 char *
@@ -595,6 +602,9 @@ netlink_if_link_populate(interface_t *ifp, struct rtattr *tb[], struct ifinfomsg
 {
 	char *name;
 	int i;
+	struct rtattr* linkinfo[IFLA_INFO_MAX+1];
+	struct rtattr* linkattr[IFLA_MACVLAN_MAX+1];
+	interface_t *ifp_base;
 
 	name = (char *) RTA_DATA(tb[IFLA_IFNAME]);
 	/* Fill the interface structure */
@@ -602,12 +612,6 @@ netlink_if_link_populate(interface_t *ifp, struct rtattr *tb[], struct ifinfomsg
 	ifp->ifindex = ifi->ifi_index;
 	ifp->mtu = *(int *) RTA_DATA(tb[IFLA_MTU]);
 	ifp->hw_type = ifi->ifi_type;
-
-	if (!ifp->vmac) {
-		if_vmac_reflect_flags(ifi->ifi_index, ifi->ifi_flags);
-		ifp->flags = ifi->ifi_flags;
-		ifp->base_ifindex = ifi->ifi_index;
-	}
 
 	if (tb[IFLA_ADDRESS]) {
 		int hw_addr_len = RTA_PAYLOAD(tb[IFLA_ADDRESS]);
@@ -630,6 +634,42 @@ netlink_if_link_populate(interface_t *ifp, struct rtattr *tb[], struct ifinfomsg
 				ifp->hw_addr_len = hw_addr_len;
 		}
 	}
+
+	/* See if this interface is a MACVLAN of ours */
+	if (tb[IFLA_LINKINFO] && tb[IFLA_LINK]){
+		/* If appears that the value of *(int*)RTA_DATA(tb[IFLA_LINKINFO]) is 0x1000c
+		 *   for macvlan.  0x10000 for nested data, or'ed with 0x0c for macvlan;
+		 *   other values are 0x09 for vlan, 0x0b for bridge, 0x08 for tun, -1 for no
+		 *   underlying interface.
+		 *
+		 * I can't find where in the kernel these values are set or defined, so use
+		 * the string as below.
+		 */
+		parse_rtattr_nested(linkinfo, IFLA_INFO_MAX, tb[IFLA_LINKINFO]);
+
+		if (linkinfo[IFLA_INFO_KIND] &&
+		    RTA_PAYLOAD(linkinfo[IFLA_INFO_KIND]) >= strlen(macvlan_ll_kind) &&
+		    !strncmp(macvlan_ll_kind, RTA_DATA(linkinfo[IFLA_INFO_KIND]), strlen(macvlan_ll_kind)) &&
+		    linkinfo[IFLA_INFO_DATA]) {
+			parse_rtattr_nested(linkattr, IFLA_MACVLAN_MAX, linkinfo[IFLA_INFO_DATA]);
+
+			if (linkattr[IFLA_MACVLAN_MODE] &&
+			    *(int*)RTA_DATA(linkattr[IFLA_MACVLAN_MODE]) == MACVLAN_MODE_PRIVATE) {
+				ifp->base_ifindex = *(int*)RTA_DATA(tb[IFLA_LINK]);
+				ifp->vmac = true;
+			}
+		}
+	}
+
+	if (!ifp->vmac) {
+		if_vmac_reflect_flags(ifi->ifi_index, ifi->ifi_flags);
+		ifp->flags = ifi->ifi_flags;
+		ifp->base_ifindex = ifi->ifi_index;
+	} else {
+		if ((ifp_base = if_get_by_ifindex(ifp->base_ifindex)))
+			ifp->flags = ifp_base->flags;
+	}
+
 	return 1;
 }
 

keepalived/vrrp/vrrp_parser.c
@@ -190,11 +190,20 @@ static void
 vrrp_vmac_handler(vector_t *strvec)
 {
 	vrrp_t *vrrp = LIST_TAIL_DATA(vrrp_data->vrrp);
+	interface_t *ifp;
 
 	__set_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags);
 
-	if (vector_size(strvec) >= 2)
+	if (vector_size(strvec) >= 2) {
 		strncpy(vrrp->vmac_ifname, vector_slot(strvec, 1), IFNAMSIZ - 1);
+
+		/* Check if the interface exists and is a macvlan we can use */
+		if ((ifp = if_get_by_ifname(vrrp->vmac_ifname)) &&
+		    !ifp->vmac) {
+			log_message(LOG_INFO, "(%s): interface %s already exists and is not a private macvlan; ignoring vmac if_name", vrrp->iname, vrrp->vmac_ifname);
+			vrrp->vmac_ifname[0] = '\0';
+		}
+	}
 }
 static void
 vrrp_vmac_xmit_base_handler(vector_t *strvec)

keepalived/vrrp/vrrp_vmac.c
@@ -36,8 +36,8 @@
 #include "vrrp_ipaddress.h"
 
 #ifdef _HAVE_VRRP_VMAC_
-/* private matter */
-static const char *ll_kind = "macvlan";
+const char * const macvlan_ll_kind = "macvlan";
+u_char ll_addr[ETH_ALEN] = {0x00, 0x00, 0x5e, 0x00, 0x01, 0x00};
 
 static void
 make_link_local_address(struct in6_addr* l3_addr, const u_char* ll_addr)
@@ -93,7 +93,6 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 	interface_t *ifp;
 	interface_t *base_ifp;
 	char ifname[IFNAMSIZ];
-	u_char ll_addr[ETH_ALEN] = {0x00, 0x00, 0x5e, 0x00, 0x01, vrrp->vrid};
 	struct {
 		struct nlmsghdr n;
 		struct ifinfomsg ifi;
@@ -106,6 +105,8 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 	if (vrrp->family == AF_INET6)
 		ll_addr[4] = 0x02;
 
+	ll_addr[ETH_ALEN-1] = vrrp->vrid;
+
 	memset(&req, 0, sizeof (req));
 	memset(ifname, 0, IFNAMSIZ);
 	strncpy(ifname, vrrp->vmac_ifname, IFNAMSIZ - 1);
@@ -115,28 +116,12 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 	 * by a previous instance.
 	 */
 	if ((ifp = if_get_by_ifname(ifname))) {
-		/* Check to see whether this interface has correct vmac ? */
+		/* Check to see whether this interface has wrong mac ? */
 		if (memcmp((const void *) ifp->hw_addr,
-			   (const void *) ll_addr, ETH_ALEN) == 0) {
-
-			/* We have found a VIF and the vmac matches */
-			log_message(LOG_INFO, "vmac: Matching interface VMAC found on interfaces %s for "
-					      "vrrp_instance %s!!!"
-					    , vrrp->vmac_ifname, vrrp->iname);
-
-			/* (re)set VMAC properties (if deleted on reload) */
-			ifp->base_ifindex = vrrp->ifp->ifindex;
-			ifp->vmac = 1;
-			ifp->flags = vrrp->ifp->flags; /* Copy base interface flags */
-			vrrp->ifp = ifp;
-			/* Save ifindex for use on delete */
-			vrrp->vmac_ifindex = IF_INDEX(ifp);
-			__set_bit(VRRP_VMAC_UP_BIT, &vrrp->vmac_flags);
-			return 1;
-		} else {
+			   (const void *) ll_addr, ETH_ALEN) != 0) {
 			/* We have found a VIF but the vmac do not match */
 			log_message(LOG_INFO, "vmac: Removing old VMAC interface %s due to conflicting "
-					      "interface VMAC for vrrp_instance %s!!!"
+					      "interface MAC for vrrp_instance %s!!!"
 					    , vrrp->vmac_ifname, vrrp->iname);
 
 			/* Request that NETLINK remove the VIF interface first */
@@ -167,7 +152,7 @@ netlink_link_add_vmac(vrrp_t *vrrp)
 	/* macvlan settings */
 	linkinfo = NLMSG_TAIL(&req.n);
 	addattr_l(&req.n, sizeof(req), IFLA_LINKINFO, NULL, 0);
-	addattr_l(&req.n, sizeof(req), IFLA_INFO_KIND, (void *)ll_kind, strlen(ll_kind));
+	addattr_l(&req.n, sizeof(req), IFLA_INFO_KIND, (void *)macvlan_ll_kind, strlen(macvlan_ll_kind));
 	data = NLMSG_TAIL(&req.n);
 	addattr_l(&req.n, sizeof(req), IFLA_INFO_DATA, NULL, 0);
 