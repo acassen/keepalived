acassen/keepalived

commit 4ed2ce1582b86fbb547baa9f0273919f1efc4959

Ensure number of VIPs doesn't exceed 255 per instance
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/include/vrrp.h
@@ -76,6 +76,7 @@ typedef struct {
 #define VRRP_PRIO_OWNER		255		/* priority of the ip owner -- rfc2338.5.3.4 */
 #define VRRP_PRIO_DFL		100		/* default priority -- rfc2338.5.3.4 */
 #define VRRP_PRIO_STOP		0		/* priority to stop -- rfc2338.5.3.4 */
+#define VRRP_MAX_ADDR		((2^8)-1)	/* count addr field is 8 bits wide */
 #define VRRP_AUTH_NONE		0		/* no authentification -- rfc2338.5.3.6 */
 #ifdef _WITH_VRRP_AUTH_
 #define VRRP_AUTH_PASS		1		/* password authentification -- rfc2338.5.3.6 */

keepalived/vrrp/vrrp.c
@@ -1827,8 +1827,14 @@ vrrp_complete_instance(vrrp_t * vrrp)
 		hdr_len = sizeof(struct ether_header) + sizeof(struct ip6_hdr) + sizeof(vrrphdr_t);
 		max_addr = (vrrp->ifp->mtu - hdr_len) / sizeof(struct in6_addr);
 	}
+
+	/* Count IP addrs field is 8 bits wide, giving a maximum address count of 255 */
+	if (max_addr > VRRP_MAX_ADDR)
+		max_addr = VRRP_MAX_ADDR;
+
+	/* Move any extra addresses to be evips. We won't advertise them, but at least we can respond to them */
 	if (!LIST_ISEMPTY(vrrp->vip) && LIST_SIZE(vrrp->vip) > max_addr) {
-		log_message(LOG_INFO, "(%s): Number of VIPs (%d) exceeds space available in packet (max %d addresses) - excess moved to eVIPs",
+		log_message(LOG_INFO, "(%s): Number of VIPs (%d) exceeds maximum/space available in packet (max %d addresses) - excess moved to eVIPs",
 				vrrp->iname, LIST_SIZE(vrrp->vip), max_addr);
 		for (i = 0, e = LIST_HEAD(vrrp->vip); e; i++, e = next) {
 			next = e->next;
@@ -1856,6 +1862,7 @@ vrrp_complete_instance(vrrp_t * vrrp)
 		vrrp->wantstate = VRRP_STATE_BACK;
 	}
 	else if (vrrp->base_priority == VRRP_PRIO_OWNER && !vrrp->nopreempt) {
+		/* Act as though state MASTER had been specified, to speed transition to master state */
 		vrrp->init_state = VRRP_STATE_MAST;
 		vrrp->wantstate = VRRP_STATE_MAST;
 	}

commit ca6fce31f377451286c325a67e72af79da58b091

Fix maximum number of VIPs allowed
Commit 4ed2ce1 introduced an incorrect definition for VRRP_MAX_ADDR.
^ does not mean power in C, so the definition of VRRP_MAX_ADDR as
((2^8)-1) did not produce 255 as desired, but 9. This commit simply
defines VRRP_MAX_ADDR as 0xFF.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/include/vrrp.h
@@ -74,7 +74,7 @@ typedef struct {
 #define VRRP_PRIO_OWNER		255		/* priority of the ip owner -- rfc2338.5.3.4 */
 #define VRRP_PRIO_DFL		100		/* default priority -- rfc2338.5.3.4 */
 #define VRRP_PRIO_STOP		0		/* priority to stop -- rfc2338.5.3.4 */
-#define VRRP_MAX_ADDR		((2^8)-1)	/* count addr field is 8 bits wide */
+#define VRRP_MAX_ADDR		0xFF		/* count addr field is 8 bits wide */
 #define VRRP_AUTH_NONE		0		/* no authentification -- rfc2338.5.3.6 */
 #ifdef _WITH_VRRP_AUTH_
 #define VRRP_AUTH_PASS		1		/* password authentification -- rfc2338.5.3.6 */