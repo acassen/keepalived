acassen/keepalived

commit 057b37659d09cf5830a7cf6b805ea0e9a6727f85

Ensure standard configure generated defines are used
The defines used in the compiles in the various subdirectories were
specified in each Makefile.in which could lead to inconsistencies.
This commit defines APP_DEFS in configure.in, which is then used
in each Makefile.in.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

configure
@@ -621,6 +621,7 @@ ac_includes_default="\
 
 ac_subst_vars='LTLIBOBJS
 LIBOBJS
+APP_DEFS
 VRRP_SUPPORT
 USE_NL
 IPVS_SUPPORT
@@ -4837,6 +4838,9 @@ fi
 
 
 
+
+
+APP_DEFS="-D${KERN} -D${IPVS_SUPPORT} -D${IPVS_SYNCD} -D${VRRP_SUPPORT} -D${VRRP_VMAC} -D${ADDR_GEN_MODE} -D${SNMP_SUPPORT} -D${USE_NL3} -D${VRRP_AUTH_SUPPORT} -D${SO_MARK_SUPPORT} -D${USE_LIBIPTC} -D${USE_LIBIPSET} -D${IPV4_DEVCONF} -D${IF_H_LINK_H_COLLISION} ${DFLAGS}"
 
 
 { $as_echo "$as_me:${as_lineno-$LINENO}: checking for an ANSI C-conforming const" >&5

configure.in
@@ -439,6 +439,9 @@ AC_SUBST(IPVS_SUPPORT)
 AC_SUBST(USE_NL)
 AC_SUBST(VRRP_SUPPORT)
 
+APP_DEFS="-D${KERN} -D${IPVS_SUPPORT} -D${IPVS_SYNCD} -D${VRRP_SUPPORT} -D${VRRP_VMAC} -D${ADDR_GEN_MODE} -D${SNMP_SUPPORT} -D${USE_NL3} -D${VRRP_AUTH_SUPPORT} -D${SO_MARK_SUPPORT} -D${USE_LIBIPTC} -D${USE_LIBIPSET} -D${IPV4_DEVCONF} -D${IF_H_LINK_H_COLLISION} ${DFLAGS}"
+AC_SUBST(APP_DEFS)
+
 dnl ----[ Checks for typedefs, structures, and compiler characteristics ]----
 AC_C_CONST
 AC_TYPE_PID_T

keepalived/check/Makefile.in
@@ -10,8 +10,7 @@ SNMP_FLAG = @SNMP_SUPPORT@
 INCLUDES = -I../include -I../../lib
 CFLAGS	 = $(INCLUDES) @CFLAGS@ @CPPFLAGS@ \
 	   -Wall -Wunused -Wstrict-prototypes
-DEFS	 = -D@KERN@ -D@IPVS_SUPPORT@ -D@IPVS_SYNCD@ -D@VRRP_SUPPORT@ -D@SNMP_SUPPORT@ -D@SO_MARK_SUPPORT@ @DFLAGS@
-COMPILE	 = $(CC) $(CFLAGS) $(DEFS)
+COMPILE	 = $(CC) $(CFLAGS) @APP_DEFS@
 
 OBJS = 	check_daemon.o check_data.o check_parser.o \
 	check_api.o check_tcp.o check_http.o check_ssl.o \

keepalived/core/Makefile.in
@@ -9,8 +9,7 @@ SNMP_FLAG = @SNMP_SUPPORT@
 INCLUDES = -I../include -I../../lib
 CFLAGS	 = $(INCLUDES) @CFLAGS@ @CPPFLAGS@ \
 	   -Wall -Wunused -Wstrict-prototypes
-DEFS	 = -D@KERN@ -D@IPVS_SUPPORT@ -D@VRRP_SUPPORT@ -D@SNMP_SUPPORT@ -D@SO_MARK_SUPPORT@ -D@USE_LIBIPSET@ @DFLAGS@
-COMPILE	 = $(CC) $(CFLAGS) $(DEFS)
+COMPILE	 = $(CC) $(CFLAGS) @APP_DEFS@
 
 OBJS = 	main.o daemon.o pidfile.o layer4.o smtp.o \
 	global_data.o global_parser.o

keepalived/libipvs-2.4/Makefile.in
@@ -1,7 +1,7 @@
 # Makefile to make libipvsc.
 
 CC	= @CC@
-CFLAGS	= @CFLAGS@ @CPPFLAGS@ -Wall -Wunused
+CFLAGS	= @CFLAGS@ @CPPFLAGS@ @APP_DEFS@ -Wall -Wunused
 
 export OBJS += libipvs.a
 

keepalived/libipvs-2.6/Makefile.in
@@ -1,7 +1,7 @@
 # Makefile for libipvs
 
 CC		= @CC@
-CFLAGS		= @CFLAGS@ @CPPFLAGS@ -D@USE_NL@ -Wall -Wunused
+CFLAGS		= @CFLAGS@ @CPPFLAGS@ @APP_DEFS@ -Wall -Wunused
 
 export OBJS += libipvs.a
 

keepalived/vrrp/Makefile.in
@@ -13,8 +13,7 @@ NL3_FLAG = @USE_NL3@
 INCLUDES = -I../include -I../../lib
 CFLAGS	 = $(INCLUDES) @CFLAGS@ @CPPFLAGS@ \
 	   -Wall -Wunused -Wstrict-prototypes
-DEFS	 = -D@KERN@ -D@IPVS_SUPPORT@ -D@IPVS_SYNCD@ -D@VRRP_VMAC@ -D@ADDR_GEN_MODE@ -D@SNMP_SUPPORT@ -D@USE_NL3@ @DFLAGS@ -D@VRRP_AUTH_SUPPORT@ -D@USE_LIBIPTC@ -D@USE_LIBIPSET@ -D@IPV4_DEVCONF@ -D@IF_H_LINK_H_COLLISION@
-COMPILE	 = $(CC) $(CFLAGS) $(DEFS)
+COMPILE	 = $(CC) $(CFLAGS) @APP_DEFS@
 
 OBJS = 	vrrp_daemon.o vrrp_print.o vrrp_data.o vrrp_parser.o \
 	vrrp.o vrrp_notify.o vrrp_scheduler.o vrrp_sync.o vrrp_index.o \

lib/Makefile.in
@@ -8,8 +8,7 @@ CC	 = @CC@
 INCLUDES = -I.
 CFLAGS	 = $(INCLUDES) @CFLAGS@ @CPPFLAGS@ \
 	   -Wall -Wunused -Wstrict-prototypes
-DEFS	 = @DFLAGS@ -D@SNMP_SUPPORT@
-COMPILE	 = $(CC) $(CFLAGS) $(DEFS)
+COMPILE	 = $(CC) $(CFLAGS) @APP_DEFS@
 
 OBJS = 	memory.o utils.o notify.o timer.o scheduler.o \
 	vector.o list.o html.o parser.o signals.o logger.o

commit fdc58299264e3389f1b3d1467596f365e5fb3cc9

Fix IPVS with IPv6 addresses
Commit 057b376 caused libipvs to stop using the netlink interface,
thereby stopping support for IPv6, which only works with the netlink
interface.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

configure
@@ -624,7 +624,7 @@ LIBOBJS
 BUILD_OPTS
 APP_DEFS
 VRRP_SUPPORT
-USE_NL
+IPVS_USE_NL
 IPVS_SUPPORT
 VERSION_YEAR
 VERSION_DATE
@@ -4058,7 +4058,7 @@ fi
 $as_echo "$ac_cv_lib_nl_genl_3_genl_connect" >&6; }
 if test "x$ac_cv_lib_nl_genl_3_genl_connect" = xyes; then :
 
-        USE_NL="LIBIPVS_USE_NL"
+        IPVS_USE_NL="LIBIPVS_USE_NL"
         CFLAGS="$CFLAGS $(pkg-config --cflags libnl-genl-3.0)"
         LIBS="$LIBS $(pkg-config --libs libnl-genl-3.0)"
 
@@ -4153,14 +4153,14 @@ fi
 $as_echo "$ac_cv_lib_nl_nl_socket_modify_cb" >&6; }
 if test "x$ac_cv_lib_nl_nl_socket_modify_cb" = xyes; then :
 
-        USE_NL="LIBIPVS_USE_NL"
+        IPVS_USE_NL="LIBIPVS_USE_NL"
         USE_NL3="_HAVE_LIBNL1_"
         CFLAGS="$CFLAGS -DFALLBACK_LIBNL1"
         LIBS="$LIBS $(pkg-config --libs libnl-1)"
 
 else
 
-        USE_NL="LIBIPVS_DONTUSE_NL"
+        IPVS_USE_NL="LIBIPVS_DONTUSE_NL"
         { $as_echo "$as_me:${as_lineno-$LINENO}: WARNING: keepalived will be built without libnl support." >&5
 $as_echo "$as_me: WARNING: keepalived will be built without libnl support." >&2;}
 
@@ -5130,7 +5130,7 @@ fi
 
 
 
-APP_DEFS="-D${KERN} -D${IPVS_SUPPORT} -D${IPVS_SYNCD} -D${VRRP_SUPPORT} -D${VRRP_VMAC} -D${ADDR_GEN_MODE} -D${SNMP_SUPPORT} -D${SNMP_KEEPALIVED_SUPPORT} -D${SNMP_CHECKER_SUPPORT} -D${SNMP_RFC_SUPPORT} -D${SNMP_RFCV2_SUPPORT} -D${SNMP_RFCV3_SUPPORT} -D${USE_NL3} -D${VRRP_AUTH_SUPPORT} -D${SO_MARK_SUPPORT} -D${USE_LIBIPTC} -D${USE_LIBIPSET} -D${IPV4_DEVCONF} -D${IF_H_LINK_H_COLLISION} -D${LINUX_NET_IF_H_COLLISION} -D${SOCK_NONBLOCK_SUPPORT} -D${SOCK_CLOEXEC_SUPPORT} -D${FIB_ROUTING_SUPPORT} ${DFLAGS}"
+APP_DEFS="-D${KERN} -D${IPVS_SUPPORT} -D${IPVS_SYNCD} -D${VRRP_SUPPORT} -D${VRRP_VMAC} -D${ADDR_GEN_MODE} -D${SNMP_SUPPORT} -D${SNMP_KEEPALIVED_SUPPORT} -D${SNMP_CHECKER_SUPPORT} -D${SNMP_RFC_SUPPORT} -D${SNMP_RFCV2_SUPPORT} -D${SNMP_RFCV3_SUPPORT} -D${IPVS_USE_NL} -D${USE_NL3} -D${VRRP_AUTH_SUPPORT} -D${SO_MARK_SUPPORT} -D${USE_LIBIPTC} -D${USE_LIBIPSET} -D${IPV4_DEVCONF} -D${IF_H_LINK_H_COLLISION} -D${LINUX_NET_IF_H_COLLISION} -D${SOCK_NONBLOCK_SUPPORT} -D${SOCK_CLOEXEC_SUPPORT} -D${FIB_ROUTING_SUPPORT} ${DFLAGS}"
 BUILD_OPTS=`echo ${APP_DEFS} | sed -e 's/ "$//' -e 's/.*"//' -e 's/-D//g' -e 's/_ / /g' -e 's/ _/ /g' -e 's/^_//' -e 's/_$//'`
 
 
@@ -6610,7 +6610,7 @@ if test "${IPVS_SUPPORT}" = "_WITH_LVS_"; then
   elif test "${IPVS_SYNCD}" = "_WITHOUT_IPVS_SYNCD_"; then
     echo "IPVS sync daemon support : No"
   fi
-  if test "${USE_NL}" = "LIBIPVS_USE_NL"; then
+  if test "${IPVS_USE_NL}" = "LIBIPVS_USE_NL"; then
     echo "IPVS use libnl           : Yes"
   else
     echo "IPVS use libnl           : No"

configure.ac
@@ -80,7 +80,7 @@ AC_CHECK_LIB(nl-3, nl_socket_alloc,
   [
     AC_CHECK_LIB(nl-genl-3, genl_connect,
       [
-        USE_NL="LIBIPVS_USE_NL"
+        IPVS_USE_NL="LIBIPVS_USE_NL"
         CFLAGS="$CFLAGS $(pkg-config --cflags libnl-genl-3.0)"
         LIBS="$LIBS $(pkg-config --libs libnl-genl-3.0)"
       ],
@@ -97,13 +97,13 @@ AC_CHECK_LIB(nl-3, nl_socket_alloc,
   [
     AC_CHECK_LIB(nl, nl_socket_modify_cb,
       [
-        USE_NL="LIBIPVS_USE_NL"
+        IPVS_USE_NL="LIBIPVS_USE_NL"
         USE_NL3="_HAVE_LIBNL1_"
         CFLAGS="$CFLAGS -DFALLBACK_LIBNL1"
         LIBS="$LIBS $(pkg-config --libs libnl-1)"
       ],
       [
-        USE_NL="LIBIPVS_DONTUSE_NL"
+        IPVS_USE_NL="LIBIPVS_DONTUSE_NL"
         AC_MSG_WARN([keepalived will be built without libnl support.])
       ])
   ])
@@ -598,10 +598,10 @@ AC_SUBST(VERSION)
 AC_SUBST(VERSION_DATE)
 AC_SUBST(VERSION_YEAR)
 AC_SUBST(IPVS_SUPPORT)
-AC_SUBST(USE_NL)
+AC_SUBST(IPVS_USE_NL)
 AC_SUBST(VRRP_SUPPORT)
 
-APP_DEFS="-D${KERN} -D${IPVS_SUPPORT} -D${IPVS_SYNCD} -D${VRRP_SUPPORT} -D${VRRP_VMAC} -D${ADDR_GEN_MODE} -D${SNMP_SUPPORT} -D${SNMP_KEEPALIVED_SUPPORT} -D${SNMP_CHECKER_SUPPORT} -D${SNMP_RFC_SUPPORT} -D${SNMP_RFCV2_SUPPORT} -D${SNMP_RFCV3_SUPPORT} -D${USE_NL3} -D${VRRP_AUTH_SUPPORT} -D${SO_MARK_SUPPORT} -D${USE_LIBIPTC} -D${USE_LIBIPSET} -D${IPV4_DEVCONF} -D${IF_H_LINK_H_COLLISION} -D${LINUX_NET_IF_H_COLLISION} -D${SOCK_NONBLOCK_SUPPORT} -D${SOCK_CLOEXEC_SUPPORT} -D${FIB_ROUTING_SUPPORT} ${DFLAGS}"
+APP_DEFS="-D${KERN} -D${IPVS_SUPPORT} -D${IPVS_SYNCD} -D${VRRP_SUPPORT} -D${VRRP_VMAC} -D${ADDR_GEN_MODE} -D${SNMP_SUPPORT} -D${SNMP_KEEPALIVED_SUPPORT} -D${SNMP_CHECKER_SUPPORT} -D${SNMP_RFC_SUPPORT} -D${SNMP_RFCV2_SUPPORT} -D${SNMP_RFCV3_SUPPORT} -D${IPVS_USE_NL} -D${USE_NL3} -D${VRRP_AUTH_SUPPORT} -D${SO_MARK_SUPPORT} -D${USE_LIBIPTC} -D${USE_LIBIPSET} -D${IPV4_DEVCONF} -D${IF_H_LINK_H_COLLISION} -D${LINUX_NET_IF_H_COLLISION} -D${SOCK_NONBLOCK_SUPPORT} -D${SOCK_CLOEXEC_SUPPORT} -D${FIB_ROUTING_SUPPORT} ${DFLAGS}"
 BUILD_OPTS=`echo ${APP_DEFS} | sed -e 's/ "$//' -e 's/.*"//' -e 's/-D//g' -e 's/_ / /g' -e 's/ _/ /g' -e 's/^_//' -e 's/_$//'`
 AC_SUBST(APP_DEFS)
 AC_SUBST(BUILD_OPTS)
@@ -655,7 +655,7 @@ if test "${IPVS_SUPPORT}" = "_WITH_LVS_"; then
   elif test "${IPVS_SYNCD}" = "_WITHOUT_IPVS_SYNCD_"; then
     echo "IPVS sync daemon support : No"
   fi
-  if test "${USE_NL}" = "LIBIPVS_USE_NL"; then
+  if test "${IPVS_USE_NL}" = "LIBIPVS_USE_NL"; then
     echo "IPVS use libnl           : Yes"
   else
     echo "IPVS use libnl           : No"

keepalived/libipvs-2.6/libipvs.c
@@ -1053,6 +1053,7 @@ out_err:
 	return NULL;
 }
 
+#ifdef _INCLUDE_UNUSED_CODE_
 #ifdef LIBIPVS_USE_NL
 static int ipvs_timeout_parse_cb(struct nl_msg *msg, void *arg)
 {
@@ -1074,7 +1075,6 @@ static int ipvs_timeout_parse_cb(struct nl_msg *msg, void *arg)
 }
 #endif
 
-#ifdef _INCLUDE_UNUSED_CODE_
 ipvs_timeout_t *ipvs_get_timeout(void)
 {
 	ipvs_timeout_t *u;
@@ -1106,6 +1106,7 @@ ipvs_timeout_t *ipvs_get_timeout(void)
 }
 #endif
 
+#ifdef _INCLUDE_UNUSED_CODE_
 #ifdef LIBIPVS_USE_NL
 static int ipvs_daemon_parse_cb(struct nl_msg *msg, void *arg)
 {
@@ -1141,7 +1142,6 @@ static int ipvs_daemon_parse_cb(struct nl_msg *msg, void *arg)
 }
 #endif
 
-#ifdef _INCLUDE_UNUSED_CODE_
 ipvs_daemon_t *ipvs_get_daemon(void)
 {
 	ipvs_daemon_t *u;