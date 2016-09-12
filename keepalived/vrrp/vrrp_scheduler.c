acassen/keepalived

commit 7c677ea600c97866bc17baa0b4837ae56d90bc94

Streamline use of vrrp_update_priority()
Don't call vrrp_update_priorty() on a VRRP instance that is the address
owner, since it won't update the effective priority anyway.

vrrp_update_priority() also doesn't need to check for sync group or
global tracking, since it will only be called if !sync || global_tracking.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_scheduler.c
@@ -206,44 +206,50 @@ vrrp_init_state(list l)
 	for (e = LIST_HEAD(l); e; ELEMENT_NEXT(e)) {
 		vrrp = ELEMENT_DATA(e);
 
-		/* In case of VRRP SYNC, we have to carefully check that we are
-		 * not running floating priorities on any VRRP instance.
-		 */
-		if (vrrp->sync && !vrrp->sync->global_tracking) {
-			element e2;
-			tracked_sc_t *sc;
-			tracked_if_t *tip;
-			int warning = 0;
+		/* The effective priority is never changed for PRIO_OWNER */
+		if (vrrp->base_priority != VRRP_PRIO_OWNER) {
+			/* In case of VRRP SYNC, we have to carefully check that we are
+			 * not running floating priorities on any VRRP instance, or they
+			 * are all running with the same tracking conf.
+			 */
+			if (vrrp->sync && !vrrp->sync->global_tracking) {
+				element e2;
+				tracked_sc_t *sc;
+				tracked_if_t *tip;
+				bool int_warning = false;
+				bool script_warning = false;
 
-			if (!LIST_ISEMPTY(vrrp->track_ifp)) {
-				for (e2 = LIST_HEAD(vrrp->track_ifp); e2; ELEMENT_NEXT(e2)) {
-					tip = ELEMENT_DATA(e2);
-					if (tip->weight) {
-						tip->weight = 0;
-						warning++;
+				if (!LIST_ISEMPTY(vrrp->track_ifp)) {
+					for (e2 = LIST_HEAD(vrrp->track_ifp); e2; ELEMENT_NEXT(e2)) {
+						tip = ELEMENT_DATA(e2);
+						if (tip->weight) {
+							tip->weight = 0;
+							int_warning = true;
+						}
 					}
 				}
-			}
 
-			if (!LIST_ISEMPTY(vrrp->track_script)) {
-				for (e2 = LIST_HEAD(vrrp->track_script); e2; ELEMENT_NEXT(e2)) {
-					sc = ELEMENT_DATA(e2);
-					if (sc->weight) {
-						sc->scr->inuse--;
-						warning++;
+				if (!LIST_ISEMPTY(vrrp->track_script)) {
+					for (e2 = LIST_HEAD(vrrp->track_script); e2; ELEMENT_NEXT(e2)) {
+						sc = ELEMENT_DATA(e2);
+						if (sc->weight) {
+							sc->scr->inuse--;
+							script_warning = true;
+						}
 					}
 				}
-			}
 
-			if (warning > 0) {
-				log_message(LOG_INFO, "VRRP_Instance(%s) : ignoring "
-						 "tracked script with weights due to SYNC group",
-				       vrrp->iname);
+				if (int_warning)
+					log_message(LOG_INFO, "VRRP_Instance(%s) : ignoring weights of "
+							 "tracked interface due to SYNC group", vrrp->iname);
+				if (script_warning)
+					log_message(LOG_INFO, "VRRP_Instance(%s) : ignoring "
+							 "tracked script with weights due to SYNC group", vrrp->iname);
+			} else {
+				/* Register new priority update thread */
+				thread_add_timer(master, vrrp_update_priority,
+						 vrrp, vrrp->master_adver_int);
 			}
-		} else {
-			/* Register new priority update thread */
-			thread_add_timer(master, vrrp_update_priority,
-					 vrrp, vrrp->adver_int);
 		}
 
 		if (vrrp->wantstate == VRRP_STATE_MAST
@@ -329,14 +335,14 @@ vrrp_init_script(list l)
 
 	for (e = LIST_HEAD(l); e; ELEMENT_NEXT(e)) {
 		vscript = ELEMENT_DATA(e);
-		if (vscript->inuse == 0)
+		if (!vscript->inuse)
 			vscript->result = VRRP_SCRIPT_STATUS_DISABLED;
+		else {
+			if (vscript->result == VRRP_SCRIPT_STATUS_INIT) {
+				vscript->result = vscript->rise - 1; /* one success is enough */
+			else if (vscript->result == VRRP_SCRIPT_STATUS_INIT_GOOD) {
+				vscript->result = vscript->rise; /* one failure is enough */
 
-		if (vscript->result == VRRP_SCRIPT_STATUS_INIT) {
-			vscript->result = vscript->rise - 1; /* one success is enough */
-			thread_add_event(master, vrrp_script_thread, vscript, vscript->interval);
-		} else if (vscript->result == VRRP_SCRIPT_STATUS_INIT_GOOD) {
-			vscript->result = vscript->rise; /* one failure is enough */
 			thread_add_event(master, vrrp_script_thread, vscript, vscript->interval);
 		}
 	}
@@ -798,7 +804,9 @@ vrrp_set_effective_priority(vrrp_t *vrrp, int new_prio)
 
 
 /* Update VRRP effective priority based on multiple checkers.
- * This is a thread which is executed every adver_int.
+ * This is a thread which is executed every master_adver_int
+ * unless the vrrp instance is part of a sync group and there
+ * isn't global tracking for the group.
  */
 static int
 vrrp_update_priority(thread_t * thread)
@@ -810,29 +818,24 @@ vrrp_update_priority(thread_t * thread)
 	prio_offset = 0;
 
 	/* Now we will sum the weights of all interfaces which are tracked. */
-	if ((!vrrp->sync || vrrp->sync->global_tracking) && !LIST_ISEMPTY(vrrp->track_ifp))
+	if (!LIST_ISEMPTY(vrrp->track_ifp))
 		 prio_offset += vrrp_tracked_weight(vrrp->track_ifp);
 
 	/* Now we will sum the weights of all scripts which are tracked. */
-	if ((!vrrp->sync || vrrp->sync->global_tracking) && !LIST_ISEMPTY(vrrp->track_script))
+	if (!LIST_ISEMPTY(vrrp->track_script))
 		prio_offset += vrrp_script_weight(vrrp->track_script);
 
-	if (vrrp->base_priority == VRRP_PRIO_OWNER) {
-		/* we will not run a PRIO_OWNER into a non-PRIO_OWNER */
-		vrrp_set_effective_priority(vrrp, VRRP_PRIO_OWNER);
-	} else {
-		/* WARNING! we must compute new_prio on a signed int in order
-		   to detect overflows and avoid wrapping. */
-		new_prio = vrrp->base_priority + prio_offset;
-		if (new_prio < 1)
-			new_prio = 1;
-		else if (new_prio >= VRRP_PRIO_OWNER)
-			new_prio = VRRP_PRIO_OWNER - 1;
-		vrrp_set_effective_priority(vrrp, new_prio);
-	}
+	/* WARNING! we must compute new_prio on a signed int in order
+	   to detect overflows and avoid wrapping. */
+	new_prio = vrrp->base_priority + prio_offset;
+	if (new_prio < 1)
+		new_prio = 1;
+	else if (new_prio >= VRRP_PRIO_OWNER)
+		new_prio = VRRP_PRIO_OWNER - 1;
+	vrrp_set_effective_priority(vrrp, new_prio);
 
 	/* Register next priority update thread */
-	thread_add_timer(master, vrrp_update_priority, vrrp, vrrp->adver_int);
+	thread_add_timer(master, vrrp_update_priority, vrrp, vrrp->master_adver_int);
 	return 0;
 }
 

commit 5133512f937eefd07fde671596957559d44dc326

Fix compilation failure introduced in 7c677ea
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_scheduler.c
@@ -338,9 +338,9 @@ vrrp_init_script(list l)
 		if (!vscript->inuse)
 			vscript->result = VRRP_SCRIPT_STATUS_DISABLED;
 		else {
-			if (vscript->result == VRRP_SCRIPT_STATUS_INIT) {
+			if (vscript->result == VRRP_SCRIPT_STATUS_INIT)
 				vscript->result = vscript->rise - 1; /* one success is enough */
-			else if (vscript->result == VRRP_SCRIPT_STATUS_INIT_GOOD) {
+			else if (vscript->result == VRRP_SCRIPT_STATUS_INIT_GOOD)
 				vscript->result = vscript->rise; /* one failure is enough */
 
 			thread_add_event(master, vrrp_script_thread, vscript, vscript->interval);