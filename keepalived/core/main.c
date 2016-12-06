acassen/keepalived

commit dbc6c1aa2fed793015f4f1afbe81a10da7ad702e

Use nodename as default id for conditional configuration
If the node name returned by uname() is host123.abc.de, then
lines in the configuration file matching @host123 will match the
conditional configuration test. This means that it is no longer
necessary to specify the -i command line option if the conditional
configuration string used in the configuration is the node name.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/core/main.c
@@ -22,6 +22,9 @@
 
 #include "config.h"
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 
 #include <stdlib.h>
 #include <sys/utsname.h>
@@ -827,7 +830,9 @@ parse_cmdline(int argc, char **argv)
 			break;
 #endif
 		case 'i':
-			config_id = optarg;
+			FREE_PTR(config_id);
+			config_id = MALLOC(strlen(optarg) + 1);
+			strcpy(config_id, optarg);
 			break;
 		default:
 			exit(0);
@@ -921,6 +926,14 @@ keepalived_main(int argc, char **argv)
 		}
 		if (!os_major)
 			log_message(LOG_INFO, "Unable to parse kernel version %s", uname_buf.release);
+
+		/* config_id defaults to hostname */
+		if (!config_id) {
+			end = strchrnul(uname_buf.nodename, '.');
+			config_id = MALLOC(end - uname_buf.nodename + 1);
+			strncpy(config_id, uname_buf.nodename, end - uname_buf.nodename);
+			config_id[end - uname_buf.nodename] = '\0';
+		}
 	}
 
 	/* Check we can read the configuration file(s).

doc/keepalived.conf.SYNOPSIS
@@ -19,6 +19,7 @@ in you configuration file use this char.
 Any configuration line starting with '@' is a conditional configuration line.
 The word immediately following (i.e. without any space) the '@' character
 is compared against the string specified with the -i command line option,
+or if none specified, the first part of the node name as returned by uname,
 and if they don't match, the configuration line is ignored.
 
 The purpose of this is to allow a single configuration file to be used for
@@ -33,8 +34,10 @@ global_defs
 @backup router_id backup_router
 }
 
-If keepalived is invoked with -i main, then the router_id will be set to main_router,
-if invoked with -i backup, then backup_router, if not invoked with -i, or with
+If keepalived is invoked with -i main, or if -i is not specified and the node
+name is main.SOMETHING, then the router_id will be set to main_router,
+if invoked with -i backup, or the node name is backup, then backup_router,
+if not invoked with -i and the node name is not main or backup, or with
 -i anything else, then the router_id will not be set.
 
 1. Globals configurations

commit 02635c9c641a8c2007edb165d9db95588556bd2e

Fix memory leak introduced in commit dbc6c1a
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/core/main.c
@@ -193,6 +193,7 @@ free_parent_mallocs_exit(void)
 #endif
 
 	FREE_PTR(instance_name);
+	FREE_PTR(config_id);
 }
 
 char *