ffcf3d800230ea2d69d930d1e736fef0cacf11a1
Quentin Armitage <quentin@armitage.org.uk>
2017-03-10 15:39:33 +0000

    Add option to not use dlopen() for libipset, but link at link time
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_ipset.c
+++ keepalived/vrrp/vrrp_ipset.c
@@ -38,7 +38,6 @@
 #include <linux/types.h>        /* For __beXX types in userland */
 #include <linux/netfilter.h>    /* For nf_inet_addr */
 #include <stdint.h>
-#include <dlfcn.h>
 
 #include "logger.h"
 #include "global_data.h"
@@ -46,6 +45,9 @@
 #include "vrrp_ipset.h"
 #include "vrrp_ipaddress.h"
 #include "main.h"
+
+#ifdef _LIBIPSET_DYNAMIC_
+#include <dlfcn.h>
 
 /* The addresses of the functions we want */
 struct ipset_session* (*ipset_session_init_addr)(ipset_outfn outfn);
@@ -71,6 +73,9 @@ void (*ipset_load_types_addr)(void);
 #define ipset_load_types (*ipset_load_types_addr)
 
 static void* libipset_handle;
+#else
+#define ipset_cmd1 ipset_cmd
+#endif
 
 static bool
 do_ipset_cmd(struct ipset_session* session, enum ipset_cmd cmd, const char *setname,
@@ -181,8 +186,10 @@ static int create_sets(const char* addr4, const char* addr6, const char* addr_if
 
 bool ipset_init(void)
 {
+#ifdef _LIBIPSET_DYNAMIC_
 	if (libipset_handle)
 		return true;
+#endif
 
 #if HAVE_DECL_CLONE_NEWNET
 	/* Don't attempt to use ipsets if running in a namespace and the default
@@ -198,6 +205,7 @@ bool ipset_init(void)
 	}
 #endif
 
+#ifdef _LIBIPSET_DYNAMIC_
 	/* Attempt to open the ipset library */
 	if (!(libipset_handle = dlopen("libipset.so", RTLD_NOW)) &&
 	    !(libipset_handle = dlopen("libipset.so.3", RTLD_NOW)) &&
@@ -218,6 +226,7 @@ bool ipset_init(void)
 	ipset_data_set_addr = dlsym(libipset_handle,"ipset_data_set");
 	ipset_cmd_addr = dlsym(libipset_handle,"ipset_cmd");
 	ipset_load_types_addr = dlsym(libipset_handle,"ipset_load_types");
+#endif
 
 	ipset_load_types();
 
