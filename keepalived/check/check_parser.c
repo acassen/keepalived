ecd0ab2326b56b27f446b2082ea2b2348a5dbb98
Quentin Armitage <quentin@armitage.org.uk>
2017-04-03 15:19:55 +0100

    Fix memory leak if quorum up/down scripts specified multiple times
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/check/check_parser.c
+++ keepalived/check/check_parser.c
@@ -363,12 +363,20 @@ static void
 quorum_up_handler(vector_t *strvec)
 {
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
+	if (vs->quorum_up) {
+		log_message(LOG_INFO, "(%s): quorum_up script already specified - ignoring %s", vs->vsgname, FMT_STR_VSLOT(strvec,1));
+		return;
+	}
 	vs->quorum_up = set_check_notify_script(strvec);
 }
 static void
 quorum_down_handler(vector_t *strvec)
 {
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
+	if (vs->quorum_down) {
+		log_message(LOG_INFO, "(%s): quorum_down script already specified - ignoring %s", vs->vsgname, FMT_STR_VSLOT(strvec,1));
+		return;
+	}
 	vs->quorum_down = set_check_notify_script(strvec);
 }
 static void
