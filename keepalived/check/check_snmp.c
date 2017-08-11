acassen/keepalived

commit 346cdd018218ce447aba2e33087deaa9958f756c

Add virtualhost config for real servers
Different real servers may want different virtualhost config
settings. The real server virtualhost setting overrides the
virtual server virtualhost setting.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/check/check_snmp.c
@@ -150,7 +150,8 @@ enum check_snmp_realserver_magic {
 	CHECK_SNMP_RSRATEOUTBPSLOW,
 	CHECK_SNMP_RSRATEOUTBPSHIGH,
 #endif
-	CHECK_SNMP_RSLOADBALANCINGKIND
+	CHECK_SNMP_RSLOADBALANCINGKIND,
+	CHECK_SNMP_RSVIRTUALHOST
 };
 
 #define STATE_VSGM_FWMARK 1
@@ -893,6 +894,10 @@ check_snmp_realserver(struct variable *vp, oid *name, size_t *length,
 		if (!be->notify_down) break;
 		*var_len = strlen(be->notify_down->name);
 		return (u_char*)be->notify_down->name;
+	case CHECK_SNMP_RSVIRTUALHOST:
+		if (!br->virtualhost) break;
+		*var_len = strlen(br->virtualhost);
+		return (u_char*)br->virtualhost;
 	case CHECK_SNMP_RSFAILEDCHECKS:
 		if (btype == STATE_RS_SORRY) break;
 		if (LIST_ISEMPTY(be->failed_checkers))
@@ -1333,6 +1338,8 @@ static struct variable8 check_vars[] = {
 #endif
 	{CHECK_SNMP_RSLOADBALANCINGKIND, ASN_UNSIGNED, RONLY,
 	 check_snmp_realserver, 3, {4, 1, 40}},
+	{CHECK_SNMP_RSVIRTUALHOST, ASN_OCTET_STR, RONLY,
+	 check_snmp_realserver, 3, {4, 1, 41}},
 #ifdef _WITH_VRRP_
 	/* LVS sync daemon configuration */
 	{CHECK_SNMP_LVSSYNCDAEMONENABLED, ASN_INTEGER, RONLY,

doc/keepalived.conf.SYNOPSIS
@@ -560,7 +560,7 @@ virtual_server group <STRING>      {	# VS group declaration
     protocol TCP|UDP|SCTP               # L4 protocol
     ha_suspend				# If VS IP address is not set, suspend
 					#  healthcheckers activity
-    virtualhost <STRING>		# VirtualHost string to use for
+    virtualhost <STRING>		# Default VirtualHost string to use for
 					#  HTTP_GET or SSL_GET
 
     # Assume silently all RSs down and healthchecks
@@ -610,6 +610,9 @@ virtual_server group <STRING>      {	# VS group declaration
 					# Script to launch when
 					#  healthchecker consider service
 					#  as down.
+        virtualhost <STRING>		# Default VirtualHost string to use for
+					#  HTTP_GET or SSL_GET (overrides
+					#  virtual_server virtualhost)
 
         HTTP_GET|SSL_GET {		# HTTP and SSL healthcheckers
             url {			# A set of url to test

doc/man/man5/keepalived.conf.5
@@ -692,8 +692,9 @@ A virtual_server can be a declaration of one of
     lvs_sched   # synonym for lb_algo
     lvs_method  # synonym for lb_kind
 
-    # VirtualHost string for HTTP_GET or SSL_GET
+    # Default VirtualHost string for HTTP_GET or SSL_GET
     # eg virtualhost www.firewall.loc
+    # Overridden by virtualhost config of real server or checker
     virtualhost <STRING>
 
     # On daemon startup assume that all RSs are down
@@ -756,6 +757,11 @@ A virtual_server can be a declaration of one of
 
            uthreshold <INTEGER> # maximum number of connections to server
            lthreshold <INTEGER> # minimum number of connections to server
+
+           # Default VirtualHost string for HTTP_GET or SSL_GET
+           # eg virtualhost www.firewall.loc
+           # Overridden by virtualhost config of a checker
+           virtualhost <STRING>
 
            # pick one healthchecker
            # HTTP_GET|SSL_GET|TCP_CHECK|SMTP_CHECK|DNS_CHECK|MISC_CHECK

keepalived/check/check_data.c
@@ -405,6 +405,7 @@ free_rs(void *data)
 	free_notify_script(&rs->notify_up);
 	free_notify_script(&rs->notify_down);
 	free_list(&rs->failed_checkers);
+	FREE_PTR(rs->virtualhost);
 	FREE(rs);
 }
 static void
@@ -435,6 +436,8 @@ dump_rs(void *data)
 	if (rs->notify_down)
 		log_message(LOG_INFO, "     -> Notify script DOWN = %s, uid:gid %d:%d",
 		       rs->notify_down->name, rs->notify_down->uid, rs->notify_down->gid);
+	if (rs->virtualhost)
+		log_message(LOG_INFO, "   VirtualHost = %s", rs->virtualhost);
 }
 
 static void
@@ -473,6 +476,7 @@ alloc_rs(char *ip, char *port)
 	new->iweight = 1;
 	new->failed_checkers = alloc_list(free_failed_checkers, NULL);
 	new->forwarding_method = vs->forwarding_method;
+	new->virtualhost = NULL;
 
 	if (!LIST_EXISTS(vs->rs))
 		vs->rs = alloc_list(free_rs, dump_rs);

keepalived/check/check_http.c
@@ -598,9 +598,9 @@ http_request_thread(thread_t * thread)
 	request_t *req = HTTP_REQ(http);
 	struct sockaddr_storage *addr = &checker->co->dst;
 	unsigned timeout = checker->co->connection_to;
-	char *vhost = CHECKER_VHOST(checker);
-	char *request_host = 0;
-	char *request_host_port = 0;
+	char *vhost;
+	char *request_host;
+	char request_host_port[7];	/* ":" [0-9][0-9][0-9][0-9][0-9] "\0" */
 	char *str_request;
 	url_t *fetched_url;
 	int ret = 0;
@@ -615,17 +615,21 @@ http_request_thread(thread_t * thread)
 
 	fetched_url = fetch_next_url(http_get_check);
 
+       	if (checker->rs->virtualhost)
+		vhost = checker->rs->virtualhost;
+	else if (checker->vs->virtualhost)
+		vhost = checker->vs->virtualhost;
+	else
+		vhost = NULL;
+
 	if (vhost) {
 		/* If vhost was defined we don't need to override it's port */
 		request_host = vhost;
-		request_host_port = (char*) MALLOC(1);
-		*request_host_port = 0;
+		request_host_port[0] = '\0';
 	} else {
 		request_host = inet_sockaddrtos(addr);
 
-		/* Allocate a buffer for the port string ( ":" [0-9][0-9][0-9][0-9][0-9] "\0" ) */
-		request_host_port = (char*) MALLOC(7);
-		snprintf(request_host_port, 7, ":%d",
+		snprintf(request_host_port, sizeof(request_host_port), ":%d",
 			 ntohs(inet_sockaddrport(addr)));
 	}
 
@@ -638,11 +642,7 @@ http_request_thread(thread_t * thread)
 			fetched_url->path, request_host, request_host_port);
 	}
 
-	FREE(request_host_port);
-
-	DBG("Processing url(%u) of %s.",
-	    http->url_it + 1
-	    , FMT_HTTP_RS(checker));
+	DBG("Processing url(%u) of %s.", http->url_it + 1 , FMT_HTTP_RS(checker));
 
 	/* Set descriptor non blocking */
 	val = fcntl(thread->u.fd, F_GETFL, 0);
@@ -652,16 +652,15 @@ http_request_thread(thread_t * thread)
 	if (http_get_check->proto == PROTO_SSL)
 		ret = ssl_send_request(req->ssl, str_request, (int)strlen(str_request));
 	else
-		ret = (send(thread->u.fd, str_request, strlen(str_request), 0) != -1) ? 1 : 0;
+		ret = (send(thread->u.fd, str_request, strlen(str_request), 0) != -1);
 
 	/* restore descriptor flags */
 	fcntl(thread->u.fd, F_SETFL, val);
 
 	FREE(str_request);
 
-	if (!ret) {
+	if (!ret)
 		return timeout_epilog(thread, "Cannot send get request to");
-	}
 
 	/* Register read timeouted thread */
 	thread_add_read(thread->master, http_response_thread, checker,

keepalived/check/check_parser.c
@@ -261,7 +261,7 @@ hasuspend_handler(__attribute__((unused)) vector_t *strvec)
 }
 
 static void
-virtualhost_handler(vector_t *strvec)
+vs_virtualhost_handler(vector_t *strvec)
 {
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
 	vs->virtualhost = set_value(strvec);
@@ -378,6 +378,13 @@ notify_down_handler(vector_t *strvec)
 	rs->notify_down = set_check_notify_script(strvec);
 }
 static void
+rs_virtualhost_handler(vector_t *strvec)
+{
+	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
+	real_server_t *rs = LIST_TAIL_DATA(vs->rs);
+	rs->virtualhost = set_value(strvec);
+}
+static void
 alpha_handler(__attribute__((unused)) vector_t *strvec)
 {
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
@@ -468,7 +475,7 @@ init_check_keywords(bool active)
 	install_keyword("persistence_granularity", &pgr_handler);
 	install_keyword("protocol", &proto_handler);
 	install_keyword("ha_suspend", &hasuspend_handler);
-	install_keyword("virtualhost", &virtualhost_handler);
+	install_keyword("virtualhost", &vs_virtualhost_handler);
 
 	/* Pool regression detection and handling. */
 	install_keyword("alpha", &alpha_handler);
@@ -491,6 +498,7 @@ init_check_keywords(bool active)
 	install_keyword("inhibit_on_failure", &inhibit_handler);
 	install_keyword("notify_up", &notify_up_handler);
 	install_keyword("notify_down", &notify_down_handler);
+	install_keyword("virtualhost", &rs_virtualhost_handler);
 
 	install_sublevel_end_handler(&vs_end_handler);
 

keepalived/include/check_api.h
@@ -59,7 +59,6 @@ extern list checkers_queue;
 #define CHECKER_VALUE_INT(X) (atoi(vector_slot(X,1)))
 #define CHECKER_VALUE_UINT(X) ((unsigned)strtoul(vector_slot(X,1), NULL, 10))
 #define CHECKER_VALUE_STRING(X) (set_value(X))
-#define CHECKER_VHOST(C) (VHOST((C)->vs))
 #define CHECKER_HA_SUSPEND(C) ((C)->vs->ha_suspend)
 #define CHECKER_NEW_CO() ((conn_opts_t *) MALLOC(sizeof (conn_opts_t)))
 #define FMT_CHK(C) FMT_RS((C)->rs, (C)->vs)

keepalived/include/check_data.h
@@ -79,6 +79,7 @@ typedef struct _real_server {
 	list				failed_checkers;/* List of failed checkers */
 	bool				set;		/* in the IPVS table */
 	bool				reloaded;	/* active state was copied from old config while reloading */
+	char				*virtualhost;	/* Default virtualhost for HTTP and SSL health checkers */
 #if defined(_WITH_SNMP_CHECKER_) && defined(_WITH_LVS_)
 	/* Statistics */
 	uint32_t			activeconns;	/* active connections */
@@ -130,7 +131,8 @@ typedef struct _virtual_server {
 	unsigned			forwarding_method;
 	uint32_t			persistence_granularity;
 #endif
-	char				*virtualhost;
+	char				*virtualhost;	/* Default virtualhost for HTTP and SSL healthcheckers
+							   if not set on real servers */
 	list				rs;
 	bool				alive;
 	bool				alpha;		/* Alpha mode enabled. */
@@ -217,9 +219,8 @@ static inline int inaddr_equal(sa_family_t family, void *addr1, void *addr2)
 
 /* macro utility */
 #define ISALIVE(S)	((S)->alive)
-#define SET_ALIVE(S)	((S)->alive = 1)
-#define UNSET_ALIVE(S)	((S)->alive = 0)
-#define VHOST(V)	((V)->virtualhost)
+#define SET_ALIVE(S)	((S)->alive = true)
+#define UNSET_ALIVE(S)	((S)->alive = false)
 #define FMT_RS(R, V) (inet_sockaddrtotrio (&(R)->addr, (V)->service_type))
 #define FMT_VS(V) (format_vs((V)))
 

commit 387c25be88bab26f0fc87ba156c87fb84797c37b

Fix compiling with SNMP enabled
Commit 346cdd0 introduced a compilation error when using SNMP.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/check/check_snmp.c
@@ -895,9 +895,9 @@ check_snmp_realserver(struct variable *vp, oid *name, size_t *length,
 		*var_len = strlen(be->notify_down->name);
 		return (u_char*)be->notify_down->name;
 	case CHECK_SNMP_RSVIRTUALHOST:
-		if (!br->virtualhost) break;
-		*var_len = strlen(br->virtualhost);
-		return (u_char*)br->virtualhost;
+		if (!be->virtualhost) break;
+		*var_len = strlen(be->virtualhost);
+		return (u_char*)be->virtualhost;
 	case CHECK_SNMP_RSFAILEDCHECKS:
 		if (btype == STATE_RS_SORRY) break;
 		if (LIST_ISEMPTY(be->failed_checkers))