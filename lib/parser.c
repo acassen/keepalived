acassen/keepalived

commit 9458c9b90c6f4ee762dc66136917066ffdc5933e

Use getcwd() malloc functionality if available
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

lib/parser.c
@@ -370,29 +370,34 @@ process_stream(vector_t *keywords_vec, int need_bob)
 	return;
 }
 
-static void
+static bool
 read_conf_file(const char *conf_file)
 {
 	FILE *stream;
 	char *path;
-	int ret;
 	glob_t globbuf;
 	size_t i;
 	int	res;
 	struct stat stb;
+	unsigned num_matches = 0;
 
 	globbuf.gl_offs = 0;
-	res = glob(conf_file, 0, NULL, &globbuf);
+	res = glob(conf_file, GLOB_MARK, NULL, &globbuf);
 
-	if (res == GLOB_NOMATCH) {
-		log_message(LOG_INFO, "No config files matched '%s'.", conf_file);
-		goto done;
-	} else if (res) {
-		log_message(LOG_INFO, "Error reading config file(s): glob(\"%s\") returned %d, skipping.", res);
-		goto done;
+	if (res) {
+		if (res == GLOB_NOMATCH)
+			log_message(LOG_INFO, "No config files matched '%s'.", conf_file);
+		else
+			log_message(LOG_INFO, "Error reading config file(s): glob(\"%s\") returned %d, skipping.", conf_file, res);
+		return true;
 	}
 
-	for(i = 0; i < globbuf.gl_pathc; i++){
+	for (i = 0; i < globbuf.gl_pathc; i++) {
+		if (globbuf.gl_pathv[i][strlen(globbuf.gl_pathv[i])-1] == '/') {
+			/* This is a directory - so skip */
+			continue;
+		}
+
 		log_message(LOG_INFO, "Opening file '%s'.", globbuf.gl_pathv[i]);
 		stream = fopen(globbuf.gl_pathv[i], "r");
 		if (!stream) {
@@ -410,43 +415,52 @@ read_conf_file(const char *conf_file)
 			continue;
 		}
 
+		num_matches++;
+
 		current_stream = stream;
 
-		char prev_path[PATH_MAX];
-		prev_path[0] = '\0';
+		path = NULL;
+#ifdef _GETCWD_NO_MALLOC_
+		char *prev_path = MALLOC(PATH_MAX);
+#endif
 		if (strchr(globbuf.gl_pathv[i], '/')) {
 			/* If the filename contains a directory element, change to that directory */
+#ifdef _GETCWD_NO_MALLOC_
 			path = getcwd(prev_path, PATH_MAX);
-			if (!path) {
-				log_message(LOG_INFO, "getcwd(%s) error (%s)"
-						    , prev_path, strerror(errno));
-			}
+#else
+			path = getcwd(NULL, 0);
+#endif
+			if (!path)
+				log_message(LOG_INFO, "getcwd() error (%s)", strerror(errno));
 
 			char *confpath = strdup(globbuf.gl_pathv[i]);
 			dirname(confpath);
-			ret = chdir(confpath);
-			if (ret < 0) {
-				log_message(LOG_INFO, "chdir(%s) error (%s)"
-						    , confpath, strerror(errno));
-			}
+			if (chdir(confpath) < 0)
+				log_message(LOG_INFO, "chdir(%s) error (%s)", confpath, strerror(errno));
 			free(confpath);
 		}
 
 		process_stream(current_keywords, 0);
 		fclose(stream);
 
-		if (prev_path[0]) {
-			/* If we changed directory, restore the previous directory */
-			ret = chdir(prev_path);
-			if (ret < 0) {
-				log_message(LOG_INFO, "chdir(%s) error (%s)"
-						    , prev_path, strerror(errno));
-			}
+		/* If we changed directory, restore the previous directory */
+		if (path) {
+			if (chdir(path) < 0)
+				log_message(LOG_INFO, "chdir(%s) error (%s)", path, strerror(errno));
+#ifdef _GET_CWD_NO_MALLOC_
+			FREE(prev_path);
+#else
+			free(path);
+#endif
 		}
 	}
 
-done:
 	globfree(&globbuf);
+
+	if (!num_matches)
+		log_message(LOG_INFO, "No config files matched '%s'.", conf_file);
+
+	return false;
 }
 
 bool check_conf_file(const char *conf_file)
@@ -456,33 +470,45 @@ bool check_conf_file(const char *conf_file)
 	bool ret = true;
 	int res;
 	struct stat stb;
+	unsigned num_matches = 0;
 
 	globbuf.gl_offs = 0;
-	res = glob(conf_file, 0, NULL, &globbuf);
+	res = glob(conf_file, GLOB_MARK, NULL, &globbuf);
 	if (res) {
 		log_message(LOG_INFO, "Unable to find configuration file %s (glob returned %d)", conf_file, res);
 		return false;
 	}
 
-	if (globbuf.gl_pathc == 0) {
-		log_message(LOG_INFO, "Unable to find configuration file %s", conf_file);
-		ret = false;
-	} else {
-		for (i = 0; i < globbuf.gl_pathc; i++) {
-			if (access(globbuf.gl_pathv[i], R_OK)) {
-				log_message(LOG_INFO, "Unable to read configuration file %s", globbuf.gl_pathv[i]);
-				ret = false;
-				break;
-			}
+	for (i = 0; i < globbuf.gl_pathc; i++) {
+		if (globbuf.gl_pathv[i][strlen(globbuf.gl_pathv[i])-1] == '/') {
+			/* This is a directory - so skip */
+			continue;
+		}
 
-			/* Make sure that the file is a regular file, and not for example a directory or executable */
-			if (stat(globbuf.gl_pathv[i], &stb) ||
-			    !S_ISREG(stb.st_mode) ||
-			     (stb.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH))) {
-				log_message(LOG_INFO, "Configuration file '%s' is not a regular non-executable file", globbuf.gl_pathv[i]);
-				ret = false;
-				break;
-			}
+		if (access(globbuf.gl_pathv[i], R_OK)) {
+			log_message(LOG_INFO, "Unable to read configuration file %s", globbuf.gl_pathv[i]);
+			ret = false;
+			break;
+		}
+
+		/* Make sure that the file is a regular file, and not for example a directory or executable */
+		if (stat(globbuf.gl_pathv[i], &stb) ||
+		    !S_ISREG(stb.st_mode) ||
+		     (stb.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH))) {
+			log_message(LOG_INFO, "Configuration file '%s' is not a regular non-executable file", globbuf.gl_pathv[i]);
+			ret = false;
+			break;
+		}
+
+		num_matches++;
+	}
+
+	if (ret) {
+	       	if (num_matches > 1)
+			log_message(LOG_INFO, "WARNING, more than one file matches configuration file %s, using %s", conf_file, globbuf.gl_pathv[0]);
+		else if (num_matches == 0) {
+			log_message(LOG_INFO, "Unable to find configuration file %s", conf_file);
+			ret = false;
 		}
 	}
 
@@ -649,7 +675,6 @@ alloc_value_block(void (*alloc_func) (vector_t *))
 
 			free_strvec(vec);
 		}
-		memset(buf, 0, MAXBUF);
 	}
 	FREE(buf);
 }

configure
@@ -6514,6 +6514,44 @@ fi
 done
 
 
+{ $as_echo "$as_me:${as_lineno-$LINENO}: checking getcwd(NULL,0) does malloc" >&5
+$as_echo_n "checking getcwd(NULL,0) does malloc... " >&6; }
+if test "$cross_compiling" = yes; then :
+  { { $as_echo "$as_me:${as_lineno-$LINENO}: error: in \`$ac_pwd':" >&5
+$as_echo "$as_me: error: in \`$ac_pwd':" >&2;}
+as_fn_error $? "cannot run test program while cross compiling
+See \`config.log' for more details" "$LINENO" 5; }
+else
+  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+    #include <unistd.h>
+
+    int main(int argc, char **argv)
+    {
+      exit (!getcwd(NULL, 0));
+    }
+
+_ACEOF
+if ac_fn_c_try_run "$LINENO"; then :
+  { $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+$as_echo "yes" >&6; }
+else
+
+    { $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+$as_echo "no" >&6; }
+
+$as_echo "#define _GETCWD_NO_MALLOC_  1 " >>confdefs.h
+
+    BUILD_OPTIONS="$BUILD_OPTIONS GETCWD_NO_MALLOC"
+
+fi
+rm -f core *.core core.conftest.* gmon.out bb.out conftest$ac_exeext \
+  conftest.$ac_objext conftest.beam conftest.$ac_ext
+fi
+
+CPPFLAGS="$SAV_CPPFLAGS"
+
 if test -n "$enable_dynamic_linking"; then
   enable_libiptc_dynamic=$enable_dynamic_linking
   enable_libipset_dynamic=$enable_dynamic_linking

configure.ac
@@ -374,6 +374,24 @@ AC_CHECK_FUNCS([dup2 getcwd gettimeofday memmove memset select setenv socket str
 dnl - pipe2() since Linux 2.6.27 and glibc 2.9.
 AC_CHECK_FUNCS([pipe2], [add_build_opt([PIPE2])])
 
+dnl ---[ Does whether getcwd(NULL, n) do malloc? ]---
+AC_MSG_CHECKING([getcwd(NULL,0) does malloc])
+AC_RUN_IFELSE([AC_LANG_SOURCE([[
+    #include <unistd.h>
+
+    int main(int argc, char **argv)
+    {
+      exit (!getcwd(NULL, 0));
+    }
+  ]])],
+  [AC_MSG_RESULT([yes])],
+  [
+    AC_MSG_RESULT([no])
+    AC_DEFINE([_GETCWD_NO_MALLOC_], [ 1 ], [Define to 1 if getcwd(NULL,n) does not malloc])
+    add_build_opt([GETCWD_NO_MALLOC])
+  ])
+CPPFLAGS="$SAV_CPPFLAGS"
+
 dnl - Do we want to override dynamic/static linking?
 if test -n "$enable_dynamic_linking"; then
   enable_libiptc_dynamic=$enable_dynamic_linking

lib/config.h.in
@@ -520,6 +520,9 @@
 /* Define to 1 to build with debugging support */
 #undef _DEBUG_
 
+/* Define to 1 if getcwd(NULL,n) does not malloc */
+#undef _GETCWD_NO_MALLOC_
+
 /* Define to 1 if have FIB routing support */
 #undef _HAVE_FIB_ROUTING_
 

commit 645abb74ea67188f978ce3acae29d217240bb110

Fix whitespace error introduced in commit 9458c9b9
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

lib/parser.c
@@ -53,7 +53,7 @@ static int skip_sublevel = 0;
 
 static char *
 null_strvec(const vector_t *strvec, size_t index)
-{ 
+{
 	if (index - 1 < vector_size(strvec) && index > 0 && vector_slot(strvec, index - 1))
 		log_message(LOG_INFO, "*** Configuration line starting `%s` is missing a parameter after keyword `%s` at word position %zu", vector_slot(strvec, 0) ? (char *)vector_slot(strvec, 0) : "***MISSING ***", (char *)vector_slot(strvec, index - 1), index + 1);
 	else
@@ -62,7 +62,7 @@ null_strvec(const vector_t *strvec, size_t index)
 	exit(KEEPALIVED_EXIT_CONFIG);
 
 	return NULL;
-} 
+}
 
 static void
 keyword_alloc(vector_t *keywords_vec, const char *string, void (*handler) (vector_t *), bool active)
@@ -489,7 +489,7 @@ bool check_conf_file(const char *conf_file)
 	}
 
 	if (ret) {
-	       	if (num_matches > 1)
+		if (num_matches > 1)
 			log_message(LOG_INFO, "WARNING, more than one file matches configuration file %s, using %s", conf_file, globbuf.gl_pathv[0]);
 		else if (num_matches == 0) {
 			log_message(LOG_INFO, "Unable to find configuration file %s", conf_file);