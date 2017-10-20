dad3180c5fa41d1ad6720dd49703fb3af4cc4474
Damien Clabaut <damien.clabaut@corp.ovh.com>
2017-10-20 11:53:02 -0400

    Flake8 compliance

--- tools/json_tracking/make_conf.py
+++ tools/json_tracking/make_conf.py
@@ -12,7 +12,6 @@
 #
 #####
 
-import argparse
 import jinja2
 
 # Configuration
@@ -28,6 +27,7 @@ keepalived_offset_folder = "/etc/keepalived_offset/"
 # Path to the output file
 output_file = "keepalived.conf"
 
+
 class Vlan:
     def __init__(self, name, ip4addr, ip4net, ip6addr):
         self.name = name
@@ -35,19 +35,20 @@ class Vlan:
         self.ip4net = ip4net
         self.ip6addr = ip6addr
 
+
 vlans = []
-for vlan_id in range(first_vlan, last_vlan +1):
+for vlan_id in range(first_vlan, last_vlan + 1):
     ip4addr = "10.0." + str(vlan_id) + ".254"
     ip4net = "10.0." + str(vlan_id) + ".0/24"
     ip6addr = "fd00:42:ffff:" + format(vlan_id, '02x') + ":ff:ff:ff:ff/64"
-    new_vlan = Vlan(vlan_id, ip4addr, ip4net, ip6addr) 
+    new_vlan = Vlan(vlan_id, ip4addr, ip4net, ip6addr)
     vlans.append(new_vlan)
 
 to_return = ""
-templateLoader = jinja2.FileSystemLoader( searchpath="./" )
-templateEnv = jinja2.Environment( loader=templateLoader )
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
 vrrpconf = templateEnv.get_template('template_vrrp_instance').render(
-                vlans=vlans,track_files=keepalived_offset_folder
+                vlans=vlans, track_files=keepalived_offset_folder
                 ) + "\n"
 
 output_file = open(output_file, "w")
