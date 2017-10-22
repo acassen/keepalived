d8db888e1d99a99a8af8578db1dcae8bc8747f6a
Quentin Armitage <quentin@armitage.org.uk>
2017-10-22 09:37:35 +0100

    Log a helpful message i using mem-check and too many allocs
    keepalived simply being terminated by SIGABRT with no diagnostic
    message was unhelpful.
    
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- lib/memory.c
+++ lib/memory.c
@@ -144,7 +144,10 @@ keepalived_malloc(size_t size, char *file, char *function, int line)
 	if (i == number_alloc_list)
 		number_alloc_list++;
 
-	assert(number_alloc_list < MAX_ALLOC_LIST);
+	if (number_alloc_list >= MAX_ALLOC_LIST) {
+		log_message(LOG_INFO, "number_alloc_list = %d exceeds MAX_ALLOC_LIST. Please increase value in lib/memory.h", number_alloc_list);
+		assert(number_alloc_list < MAX_ALLOC_LIST);
+	}
 
 	alloc_list[i].ptr = buf;
 	alloc_list[i].size = size;
