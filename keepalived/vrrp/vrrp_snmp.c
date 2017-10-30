acassen/keepalived

commit c0f8c1a69613b2cd72b27eceb3e6aa6fa5041bda

Rationalise reporting of child process exit status
report_child_status() is now only called in the main keepalived
program. The reporting of the exit status of vrrp track scripts
and MISC_CHECK scripts is now handled in the specific code for
those scripts. This means that non 0 exit statuses aren't
repeatedly reported for vrrp track scripts.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_snmp.c
@@ -490,12 +490,12 @@ vrrp_snmp_script(struct variable *vp, oid *name, size_t *length,
 		long_ret.s = scr->weight;
 		return (u_char *)&long_ret;
 	case VRRP_SNMP_SCRIPT_RESULT:
-		switch (scr->result) {
+		switch (scr->init_state) {
 		case VRRP_SCRIPT_STATUS_INIT:
 			long_ret.u = 1; break;
-		case VRRP_SCRIPT_STATUS_INIT_GOOD:
+		case VRRP_SCRIPT_STATUS_GOOD:
 			long_ret.u = 4; break;
-		case VRRP_SCRIPT_STATUS_INIT_FAILED:
+		case VRRP_SCRIPT_STATUS_FAILED:
 			long_ret.u = 5; break;
 		case VRRP_SCRIPT_STATUS_DISABLED:
 			long_ret.u = 0; break;

keepalived/check/check_misc.c
@@ -298,6 +298,8 @@ misc_check_child_thread(thread_t * thread)
 	int timeout = 0;
 	char *script_exit_type = NULL;
 	bool script_success;
+	char *reason = NULL;
+	int reason_code;
 
 	checker = THREAD_ARG(thread);
 	misck_checker = CHECKER_ARG(checker);
@@ -348,9 +350,10 @@ misc_check_child_thread(thread_t * thread)
 					       checker->rs, true);
 
 			/* everything is good */
-			if (!checker->is_up) {
+			if (!checker->is_up || !misck_checker->initial_state_reported) {
 				script_exit_type = "succeeded";
 				script_success = true;
+				misck_checker->initial_state_reported = false;
 			}
 
 			checker->retry_it = 0;
@@ -360,6 +363,8 @@ misc_check_child_thread(thread_t * thread)
 			else {
 				script_exit_type = "failed";
 				script_success = false;
+				reason = "exited with status";
+				reason_code = status;
 
 				checker->retry_it = 0;
 			}
@@ -375,13 +380,21 @@ misc_check_child_thread(thread_t * thread)
 	        }
 
 		/* We treat forced termination as a failure */
-		if ((misck_checker->state == SCRIPT_STATE_REQUESTING_TERMINATION ||
-		     misck_checker->state == SCRIPT_STATE_FORCING_TERMINATION) &&
-		    checker->is_up) {
+		if (checker->is_up) {
 			if (checker->retry_it < checker->retry)
 				checker->retry_it++;
 			else {
-				script_exit_type = "timed out";
+				if ((misck_checker->state == SCRIPT_STATE_REQUESTING_TERMINATION &&
+				     WTERMSIG(wait_status) == SIGTERM) ||
+				    (misck_checker->state == SCRIPT_STATE_FORCING_TERMINATION &&
+				     (WTERMSIG(wait_status) == SIGTERM || WTERMSIG(wait_status) == SIGKILL)))
+					script_exit_type = "timed out";
+				else {
+					script_exit_type = "failed";
+					reason = "due to signal";
+					reason_code = WTERMSIG(wait_status);
+				}
+
 				script_success = false;
 
 				checker->retry_it = 0;
@@ -392,10 +405,19 @@ misc_check_child_thread(thread_t * thread)
 	if (script_exit_type) {
 		char message[40];
 
-		log_message(LOG_INFO, "Misc check to [%s] for [%s] %s."
-				    , inet_sockaddrtos(&checker->rs->addr)
-				    , misck_checker->path
-				    , script_exit_type);
+		if (reason)
+			log_message(LOG_INFO, "Misc check to [%s] for [%s] %s (%s %d)."
+					    , inet_sockaddrtos(&checker->rs->addr)
+					    , misck_checker->path
+					    , script_exit_type
+					    , reason
+					    , reason_code);
+		else
+			log_message(LOG_INFO, "Misc check to [%s] for [%s] %s."
+					    , inet_sockaddrtos(&checker->rs->addr)
+					    , misck_checker->path
+					    , script_exit_type);
+
 		snprintf(message, sizeof(message), "=> MISC CHECK %s on service <=", script_exit_type);
 		smtp_alert(checker, NULL, NULL,
 			   script_success ? "UP " : "DOWN", message);

keepalived/include/check_misc.h
@@ -42,6 +42,7 @@ typedef struct _misc_checker {
 	uid_t			uid;		/* uid for script execution */
 	gid_t			gid;		/* gid for script execution */
 	bool			insecure;	/* script is insecure */
+	bool			initial_state_reported;
 	timeval_t		last_ran;	/* Time script last ran */
 } misc_checker_t;
 

keepalived/include/vrrp_track.h
@@ -53,10 +53,6 @@
  * success, we increase result and set it to rise+fall-1 when we pass above
  * rise-1.
  */
-#define VRRP_SCRIPT_STATUS_DISABLED    -4
-#define VRRP_SCRIPT_STATUS_INIT_FAILED -3
-#define VRRP_SCRIPT_STATUS_INIT_GOOD   -2
-#define VRRP_SCRIPT_STATUS_INIT        -1
 
 /* external script we call to track local processes */
 typedef struct _vrrp_script {
@@ -70,6 +66,7 @@ typedef struct _vrrp_script {
 	int			rise;		/* R: how many successes before OK */
 	int			fall;		/* F: how many failures before KO */
 	script_state_t		state;		/* current state of script */
+	script_init_state_t	init_state;	/* current initialisation state of script */
 	uid_t			uid;		/* uid to run script as */
 	gid_t			gid;		/* gid to run script as */
 	bool			insecure;	/* Set if script is run by root, but is non-root modifiable */

keepalived/vrrp/vrrp.c
@@ -2924,7 +2924,7 @@ clear_diff_script(void)
 			if (nvscript) {
 				log_message(LOG_INFO, "VRRP_Script(%s) considered successful on reload",
 					   nvscript->sname);
-				nvscript->result = VRRP_SCRIPT_STATUS_INIT_GOOD;
+				nvscript->init_state = SCRIPT_INIT_STATE_GOOD;
 			}
 		}
 	}

keepalived/vrrp/vrrp_data.c
@@ -147,14 +147,14 @@ dump_vscript(void *data)
 	log_message(LOG_INFO, "   Fall = %d", vscript->fall);
 	log_message(LOG_INFO, "   Insecure = %s", vscript->insecure ? "yes" : "no");
 
-	switch (vscript->result) {
-	case VRRP_SCRIPT_STATUS_INIT:
+	switch (vscript->init_state) {
+	case SCRIPT_INIT_STATE_INIT:
 		str = "INIT"; break;
-	case VRRP_SCRIPT_STATUS_INIT_GOOD:
+	case SCRIPT_INIT_STATE_GOOD:
 		str = "INIT/GOOD"; break;
-	case VRRP_SCRIPT_STATUS_INIT_FAILED:
+	case SCRIPT_INIT_STATE_FAILED:
 		str = "INIT/FAILED"; break;
-	case VRRP_SCRIPT_STATUS_DISABLED:
+	case SCRIPT_INIT_STATE_DISABLED:
 		str = "DISABLED"; break;
 	default:
 		str = (vscript->result >= vscript->rise) ? "GOOD" : "BAD";
@@ -573,7 +573,7 @@ alloc_vrrp_script(char *sname)
 	new->interval = VRRP_SCRIPT_DI * TIMER_HZ;
 	new->timeout = VRRP_SCRIPT_DT * TIMER_HZ;
 	new->weight = VRRP_SCRIPT_DW;
-	new->result = VRRP_SCRIPT_STATUS_INIT;
+	new->init_state = SCRIPT_INIT_STATE_INIT;
 	new->state = SCRIPT_STATE_IDLE;
 	new->inuse = 0;
 	new->rise = 1;

keepalived/vrrp/vrrp_parser.c
@@ -787,7 +787,7 @@ static void
 vrrp_vscript_init_fail_handler(__attribute__((unused)) vector_t *strvec)
 {
 	vrrp_script_t *vscript = LIST_TAIL_DATA(vrrp_data->vrrp_script);
-	vscript->result = VRRP_SCRIPT_STATUS_INIT_FAILED;
+	vscript->init_state = SCRIPT_INIT_STATE_FAILED;
 }
 static void
 vrrp_version_handler(vector_t *strvec)

keepalived/vrrp/vrrp_print.c
@@ -94,14 +94,14 @@ vscript_print(FILE *file, void *data)
 	fprintf(file, "   Insecure = %s\n", vscript->insecure ? "yes" : "no");
 	fprintf(file, "   uid:gid = %d:%d\n", vscript->uid, vscript->gid);
 
-	switch (vscript->result) {
-	case VRRP_SCRIPT_STATUS_INIT:
+	switch (vscript->init_state) {
+	case SCRIPT_INIT_STATE_INIT:
 		str = "INIT"; break;
-	case VRRP_SCRIPT_STATUS_INIT_GOOD:
+	case SCRIPT_INIT_STATE_GOOD:
 		str = "INIT/GOOD"; break;
-	case VRRP_SCRIPT_STATUS_INIT_FAILED:
+	case SCRIPT_INIT_STATE_FAILED:
 		str = "INIT/FAILED"; break;
-	case VRRP_SCRIPT_STATUS_DISABLED:
+	case SCRIPT_INIT_STATE_DISABLED:
 		str = "DISABLED"; break;
 	default:
 		str = (vscript->result >= vscript->rise) ? "GOOD" : "BAD";

commit 14bfba16594d1f329932b16b653fbacabe6d64c4

Fix compiling with SNMP enabled, broken by commit c0f8c1a
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_snmp.c
@@ -491,13 +491,13 @@ vrrp_snmp_script(struct variable *vp, oid *name, size_t *length,
 		return (u_char *)&long_ret;
 	case VRRP_SNMP_SCRIPT_RESULT:
 		switch (scr->init_state) {
-		case VRRP_SCRIPT_STATUS_INIT:
+		case SCRIPT_INIT_STATE_INIT:
 			long_ret.u = 1; break;
-		case VRRP_SCRIPT_STATUS_GOOD:
+		case SCRIPT_INIT_STATE_GOOD:
 			long_ret.u = 4; break;
-		case VRRP_SCRIPT_STATUS_FAILED:
+		case SCRIPT_INIT_STATE_FAILED:
 			long_ret.u = 5; break;
-		case VRRP_SCRIPT_STATUS_DISABLED:
+		case SCRIPT_INIT_STATE_DISABLED:
 			long_ret.u = 0; break;
 		default:
 			long_ret.u = (scr->result >= scr->rise) ? 3 : 2;