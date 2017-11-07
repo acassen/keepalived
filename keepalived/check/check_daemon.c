d2c73247be46361b5c1c5b0aad8109d1baf5ea17
Quentin Armitage <quentin@armitage.org.uk>
2017-11-07 00:41:05 +0000

    Stop reporting vrrp/checker child didn't terminate after seg fault
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/check/check_daemon.c
+++ keepalived/check/check_daemon.c
@@ -296,6 +296,7 @@ check_respawn_thread(thread_t * thread)
 		start_check_child();
 	} else {
 		log_message(LOG_ALERT, "Healthcheck child process(%d) died: Exiting", pid);
+		checkers_child = 0;
 		raise(SIGTERM);
 	}
 	return 0;
