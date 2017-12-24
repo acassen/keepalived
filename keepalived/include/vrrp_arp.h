2ce59ad9baed84536cb6be51d55864ea06dc4f81
Quentin Armitage <quentin@armitage.org.uk>
2017-12-24 14:13:52 +0000

    Use system header files struct ether_arp rather than local arphdr_t
    keepalived was using a locally defined structure for an arp header
    rather than using the system headers provided structure.
    
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/include/vrrp_arp.h
+++ keepalived/include/vrrp_arp.h
@@ -25,30 +25,11 @@
 
 /* system includes */
 #include <sys/types.h>
-#include <netinet/if_ether.h>	/* musl libc has name collisions if use <linux/if_ether.h> */
 
 /* local includes */
 #include "vrrp.h"
 #include "vrrp_if.h"
 #include "vrrp_ipaddress.h"
-
-/* local definitions */
-#define IPPROTO_ADDR_LEN	4
-
-/* types definition */
-typedef struct _arphdr {
-	unsigned short int	ar_hrd;	/* Format of hardware address.  */
-	unsigned short int	ar_pro;	/* Format of protocol address.  */
-	unsigned char		ar_hln;	/* Length of hardware address.  */
-	unsigned char		ar_pln;	/* Length of protocol address.  */
-	unsigned short int	ar_op;	/* ARP opcode (command).  */
-
-	/* Ethernet looks like this : This bit is variable sized however...  */
-	unsigned char		__ar_sha[ETH_ALEN];	/* Sender hardware address.  */
-	unsigned char		__ar_sip[4];		/* Sender IP address.  */
-	unsigned char		__ar_tha[ETH_ALEN];	/* Target hardware address.  */
-	unsigned char		__ar_tip[4];		/* Target IP address.  */
-} arphdr_t;
 
 /* prototypes */
 extern void gratuitous_arp_init(void);
