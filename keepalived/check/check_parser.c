acassen/keepalived

commit fecfe5af99137b886bf54a613a29cee6c2b6a958

Improve persistence handling
Properly support persistence_granularity for IPv6.

Set persistence_timeout default if granularity specified.

Only support persistence engine if supported by the kernel.

This commit also changes variables timeout_persistence and
granularity_persistence to persistence_timeout and
granularity_timeout.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/check/check_parser.c
@@ -144,7 +144,7 @@ pto_handler(vector_t *strvec)
 	unsigned long timeout;
 
 	if (vector_size(strvec) < 2) {
-		vs->timeout_persistence = IPVS_SVC_PERSISTENT_TIMEOUT;
+		vs->persistence_timeout = IPVS_SVC_PERSISTENT_TIMEOUT;
 		return;
 	}
 
@@ -155,8 +155,9 @@ pto_handler(vector_t *strvec)
 		return;
 	}
 
-	vs->timeout_persistence = timeout;
+	vs->persistence_timeout = timeout;
 }
+#ifdef IPVS_SVC_ATTR_PE_NAME
 static void
 pengine_handler(vector_t *strvec)
 {
@@ -167,16 +168,25 @@ pengine_handler(vector_t *strvec)
 	strncpy(vs->pe_name, str, size - 1);
 	vs->pe_name[size - 1] = '\0';
 }
+#endif
 static void
 pgr_handler(vector_t *strvec)
 {
+	struct in_addr addr;
+
 	virtual_server_t *vs = LIST_TAIL_DATA(check_data->vs);
 	if (vs->addr.ss_family == AF_INET6)
-		vs->granularity_persistence = atoi(vector_slot(strvec, 1));
-	else
-		inet_ston(vector_slot(strvec, 1), &vs->granularity_persistence);
-	if (!vs->timeout_persistence)
-		vs->timeout_persistence = IPVS_SVC_PERSISTENT_TIMEOUT;
+		vs->persistence_granularity = atoi(vector_slot(strvec, 1));
+	else {
+		if (inet_aton(vector_slot(strvec, 1), &addr)) {
+			log_message(LOG_INFO, "Invalid persistence_timeout specified - %s", FMT_STR_VSLOT(strvec, 1));
+			return;
+		}
+		vs->persistence_granularity = addr.s_addr;
+	}
+
+	if (!vs->persistence_timeout)
+		vs->persistence_timeout = IPVS_SVC_PERSISTENT_TIMEOUT;
 }
 static void
 proto_handler(vector_t *strvec)
@@ -348,7 +358,9 @@ init_check_keywords(bool active)
 	install_keyword("lvs_sched", &lbalgo_handler);
 	install_keyword("lb_kind", &lbkind_handler);
 	install_keyword("lvs_method", &lbkind_handler);
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	install_keyword("persistence_engine", &pengine_handler);
+#endif
 	install_keyword("persistence_timeout", &pto_handler);
 	install_keyword("persistence_granularity", &pgr_handler);
 	install_keyword("protocol", &proto_handler);

doc/KEEPALIVED-MIB
@@ -22,12 +22,14 @@ IMPORTS
         FROM SNMPv2-TC;
 
 keepalived MODULE-IDENTITY
-     LAST-UPDATED "201608030000Z"
+     LAST-UPDATED "201608230000Z"
      ORGANIZATION "Keepalived"
      CONTACT-INFO "http://www.keepalived.org"
      DESCRIPTION
         "This MIB describes objects used by keepalived, both
          for VRRP and health checker."
+     REVISION "201608230000Z"
+     DESCRIPTION "add IPv6 persistence granularity"
      REVISION "201608030000Z"
      DESCRIPTION "net-namespace added"
      REVISION "201607260000Z"
@@ -2248,7 +2250,8 @@ VirtualServerEntry ::= SEQUENCE {
     virtualServerRateInBPSLow Unsigned32,
     virtualServerRateInBPSHigh Unsigned32,
     virtualServerRateOutBPSLow Unsigned32,
-    virtualServerRateOutBPSHigh Unsigned32
+    virtualServerRateOutBPSHigh Unsigned32,
+    virtualServerPersistGranularity6 Unsigned32
 }
 
 virtualServerIndex OBJECT-TYPE
@@ -2392,7 +2395,7 @@ virtualServerPersistGranularity OBJECT-TYPE
     MAX-ACCESS read-only
     STATUS current
     DESCRIPTION
-        "Netmask specifying the granularity of the persistence mechanism."
+        "Netmask specifying the granularity of the IPv4 persistence mechanism."
     ::= { virtualServerEntry 15 }
 
 virtualServerDelayLoop OBJECT-TYPE
@@ -2709,6 +2712,15 @@ virtualServerRateOutBPSHigh OBJECT-TYPE
         "Current outgoing rate for this virtual server.
 	Together with virtualServerRateOutBPSLow composes 64-bit number."
     ::= { virtualServerEntry 50 }
+
+virtualServerPersistGranularity6 OBJECT-TYPE
+    SYNTAX Unsigned32
+    MAX-ACCESS read-only
+    STATUS current
+    DESCRIPTION
+        "Netmask specifying the granularity of the IPv6 persistence mechanism."
+    ::= { virtualServerEntry 51 }
+
 
 -- real servers
 
@@ -3624,7 +3636,8 @@ virtualServerGroup OBJECT-GROUP
     virtualServerRateInBPSLow,
     virtualServerRateInBPSHigh,
     virtualServerRateOutBPSLow,
-    virtualServerRateOutBPSHigh
+    virtualServerRateOutBPSHigh,
+    virtualServerPersistGranularity6
     }
     STATUS current
     DESCRIPTION

keepalived/check/check_data.c
@@ -186,11 +186,16 @@ dump_vs(void *data)
 						   vs->delay_loop,
 	       vs->sched);
 	log_message(LOG_INFO, "   One packet scheduling = %sabled%s", vs->ops ? "en" : "dis", (vs->ops && vs->service_type != IPPROTO_UDP) ? " (inactive due to not UDP)" : "");
-	if (vs->timeout_persistence)
-		log_message(LOG_INFO, "   persistence timeout = %u", vs->timeout_persistence);
-	if (vs->granularity_persistence)
-		log_message(LOG_INFO, "   persistence granularity = %s",
-		       inet_ntop2(vs->granularity_persistence));
+	if (vs->persistence_timeout)
+		log_message(LOG_INFO, "   persistence timeout = %u", vs->persistence_timeout);
+	if (vs->persistence_granularity) {
+		if (vs->addr.ss_family == AF_INET6)
+			log_message(LOG_INFO, "   persistence granularity = %d",
+				       vs->persistence_granularity);
+		else
+			log_message(LOG_INFO, "   persistence granularity = %s",
+				       inet_ntop2(vs->persistence_granularity));
+	}
 	if (vs->service_type == IPPROTO_TCP)
 		log_message(LOG_INFO, "   protocol = TCP");
 	else if (vs->service_type == IPPROTO_UDP)

keepalived/check/check_snmp.c
@@ -63,6 +63,7 @@ enum check_snmp_virtualserver_magic {
 	CHECK_SNMP_VSPERSIST,
 	CHECK_SNMP_VSPERSISTTIMEOUT,
 	CHECK_SNMP_VSPERSISTGRANULARITY,
+	CHECK_SNMP_VSPERSISTGRANULARITY6,
 	CHECK_SNMP_VSDELAYLOOP,
 	CHECK_SNMP_VSHASUSPEND,
 	CHECK_SNMP_VSOPS,
@@ -99,7 +100,7 @@ enum check_snmp_virtualserver_magic {
 	CHECK_SNMP_VSRATEINBPSLOW,
 	CHECK_SNMP_VSRATEINBPSHIGH,
 	CHECK_SNMP_VSRATEOUTBPSLOW,
-	CHECK_SNMP_VSRATEOUTBPSHIGH
+	CHECK_SNMP_VSRATEOUTBPSHIGH,
 #endif
 #endif
 };
@@ -471,16 +472,20 @@ check_snmp_virtualserver(struct variable *vp, oid *name, size_t *length,
 		*var_len = strlen(v->virtualhost);
 		return (u_char*)v->virtualhost;
 	case CHECK_SNMP_VSPERSIST:
-		long_ret = (v->timeout_persistence)?1:2;
+		long_ret = (v->persistence_timeout)?1:2;
 		return (u_char*)&long_ret;
 	case CHECK_SNMP_VSPERSISTTIMEOUT:
-		if (!v->timeout_persistence) break;
-		long_ret = v->timeout_persistence;
+		if (!v->persistence_timeout) break;
+		long_ret = v->persistence_timeout;
 		return (u_char*)&long_ret;
 	case CHECK_SNMP_VSPERSISTGRANULARITY:
-		if (!v->granularity_persistence) break;
-		*var_len = sizeof(v->granularity_persistence);
-		return (u_char*)&v->granularity_persistence;
+		if (!v->persistence_granularity || v->addr.ss_family == AF_INET6) break;
+		*var_len = sizeof(v->persistence_granularity);
+		return (u_char*)&v->persistence_granularity;
+	case CHECK_SNMP_VSPERSISTGRANULARITY6:
+		if (!v->persistence_granularity || v->addr.ss_family == AF_INET) break;
+		*var_len = sizeof(v->persistence_granularity);
+		return (u_char*)&v->persistence_granularity;
 	case CHECK_SNMP_VSDELAYLOOP:
 		if (v->delay_loop >= TIMER_MAX_SEC)
 			long_ret = v->delay_loop/TIMER_HZ;
@@ -1212,6 +1217,8 @@ static struct variable8 check_vars[] = {
 	 check_snmp_virtualserver, 3, {3, 1, 50}},
 #endif
 #endif
+	{CHECK_SNMP_VSPERSISTGRANULARITY6, ASN_UNSIGNED, RONLY,
+	 check_snmp_virtualserver, 3, {3, 1, 51}},
 	/* realServerTable */
 	{CHECK_SNMP_RSTYPE, ASN_INTEGER, RONLY,
 	 check_snmp_realserver, 3, {4, 1, 2}},

keepalived/check/ipvswrapper.c
@@ -410,20 +410,21 @@ ipvs_set_rule(int cmd, virtual_server_t * vs, real_server_t * rs)
 	srule->user.netmask = (vs->addr.ss_family == AF_INET6) ? 128 : ((u_int32_t) 0xffffffff);
 	srule->user.protocol = vs->service_type;
 
-	srule->user.timeout = vs->timeout_persistence;
+	srule->user.timeout = vs->persistence_timeout;
+	if (cmd == IP_VS_SO_SET_ADD || cmd == IP_VS_SO_SET_DEL)
+		if (vs->persistence_granularity)
+			srule->user.netmask = vs->persistence_granularity;
 
-	if (vs->timeout_persistence || vs->granularity_persistence)
+	if (vs->persistence_timeout || vs->persistence_granularity)
 		srule->user.flags |= IP_VS_SVC_F_PERSISTENT;
 
 	/* Only for UDP services */
 	if (vs->ops == 1 && srule->user.protocol == IPPROTO_UDP)
 		srule->user.flags |= IP_VS_SVC_F_ONEPACKET;
 
-	if (cmd == IP_VS_SO_SET_ADD || cmd == IP_VS_SO_SET_DEL)
-		if (vs->granularity_persistence)
-			srule->user.netmask = vs->granularity_persistence;
-
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	strcpy(srule->pe_name, vs->pe_name);
+#endif
 
 	/* SVR specific */
 	if (rs) {

keepalived/check/libipvs.c
@@ -83,7 +83,7 @@ static struct nla_policy ipvs_service_policy[IPVS_SVC_ATTR_MAX + 1] = {
 	[IPVS_SVC_ATTR_TIMEOUT]		= { .type = NLA_U32 },
 	[IPVS_SVC_ATTR_NETMASK]		= { .type = NLA_U32 },
 	[IPVS_SVC_ATTR_STATS]		= { .type = NLA_NESTED },
-#ifdef IPVS_SVR_ATTR_PE_NAME		/* Since Linux 2.6.37 */
+#ifdef IPVS_SVC_ATTR_PE_NAME		/* Since Linux 2.6.37 */
 	[IPVS_SVC_ATTR_PE_NAME]		= { .type = NLA_STRING,
 					    .maxlen = IP_VS_PENAME_MAXLEN }
 #endif
@@ -146,14 +146,21 @@ static struct nla_policy ipvs_stats_policy[IPVS_STATS_ATTR_MAX + 1] = {
 	{ errno = EAFNOSUPPORT; goto out_err; }			\
 	s->user.addr = s->nf_addr.ip;				\
 
+#ifdef IPVS_SVC_ATTR_PE_NAME
 #define CHECK_PE(s, ret) if (s->pe_name[0])			\
 	{ errno = EAFNOSUPPORT; goto out_err; }
+#endif
 
 #define CHECK_COMPAT_DEST(s, ret) CHECK_IPV4(s, ret)
 
+#ifdef IPVS_SVC_ATTR_PE_NAME
 #define CHECK_COMPAT_SVC(s, ret)				\
 	CHECK_IPV4(s, ret);					\
 	CHECK_PE(s, ret);
+#else
+#define CHECK_COMPAT_SVC(s, ret)				\
+	CHECK_IPV4(s, ret);
+#endif
 
 #ifdef LIBIPVS_USE_NL
 #ifndef FALLBACK_LIBNL1
@@ -313,8 +320,10 @@ static int ipvs_nl_fill_service_attr(struct nl_msg *msg, ipvs_service_t *svc)
 	}
 
 	NLA_PUT_STRING(msg, IPVS_SVC_ATTR_SCHED_NAME, svc->user.sched_name);
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	if (svc->pe_name[0])
 		NLA_PUT_STRING(msg, IPVS_SVC_ATTR_PE_NAME, svc->pe_name);
+#endif
 	NLA_PUT(msg, IPVS_SVC_ATTR_FLAGS, sizeof(flags), &flags);
 	NLA_PUT_U32(msg, IPVS_SVC_ATTR_TIMEOUT, svc->user.timeout);
 	NLA_PUT_U32(msg, IPVS_SVC_ATTR_NETMASK, svc->user.netmask);
@@ -770,10 +779,12 @@ static int ipvs_services_parse_cb(struct nl_msg *msg, void *arg)
 		nla_get_string(svc_attrs[IPVS_SVC_ATTR_SCHED_NAME]),
 		IP_VS_SCHEDNAME_MAXLEN);
 
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	if (svc_attrs[IPVS_SVC_ATTR_PE_NAME])
 		strncpy(get->user.entrytable[i].pe_name,
 			nla_get_string(svc_attrs[IPVS_SVC_ATTR_PE_NAME]),
 			IP_VS_PENAME_MAXLEN);
+#endif
 
 	get->user.entrytable[i].user.netmask = nla_get_u32(svc_attrs[IPVS_SVC_ATTR_NETMASK]);
 	get->user.entrytable[i].user.timeout = nla_get_u32(svc_attrs[IPVS_SVC_ATTR_TIMEOUT]);
@@ -1048,7 +1059,9 @@ ipvs_get_service_err2:
 	}
 	svc->af = AF_INET;
 	svc->nf_addr.ip = svc->user.addr;
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	svc->pe_name[0] = '\0';
+#endif
 	return svc;
 out_err:
 	FREE(svc);

keepalived/include/check_data.h
@@ -119,10 +119,12 @@ typedef struct _virtual_server {
 	int				ops;
 #ifdef _WITH_LVS_
 	char				sched[IP_VS_SCHEDNAME_MAXLEN];
-	uint32_t			timeout_persistence;
+	uint32_t			persistence_timeout;
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	char				pe_name[IP_VS_PENAME_MAXLEN];
+#endif
 	unsigned			loadbalancing_kind;
-	uint32_t			granularity_persistence;
+	uint32_t			persistence_granularity;
 #endif
 	char				*virtualhost;
 	list				rs;
@@ -223,12 +225,12 @@ static inline int inaddr_equal(sa_family_t family, void *addr1, void *addr2)
 			 (X)->af                      == (Y)->af                        &&\
 			 (X)->service_type            == (Y)->service_type		&&\
 			 (X)->loadbalancing_kind      == (Y)->loadbalancing_kind	&&\
-			 (X)->granularity_persistence == (Y)->granularity_persistence	&&\
+			 (X)->persistence_granularity == (Y)->persistence_granularity	&&\
 			 (  (!(X)->quorum_up && !(Y)->quorum_up) || \
 			    ((X)->quorum_up && (Y)->quorum_up && !strcmp ((X)->quorum_up, (Y)->quorum_up)) \
 			 ) &&\
 			 !strcmp((X)->sched, (Y)->sched)				&&\
-			 (X)->timeout_persistence     == (Y)->timeout_persistence 	&&\
+			 (X)->persistence_timeout     == (Y)->persistence_timeout 	&&\
 			 (((X)->vsgname && (Y)->vsgname &&				\
 			   !strcmp((X)->vsgname, (Y)->vsgname)) ||			\
 			  (!(X)->vsgname && !(Y)->vsgname)))

keepalived/include/ip_vs.h
@@ -38,7 +38,9 @@ struct ip_vs_service_app {
 	struct ip_vs_service_user user;
 	u_int16_t		af;
 	union nf_inet_addr	nf_addr;
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	char			pe_name[IP_VS_PENAME_MAXLEN];
+#endif
 };
 
 struct ip_vs_dest_app {
@@ -53,7 +55,9 @@ struct ip_vs_service_entry_app {
 	ip_vs_stats_t		stats;
 	u_int16_t		af;
 	union nf_inet_addr	nf_addr;
+#ifdef IPVS_SVC_ATTR_PE_NAME
 	char			pe_name[IP_VS_PENAME_MAXLEN];
+#endif
 
 };
 

commit 1479ce13459225ae56098af8363d4cd2163c6dc7

Fix persistence_granularity IPv4 netmask validation
The logic test from inet_aton() appears to be inverted.

Tested-by: Simon Kirby <sim@hostway.ca>
Fixes: fecfe5af9913 ("Improve persistence handling")

keepalived/check/check_parser.c
@@ -339,7 +339,7 @@ pgr_handler(vector_t *strvec)
 			return;
 		}
 	} else {
-		if (inet_aton(strvec_slot(strvec, 1), &addr)) {
+		if (!inet_aton(strvec_slot(strvec, 1), &addr)) {
 			log_message(LOG_INFO, "Invalid IPv4 persistence_granularity specified - %s", FMT_STR_VSLOT(strvec, 1));
 			return;
 		}