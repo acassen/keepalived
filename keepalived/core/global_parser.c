6969a17bca377b98c42beb7ce5844a5022cbe14a
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 12:15:13 +0100

    Stop using atoi for parsing realtime scheduling priority
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/core/global_parser.c
+++ keepalived/core/global_parser.c
@@ -421,13 +421,16 @@ get_realtime_priority(vector_t *strvec, const char *process)
 	min_priority = sched_get_priority_min(SCHED_RR);
 	max_priority = sched_get_priority_max(SCHED_RR);
 
-	priority = atoi(strvec_slot(strvec, 1));
+	if (!read_int_strvec(strvec, 1, &priority, INT_MIN, INT_MAX, true)) {
+		report_config_error(CONFIG_GENERAL_ERROR, "%s process real-time priority '%s' invalid", process, FMT_STR_VSLOT(strvec, 1));
+		return -1;
+	}
 
 	if (priority < min_priority) {
 		report_config_error(CONFIG_GENERAL_ERROR, "%s process real-time priority %d less than minimum %d - setting to minimum", process, priority, min_priority);
 		priority = min_priority;
 	}
-	if (priority > max_priority) {
+	else if (priority > max_priority) {
 		report_config_error(CONFIG_GENERAL_ERROR, "%s process real-time priority %d greater than maximum %d - setting to maximum", process, priority, max_priority);
 		priority = max_priority;
 	}
