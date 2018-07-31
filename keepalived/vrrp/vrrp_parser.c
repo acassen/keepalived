5cb21f53a637221fd1c257cf362c35d91e3e25a7
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 12:13:35 +0100

    Stop using atoi for parsing vrrp script fall
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_parser.c
+++ keepalived/vrrp/vrrp_parser.c
@@ -989,9 +989,14 @@ static void
 vrrp_vscript_fall_handler(vector_t *strvec)
 {
 	vrrp_script_t *vscript = LIST_TAIL_DATA(vrrp_data->vrrp_script);
-	vscript->fall = atoi(strvec_slot(strvec, 1));
-	if (vscript->fall < 1)
+	unsigned fall;
+
+	if (!read_unsigned_strvec(strvec, 1, &fall, 1, INT_MAX, true)) {
+		report_config_error(CONFIG_GENERAL_ERROR, "(%s): vrrp script fall value '%s' invalid, defaulting to 1", vscript->sname, FMT_STR_VSLOT(strvec, 1));
 		vscript->fall = 1;
+	}
+	else
+		vscript->fall = fall;
 }
 static void
 vrrp_vscript_user_handler(vector_t *strvec)
