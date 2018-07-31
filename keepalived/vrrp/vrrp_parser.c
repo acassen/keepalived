401495c59dfb310201175a4d023d734f6554afa1
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 12:12:05 +0100

    Stop using atoi for parsing preempt_delay
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_parser.c
+++ keepalived/vrrp/vrrp_parser.c
@@ -642,14 +642,14 @@ static void
 vrrp_preempt_delay_handler(vector_t *strvec)
 {
 	vrrp_t *vrrp = LIST_TAIL_DATA(vrrp_data->vrrp);
-	unsigned long preempt_delay = (unsigned long)(atof(vector_slot(strvec, 1)) * TIMER_HZ);
+	double preempt_delay;
 
-	if (preempt_delay > TIMER_MAX_SEC * TIMER_HZ) {
+	if (!read_double_strvec(strvec, 1, &preempt_delay, 0, 1000, true)) {
 		report_config_error(CONFIG_GENERAL_ERROR, "(%s) Preempt_delay not valid! must be between 0-%d", vrrp->iname, TIMER_MAX_SEC);
 		vrrp->preempt_delay = 0;
 	}
 	else
-		vrrp->preempt_delay = preempt_delay;
+		vrrp->preempt_delay = (unsigned long)(preempt_delay * TIMER_HZ);
 }
 static void
 vrrp_notify_backup_handler(vector_t *strvec)
