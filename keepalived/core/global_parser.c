fe88ece38236b9f5a6bd27df68fe740e743bdeb1
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 12:16:11 +0100

    Stop using atoi for parsing global garp/gna_interval
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/core/global_parser.c
+++ keepalived/core/global_parser.c
@@ -534,14 +534,26 @@ vrrp_garp_lower_prio_rep_handler(vector_t *strvec)
 static void
 vrrp_garp_interval_handler(vector_t *strvec)
 {
-	global_data->vrrp_garp_interval = (unsigned)(atof(strvec_slot(strvec, 1)) * TIMER_HZ);
+	double interval;
+
+	if (!read_double_strvec(strvec, 1, &interval, 1.0 / TIMER_HZ, UINT_MAX / TIMER_HZ, true))
+		report_config_error(CONFIG_GENERAL_ERROR, "vrrp_garp_interval '%s' is invalid", FMT_STR_VSLOT(strvec, 1));
+	else
+		global_data->vrrp_garp_interval = (unsigned)(interval * TIMER_HZ);
+
 	if (global_data->vrrp_garp_interval >= 1 * TIMER_HZ)
 		log_message(LOG_INFO, "The vrrp_garp_interval is very large - %s seconds", FMT_STR_VSLOT(strvec, 1));
 }
 static void
 vrrp_gna_interval_handler(vector_t *strvec)
 {
-	global_data->vrrp_gna_interval = (unsigned)(atof(strvec_slot(strvec, 1)) * TIMER_HZ);
+	double interval;
+
+	if (!read_double_strvec(strvec, 1, &interval, 1.0 / TIMER_HZ, UINT_MAX / TIMER_HZ, true))
+		report_config_error(CONFIG_GENERAL_ERROR, "vrrp_gna_interval '%s' is invalid", FMT_STR_VSLOT(strvec, 1));
+	else
+		global_data->vrrp_gna_interval = (unsigned)(interval * TIMER_HZ);
+
 	if (global_data->vrrp_gna_interval >= 1 * TIMER_HZ)
 		log_message(LOG_INFO, "The vrrp_gna_interval is very large - %s seconds", FMT_STR_VSLOT(strvec, 1));
 }
