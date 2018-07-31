78a84c92fbc5e06f27d31cd2bceaed256bc1e950
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 12:11:45 +0100

    Stop using atoi for parsing garp_refresh
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_parser.c
+++ keepalived/vrrp/vrrp_parser.c
@@ -762,7 +762,14 @@ static void
 vrrp_garp_refresh_handler(vector_t *strvec)
 {
 	vrrp_t *vrrp = LIST_TAIL_DATA(vrrp_data->vrrp);
-	vrrp->garp_refresh.tv_sec = atoi(strvec_slot(strvec, 1));
+	unsigned refresh;
+
+	if (!read_unsigned_strvec(strvec, 1, &refresh, 0, UINT_MAX, true)) {
+		report_config_error(CONFIG_GENERAL_ERROR, "(%s): Invalid garp_refresh '%s' - ignoring", vrrp->iname, FMT_STR_VSLOT(strvec, 1));
+		vrrp->garp_refresh.tv_sec = 0;
+	}
+	else
+		vrrp->garp_refresh.tv_sec = refresh;
 	vrrp->garp_refresh.tv_usec = 0;
 }
 static void
