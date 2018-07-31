f2f5b566ea18e8a0ab0fbda27b021f37125b4a73
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 22:45:50 +0100

    Correct error message for garp_master_refresh
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_parser.c
+++ keepalived/vrrp/vrrp_parser.c
@@ -765,7 +765,7 @@ vrrp_garp_refresh_handler(vector_t *strvec)
 	unsigned refresh;
 
 	if (!read_unsigned_strvec(strvec, 1, &refresh, 0, UINT_MAX, true)) {
-		report_config_error(CONFIG_GENERAL_ERROR, "(%s): Invalid garp_refresh '%s' - ignoring", vrrp->iname, FMT_STR_VSLOT(strvec, 1));
+		report_config_error(CONFIG_GENERAL_ERROR, "(%s): Invalid garp_master_refresh '%s' - ignoring", vrrp->iname, FMT_STR_VSLOT(strvec, 1));
 		vrrp->garp_refresh.tv_sec = 0;
 	}
 	else
