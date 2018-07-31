4c1e31ffe0f3e1dd69a6fe713766203bdd05f046
Quentin Armitage <quentin@armitage.org.uk>
2018-07-31 12:13:17 +0100

    Stop using atoi for parsing vrrp script rise
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_parser.c
+++ keepalived/vrrp/vrrp_parser.c
@@ -976,9 +976,14 @@ static void
 vrrp_vscript_rise_handler(vector_t *strvec)
 {
 	vrrp_script_t *vscript = LIST_TAIL_DATA(vrrp_data->vrrp_script);
-	vscript->rise = atoi(strvec_slot(strvec, 1));
-	if (vscript->rise < 1)
+	unsigned rise;
+
+	if (!read_unsigned_strvec(strvec, 1, &rise, 1, INT_MAX, true)) {
+		report_config_error(CONFIG_GENERAL_ERROR, "(%s): vrrp script rise value '%s' invalid, defaulting to 1", vscript->sname, FMT_STR_VSLOT(strvec, 1));
 		vscript->rise = 1;
+	}
+	else
+		vscript->rise = rise;
 }
 static void
 vrrp_vscript_fall_handler(vector_t *strvec)
