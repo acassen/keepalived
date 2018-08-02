a0a325be0635f699bd81bd59458ff33e25b28921
Quentin Armitage <quentin@armitage.org.uk>
2018-08-02 18:20:23 +0100

    Enable --config-test to work with BFD configuration
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/core/main.c
+++ keepalived/core/main.c
@@ -473,14 +473,23 @@ validate_config(void)
 
 #ifdef _WITH_LVS_
 	/* validate healthchecker config */
+#ifndef _DEBUG_
+	prog_type = PROG_TYPE_CHECKER;
+#endif
 	check_validate_config();
 #endif
 #ifdef _WITH_VRRP_
 	/* validate vrrp config */
+#ifndef _DEBUG_
+	prog_type = PROG_TYPE_VRRP;
+#endif
 	vrrp_validate_config();
 #endif
 #ifdef _WITH_BFD_
 	/* validate bfd config */
+#ifndef _DEBUG_
+	prog_type = PROG_TYPE_BFD;
+#endif
 	bfd_validate_config();
 #endif
 }
