acassen/keepalived

commit 136c7e037db70cd1298930417a4d925c709db0c4

Add debug option for dumping vrrp fd lists
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_daemon.c
@@ -104,6 +104,20 @@ static char *vrrp_syslog_ident;
 static bool two_phase_terminate;
 #endif
 
+#ifdef _VRRP_FD_DEBUG_
+bool do_vrrp_fd_debug;
+#endif
+
+#ifdef _VRRP_FD_DEBUG_
+static void
+dump_vrrp_fd(void)
+{
+	log_message(LOG_INFO, "----[ Begin VRRP fd dump ]----");
+	dump_mlist(NULL, vrrp_data->vrrp_index_fd);
+	log_message(LOG_INFO, "----[ End VRRP fd dump ]----");
+}
+#endif
+
 static void
 set_vrrp_max_fds(void)
 {
@@ -946,6 +960,9 @@ start_vrrp_child(void)
 		log_message(LOG_INFO, "VRRP child process: cannot write pidfile");
 		exit(0);
 	}
+
+	if (do_vrrp_fd_debug)
+		set_extra_threads_debug(dump_vrrp_fd);
 
 	/* Create the new master thread */
 	thread_destroy_master(master);	/* This destroys any residual settings from the parent */

lib/list.c
@@ -202,17 +202,18 @@ alloc_mlist(void (*free_func) (void *), void (*dump_func) (FILE *, void *), size
 	list new = (list) MALLOC(size * sizeof (struct _list));
 	new->free = free_func;
 	new->dump = dump_func;
+	new->count = size;
 	return new;
 }
 
-#ifdef _INCLUDE_UNUSED_CODE_
+#ifdef _VRRP_FD_DEBUG_
 void
-dump_mlist(FILE *fp, list l, size_t size)
+dump_mlist(FILE *fp, list l)
 {
 	element e;
-	int i;
+	unsigned i;
 
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < l->count; i++) {
 		for (e = LIST_HEAD(&l[i]); e; ELEMENT_NEXT(e))
 			if (l->dump)
 				(*l->dump) (fp, e->data);

lib/list.h
@@ -67,6 +67,9 @@ extern void list_remove(list l, element);
 extern void list_del(list l, void *data);
 extern void free_list_data(list l, void *data);
 extern list alloc_mlist(void (*free_func) (void *), void (*dump_func) (FILE *, void *), size_t size);
+#ifdef _VRRP_FD_DEBUG_
+extern void dump_mlist(FILE *, list);
+#endif
 extern void free_mlist(list l, size_t size);
 
 #endif

configure.ac
@@ -231,6 +231,8 @@ AC_ARG_ENABLE(regex-debug,
   [  --enable-regex-debug    compile with regex debugging support])
 AC_ARG_ENABLE(tsm-debug,
   [  --enable-tsm-debug      compile with TSM debugging support])
+AC_ARG_ENABLE(vrrp-fd-debug,
+  [  --enable-vrrp-fd-debug  compile with vrrp fd debugging support])
 AC_ARG_ENABLE(profile,
   [AS_HELP_STRING([--enable-profile], [compile with profiling flags])])
 AC_ARG_ENABLE(conversion-checks,
@@ -1805,6 +1807,15 @@ else
   ENABLE_TSM_DEBUG=No
 fi
 
+dnl ----[ VRRP FD debugging support or not ? ]----
+if test "${enable_vrrp_fd_debug}" = yes; then
+  AC_DEFINE([_VRRP_FD_DEBUG_], [ 1 ], [Define to 1 to build with vrrp fd debugging support])
+  ENABLE_VRRP_FD_DEBUG=Yes
+  add_config_opt([VRRP_FD_DEBUG])
+else
+  ENABLE_VRRP_FD_DEBUG=No
+fi
+
 dnl ----[ Profiling or not ? ]----
 WITH_PROFILING=No
 if test "${enable_profile}" = yes; then
@@ -1986,6 +1997,9 @@ if test ${ENABLE_REGEX_DEBUG} = Yes; then
 fi
 if test ${ENABLE_TSM_DEBUG} = Yes; then
   echo "TSM debugging            : ${ENABLE_TSM_DEBUG}"
+fi
+if test ${ENABLE_VRRP_FD_DEBUG} = Yes; then
+  echo "VRRP_FD debugging        : ${ENABLE_VRRP_FD_DEBUG}"
 fi
 if test ${ENABLE_DEBUG} = Yes; then
   echo "Use Debug flags          : ${ENABLE_DEBUG}"

doc/man/man8/keepalived.8
@@ -227,6 +227,7 @@ Chr@Debug option
 _
 D@Epoll thread dump
 E@Epoll debug
+F@VRRP fd debug
 N@Netlink timers
 X@Regex timers
 M@Email alert debug

keepalived/core/main.c
@@ -162,7 +162,7 @@ static const char *core_dump_pattern = "core";
 static char *orig_core_dump_pattern = NULL;
 
 /* debug flags */
-#if defined _TIMER_CHECK_ || defined _SMTP_ALERT_DEBUG_ || defined _EPOLL_DEBUG_ || defined _EPOLL_THREAD_DUMP_ || defined _REGEX_DEBUG_ || defined _WITH_REGEX_TIMERS_ || defined _TSM_DEBUG_ || defined _NETLINK_TIMERS_
+#if defined _TIMER_CHECK_ || defined _SMTP_ALERT_DEBUG_ || defined _EPOLL_DEBUG_ || defined _EPOLL_THREAD_DUMP_ || defined _REGEX_DEBUG_ || defined _WITH_REGEX_TIMERS_ || defined _TSM_DEBUG_ || defined _VRRP_FD_DEBUG_ || defined _NETLINK_TIMERS_
 #define WITH_DEBUG_OPTIONS 1
 #endif
 
@@ -186,6 +186,9 @@ static char regex_timers;
 #endif
 #ifdef _TSM_DEBUG_
 static char tsm_debug;
+#endif
+#ifdef _VRRP_FD_DEBUG_
+static char vrrp_fd_debug;
 #endif
 #ifdef _NETLINK_TIMERS_
 static char netlink_timer_debug;
@@ -933,6 +936,9 @@ initialise_debug_options(void)
 #ifdef _TSM_DEBUG_
 	do_tsm_debug = !!(tsm_debug & mask);
 #endif
+#ifdef _VRRP_FD_DEBUG_
+	do_vrrp_fd_debug = !!(vrrp_fd_debug & mask);
+#endif
 #ifdef _NETLINK_TIMERS_
 	do_netlink_timers = !!(netlink_timer_debug & mask);
 #endif
@@ -983,6 +989,9 @@ set_debug_options(const char *options)
 #endif
 #ifdef _TSM_DEBUG_
 		tsm_debug = all_processes;
+#endif
+#ifdef _VRRP_FD_DEBUG_
+		vrrp_fd_debug = all_processes;
 #endif
 #ifdef _NETLINK_TIMERS_
 		netlink_timer_debug = all_processes;
@@ -1065,6 +1074,11 @@ set_debug_options(const char *options)
 #ifdef _TSM_DEBUG_
 		case 'S':
 			tsm_debug = processes;
+			break;
+#endif
+#ifdef _VRRP_FD_DEBUG_
+		case 'F':
+			vrrp_fd_debug = processes;
 			break;
 #endif
 #ifdef _NETLINK_TIMERS_
@@ -1159,6 +1173,9 @@ usage(const char *prog)
 #endif
 #ifdef _EPOLL_THREAD_DUMP_
 	fprintf(stderr, "                                   D - epoll thread dump debug\n");
+#endif
+#ifdef _VRRP_FD_DEBUG
+	fprintf(stderr, "                                   F - vrrp fd dump debug\n");
 #endif
 #ifdef _REGEX_DEBUG_
 	fprintf(stderr, "                                   R - regex debug\n");

keepalived/include/vrrp_daemon.h
@@ -30,6 +30,10 @@
 
 extern bool non_existent_interface_specified;
 
+#ifdef _VRRP_FD_DEBUG_
+extern bool do_vrrp_fd_debug;
+#endif
+
 /* Prototypes */
 extern int start_vrrp_child(void);
 extern void vrrp_validate_config(void);

keepalived/vrrp/vrrp_data.c
@@ -850,6 +850,24 @@ free_vrrp_buffer(void)
 	vrrp_buffer_len = 0;
 }
 
+#ifdef _VRRP_FD_DEBUG_
+static void
+dump_one_vrrp_fd(FILE *fd, void *data)
+{
+	vrrp_t *vrrp = data;
+	timeval_t time_diff;
+
+	if (!fd) {
+		if (vrrp->sands.tv_sec == TIMER_DISABLED)
+			log_message(LOG_INFO, "    %s: fd %d, sands DISABLED", vrrp->iname, vrrp->sockets->fd_in);
+		else {
+			timersub(&vrrp->sands, &time_now, &time_diff);
+			log_message(LOG_INFO, "    %s: fd %d, sands %ld.%6.6ld", vrrp->iname, vrrp->sockets->fd_in, time_diff.tv_sec, time_diff.tv_usec);
+		}
+	}
+}
+#endif
+
 vrrp_data_t *
 alloc_vrrp_data(void)
 {
@@ -858,7 +876,13 @@ alloc_vrrp_data(void)
 	new = (vrrp_data_t *) MALLOC(sizeof(vrrp_data_t));
 	new->vrrp = alloc_list(free_vrrp, dump_vrrp);
 	new->vrrp_index = alloc_mlist(NULL, NULL, VRRP_INDEX_FD_SIZE);
-	new->vrrp_index_fd = alloc_mlist(NULL, NULL, FD_INDEX_SIZE);
+	new->vrrp_index_fd = alloc_mlist(NULL,
+#ifdef _VRRP_FD_DEBUG_
+					       dump_one_vrrp_fd,
+#else
+					       NULL,
+#endif
+								FD_INDEX_SIZE);
 	new->vrrp_sync_group = alloc_list(free_vgroup, dump_vgroup);
 	new->vrrp_script = alloc_list(free_vscript, dump_vscript);
 	new->vrrp_track_files = alloc_list(free_vfile, dump_vfile);

commit 861ca7c4f5a142c899745cdf5d8847184c3a5970

Fix list count broken by commit 136c7e0
Commit 136c7e0 - "Add debug option for dumping vrrp fd lists" broke
the count field of lists.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_daemon.c
@@ -113,7 +113,7 @@ static void
 dump_vrrp_fd(void)
 {
 	log_message(LOG_INFO, "----[ Begin VRRP fd dump ]----");
-	dump_mlist(NULL, vrrp_data->vrrp_index_fd);
+	dump_mlist(NULL, vrrp_data->vrrp_index_fd, FD_INDEX_SIZE);
 	log_message(LOG_INFO, "----[ End VRRP fd dump ]----");
 }
 #endif

lib/list.c
@@ -202,18 +202,17 @@ alloc_mlist(void (*free_func) (void *), void (*dump_func) (FILE *, void *), size
 	list new = (list) MALLOC(size * sizeof (struct _list));
 	new->free = free_func;
 	new->dump = dump_func;
-	new->count = size;
 	return new;
 }
 
 #ifdef _VRRP_FD_DEBUG_
 void
-dump_mlist(FILE *fp, list l)
+dump_mlist(FILE *fp, list l, size_t size)
 {
 	element e;
 	unsigned i;
 
-	for (i = 0; i < l->count; i++) {
+	for (i = 0; i < size; i++) {
 		for (e = LIST_HEAD(&l[i]); e; ELEMENT_NEXT(e))
 			if (l->dump)
 				(*l->dump) (fp, e->data);

lib/list.h
@@ -68,7 +68,7 @@ extern void list_del(list l, void *data);
 extern void free_list_data(list l, void *data);
 extern list alloc_mlist(void (*free_func) (void *), void (*dump_func) (FILE *, void *), size_t size);
 #ifdef _VRRP_FD_DEBUG_
-extern void dump_mlist(FILE *, list);
+extern void dump_mlist(FILE *, list, size);
 #endif
 extern void free_mlist(list l, size_t size);
 