fe034421eb65e59585962f57d8fd8d5d7dbbda68
Quentin Armitage <quentin@armitage.org.uk>
2019-06-13 17:44:32 +0100

    coverity: Explicit null dereferenced
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp.c
+++ keepalived/vrrp/vrrp.c
@@ -636,7 +636,8 @@ vrrp_in_chk_vips(const vrrp_t *vrrp, const ip_address_t *ipaddress, const unsign
 static int
 vrrp_check_packet(vrrp_t *vrrp, const vrrphdr_t *hd, const char *buffer, ssize_t buflen_ret, bool check_vip_addr)
 {
-	const struct iphdr *ip = NULL;
+	const struct iphdr *ip = (const struct iphdr *)buffer;
+					/* Stop coverity issuing NULL pointer dereference warning */
 	int ihl = 0;	/* Stop compiler issuing possibly uninitialised warning */
 	size_t vrrppkt_len;
 	unsigned adver_int;
@@ -660,7 +661,6 @@ vrrp_check_packet(vrrp_t *vrrp, const vrrphdr_t *hd, const char *buffer, ssize_t
 	/* IPv4 related */
 	if (vrrp->family == AF_INET) {
 		/* To begin with, we just concern ourselves with the protocol headers */
-		ip = (const struct iphdr *) (buffer);
 		ihl = ip->ihl << 2;
 
 		expected_len = ihl;
