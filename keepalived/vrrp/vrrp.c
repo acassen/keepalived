981fea9d7eece6b4f4acff85a832b0c69fdd8f42
zdc <taras.ukr@gmail.com>
2019-06-13 14:50:22 +0300

    Fix building when _HAVE_VRRP_VMAC_ is defined, but _HAVE_VRRP_IPVLAN_ is not (issue https://github.com/acassen/keepalived/issues/1309)

--- keepalived/vrrp/vrrp.c
+++ keepalived/vrrp/vrrp.c
@@ -3876,9 +3876,12 @@ clear_diff_vrrp(void)
 			 */
 			if (vrrp->ifp->is_ours &&
 			    ((__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags) &&
-			      !__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags)) ||
-			     (__test_bit(VRRP_IPVLAN_BIT, &vrrp->vmac_flags) &&
-			      !__test_bit(VRRP_IPVLAN_BIT, &vrrp->vmac_flags)))) {
+                             !__test_bit(VRRP_VMAC_BIT, &vrrp->vmac_flags))
+#ifdef _HAVE_VRRP_IPVLAN_
+			     || (__test_bit(VRRP_IPVLAN_BIT, &vrrp->vmac_flags) &&
+			     !__test_bit(VRRP_IPVLAN_BIT, &vrrp->vmac_flags))
+#endif
+			    )) {
 				netlink_link_del_vmac(vrrp);
 			}
 #endif
