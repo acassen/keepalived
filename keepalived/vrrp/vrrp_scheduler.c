d25c8bdd7047eb6f37f1f73729ee11b5fca431de
Quentin Armitage <quentin@armitage.org.uk>
2019-06-13 17:44:22 +0100

    coverity: Logically dead code
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_scheduler.c
+++ keepalived/vrrp/vrrp_scheduler.c
@@ -331,15 +331,11 @@ static timeval_t *
 vrrp_compute_timer(const sock_t *sock)
 {
 	vrrp_t *vrrp;
-	static timeval_t timer = { .tv_sec = TIMER_DISABLED };
 
 	/* The sock won't exist if there isn't a vrrp instance on it,
 	 * so rb_first will always exist. */
 	vrrp = rb_entry(rb_first_cached(&sock->rb_sands), vrrp_t, rb_sands);
-	if (vrrp)
-		return &vrrp->sands;
-
-	return &timer;
+	return &vrrp->sands;
 }
 
 void
