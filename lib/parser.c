9d3592d91b2c60e5a973a8b8b9827ef31e2f7d41
Quentin Armitage <quentin@armitage.org.uk>
2019-11-18 11:49:58 +0000

    Resolve some issues identified by LGTM
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- lib/parser.c
+++ lib/parser.c
@@ -149,10 +149,10 @@ get_config_status(void)
 	return config_err;
 }
 
-static const char * __attribute__ ((noreturn))
+static void __attribute__ ((noreturn))
 null_strvec(const vector_t *strvec, size_t index)
 {
-	if (index - 1 < vector_size(strvec) && index > 0 && vector_slot(strvec, index - 1))
+	if (index > 0 && index - 1 < vector_size(strvec) && vector_slot(strvec, index - 1))
 		report_config_error(CONFIG_MISSING_PARAMETER, "*** Configuration line starting `%s` is missing a parameter after keyword `%s` at word position %zu", vector_slot(strvec, 0) ? (char *)vector_slot(strvec, 0) : "***MISSING ***", (char *)vector_slot(strvec, index - 1), index + 1);
 	else
 		report_config_error(CONFIG_MISSING_PARAMETER, "*** Configuration line starting `%s` is missing a parameter at word position %zu", vector_slot(strvec, 0) ? (char *)vector_slot(strvec, 0) : "***MISSING ***", index + 1);
