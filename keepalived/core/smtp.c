17dbfe94794672f9de2ef730cca35a0e0e3861b4
Quentin Armitage <quentin@armitage.org.uk>
2021-04-10 18:42:48 +0100

    smtp: When initiating an SMTP message, don't use extra thread event
    Don't add an event thread after the initial TCP connect, just call
    the relevant function directly.
    
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/core/smtp.c
+++ keepalived/core/smtp.c
@@ -579,8 +579,9 @@ smtp_connect(smtp_t *smtp)
 
 	status = tcp_connect(smtp->fd, &global_data->smtp_server);
 
+	thread_t thread = { .u.f.fd = smtp->fd, .master = master, .arg = smtp };
 	/* Handle connection status code */
-	thread_add_event(master, SMTP_FSM[status].send, smtp, smtp->fd);
+	SMTP_FSM_SEND(status, &thread);
 }
 
 #ifdef _SMTP_ALERT_DEBUG_
