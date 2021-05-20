ff7f0424fd21fc793a076e53863a83930a7f7ad0
Quentin Armitage <quentin@armitage.org.uk>
2021-05-20 21:00:13 +0100

    scheduler: simplify initialisation of struct epoll_event
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- lib/scheduler.c
+++ lib/scheduler.c
@@ -649,10 +649,9 @@ thread_event_set(const thread_t *thread)
 {
 	thread_event_t *event = thread->event;
 	thread_master_t *m = thread->master;
-	struct epoll_event ev = { .events = 0 };
+	struct epoll_event ev = { .events = 0, .data.ptr = event };
 	int op;
 
-	ev.data.ptr = event;
 	if (__test_bit(THREAD_FL_READ_BIT, &event->flags))
 		ev.events |= EPOLLIN;
 
