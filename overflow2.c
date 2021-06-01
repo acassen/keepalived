d7cd6826d72257b70627b1685f968bbc43a1da1a
Magicsmx <55052512+Magicsmx@users.noreply.github.com>
2021-06-01 22:12:46 +0800

    Create overflow2.c

---
+++ overflow2.c
@@ -0,0 +1,15 @@
+{  
+    "enabled":1,
+    "version_min":300000,
+    "title":"multipart Content-Disposition should allow filename* field (3/6)",
+    "client":{  
+      "ip":"200.249.12.31",
+      "port":123
+	@@ -146,7 +146,7 @@
+  {  
+    "enabled":1,
+    "version_min":300000,
+    "title":"multipart Content-Disposition should allow filename* field (4/6)",
+    "client":{  
+      "ip":"200.249.12.31",
+      "port":123
