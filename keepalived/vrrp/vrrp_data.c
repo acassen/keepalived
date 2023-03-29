acassen/keepalived

commit 516032ec39169d05c613de0e8ee10845658748ff

config parser: Fix segfault caused by extra '}' and other parser fixes
If there was a configuration error in a block, e.g. a vrrp_instance,
keepalived would apply the configuration in the rest of the block to
the previous object of that type, e.g. the previous vrrp instance. If
there had been no previous instance, keepalived would probably segfault.

This commit changes the way the parser works. A new instance of an
object, e.g. a VRRP instance or a virtual server, is only added to the
list of those objects once the configuration of that object is complete.
In particular it no longer applies the configuration to the last entry
on the list of the relevant object type, but keeps a point to the
object currently being configured.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_data.c
@@ -48,6 +48,7 @@
 #include "vrrp_static_track.h"
 #include "parser.h"
 #include "track_file.h"
+#include "vrrp_parser.h"
 
 /* global vars */
 vrrp_data_t *vrrp_data = NULL;
@@ -84,7 +85,7 @@ dump_static_track_groups_list(FILE *fp, const list_head_t *l)
 		dump_static_track_group(fp, tgroup);
 }
 
-void
+static_track_group_t *
 alloc_static_track_group(const char *gname)
 {
 	static_track_group_t *new;
@@ -95,14 +96,17 @@ alloc_static_track_group(const char *gname)
 	INIT_LIST_HEAD(&new->vrrp_instances);
 	new->gname = STRDUP(gname);
 
-	list_add_tail(&new->e_list, &vrrp_data->static_track_groups);
+	return new;
 }
 
 /* Static addresses facility function */
 void
 alloc_saddress(const vector_t *strvec)
 {
-	alloc_ipaddress(&vrrp_data->static_addresses, strvec, true);
+	ip_address_t *new_ipaddr;
+
+	if ((new_ipaddr = alloc_ipaddress(strvec, true)))
+		list_add_tail(&new_ipaddr->e_list, &vrrp_data->static_addresses);
 }
 
 /* Static routes facility function */
@@ -845,7 +849,7 @@ dump_vrrp_list(FILE *fp, const list_head_t *l)
 		dump_vrrp(fp, vrrp);
 }
 
-void
+vrrp_sgroup_t *
 alloc_vrrp_sync_group(const char *gname)
 {
 	vrrp_sgroup_t *new;
@@ -870,7 +874,7 @@ alloc_vrrp_sync_group(const char *gname)
 	new->smtp_alert = -1;
 	new->notify_priority_changes = -1;
 
-	list_add_tail(&new->e_list, &vrrp_data->vrrp_sync_group);
+	return new;
 }
 
 static vrrp_stats *
@@ -902,7 +906,7 @@ alloc_vrrp_stats(void)
 	return new;
 }
 
-void
+vrrp_t *
 alloc_vrrp(const char *iname)
 {
 	vrrp_t *new;
@@ -963,13 +967,12 @@ alloc_vrrp(const char *iname)
 
 	new->strict_mode = PARAMETER_UNSET;
 
-	list_add_tail(&new->e_list, &vrrp_data->vrrp);
+	return new;
 }
 
 void
 alloc_vrrp_unicast_peer(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
 	unicast_peer_t *peer;
 	unsigned ttl;
 	unsigned i;
@@ -982,24 +985,24 @@ alloc_vrrp_unicast_peer(const vector_t *strvec)
 	if (inet_stosockaddr(strvec_slot(strvec, 0), NULL, &peer->address)) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: VRRP instance[%s] malformed unicast"
 				     " peer address[%s]. Skipping..."
-				   , vrrp->iname, strvec_slot(strvec, 0));
+				   , current_vrrp->iname, strvec_slot(strvec, 0));
 		FREE(peer);
 		return;
 	}
 
-	if (!vrrp->family)
-		vrrp->family = peer->address.ss_family;
-	else if (peer->address.ss_family != vrrp->family) {
+	if (!current_vrrp->family)
+		current_vrrp->family = peer->address.ss_family;
+	else if (peer->address.ss_family != current_vrrp->family) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: VRRP instance[%s] and unicast peer address"
 				     "[%s] MUST be of the same family !!! Skipping..."
-				   , vrrp->iname, strvec_slot(strvec, 0));
+				   , current_vrrp->iname, strvec_slot(strvec, 0));
 		FREE(peer);
 		return;
 	}
 
 	for (i = 1; i < vector_size(strvec); i += 2) {
 		if (i + 1 >= vector_size(strvec)) {
-			report_config_error(CONFIG_GENERAL_ERROR, "(%s) %s is missing a value", vrrp->iname, strvec_slot(strvec, i));
+			report_config_error(CONFIG_GENERAL_ERROR, "(%s) %s is missing a value", current_vrrp->iname, strvec_slot(strvec, i));
 			break;
 		}
 		if (read_unsigned(strvec_slot(strvec, i + 1), &ttl, 0, 255, false)) {
@@ -1008,50 +1011,42 @@ alloc_vrrp_unicast_peer(const vector_t *strvec)
 			else if (!strcmp(strvec_slot(strvec, i), "max_ttl"))
 				peer->max_ttl = ttl;
 			else {
-				report_config_error(CONFIG_GENERAL_ERROR, "(%s) unknown unicast_peer option %s", vrrp->iname, strvec_slot(strvec, i));
+				report_config_error(CONFIG_GENERAL_ERROR, "(%s) unknown unicast_peer option %s", current_vrrp->iname, strvec_slot(strvec, i));
 				break;
 			}
-			__set_bit(VRRP_FLAG_CHECK_UNICAST_SRC, &vrrp->flags);
+			__set_bit(VRRP_FLAG_CHECK_UNICAST_SRC, &current_vrrp->flags);
 		}
 	}
 
 	if (peer->min_ttl > peer->max_ttl)
-		report_config_error(CONFIG_GENERAL_ERROR, "(%s) min_ttl %u > max_ttl %u - all packets will be discarded", vrrp->iname, peer->min_ttl, peer->max_ttl);
+		report_config_error(CONFIG_GENERAL_ERROR, "(%s) min_ttl %u > max_ttl %u - all packets will be discarded", current_vrrp->iname, peer->min_ttl, peer->max_ttl);
 
-	list_add_tail(&peer->e_list, &vrrp->unicast_peer);
+	list_add_tail(&peer->e_list, &current_vrrp->unicast_peer);
 }
 
 void
 alloc_vrrp_track_if(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	alloc_track_if(vrrp->iname, &vrrp->track_ifp, strvec);
+	alloc_track_if(current_vrrp->iname, &current_vrrp->track_ifp, strvec);
 }
 
 void
 alloc_vrrp_track_script(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	alloc_track_script(vrrp->iname, &vrrp->track_script, strvec);
+	alloc_track_script(current_vrrp->iname, &current_vrrp->track_script, strvec);
 }
 
 void
 alloc_vrrp_track_file(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	vrrp_alloc_track_file(vrrp->iname, &vrrp_data->vrrp_track_files, &vrrp->track_file, strvec);
+	vrrp_alloc_track_file(current_vrrp->iname, &vrrp_data->vrrp_track_files, &current_vrrp->track_file, strvec);
 }
 
 #ifdef _WITH_TRACK_PROCESS_
 void
 alloc_vrrp_track_process(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	alloc_track_process(vrrp->iname, &vrrp->track_process, strvec);
+	alloc_track_process(current_vrrp->iname, &current_vrrp->track_process, strvec);
 }
 #endif
 
@@ -1059,43 +1054,33 @@ alloc_vrrp_track_process(const vector_t *strvec)
 void
 alloc_vrrp_track_bfd(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	alloc_track_bfd(vrrp->iname, &vrrp->track_bfd, strvec);
+	alloc_track_bfd(current_vrrp->iname, &current_vrrp->track_bfd, strvec);
 }
 #endif
 
 void
 alloc_vrrp_group_track_if(const vector_t *strvec)
 {
-	vrrp_sgroup_t *sgroup = list_last_entry(&vrrp_data->vrrp_sync_group, vrrp_sgroup_t, e_list);
-
-	alloc_track_if(sgroup->gname, &sgroup->track_ifp, strvec);
+	alloc_track_if(current_vsyncg->gname, &current_vsyncg->track_ifp, strvec);
 }
 
 void
 alloc_vrrp_group_track_script(const vector_t *strvec)
 {
-	vrrp_sgroup_t *sgroup = list_last_entry(&vrrp_data->vrrp_sync_group, vrrp_sgroup_t, e_list);
-
-	alloc_track_script(sgroup->gname, &sgroup->track_script, strvec);
+	alloc_track_script(current_vsyncg->gname, &current_vsyncg->track_script, strvec);
 }
 
 void
 alloc_vrrp_group_track_file(const vector_t *strvec)
 {
-	vrrp_sgroup_t *sgroup = list_last_entry(&vrrp_data->vrrp_sync_group, vrrp_sgroup_t, e_list);
-
-	vrrp_alloc_track_file(sgroup->gname, &vrrp_data->vrrp_track_files, &sgroup->track_file, strvec);
+	vrrp_alloc_track_file(current_vsyncg->gname, &vrrp_data->vrrp_track_files, &current_vsyncg->track_file, strvec);
 }
 
 #ifdef _WITH_TRACK_PROCESS_
 void
 alloc_vrrp_group_track_process(const vector_t *strvec)
 {
-	vrrp_sgroup_t *sgroup = list_last_entry(&vrrp_data->vrrp_sync_group, vrrp_sgroup_t, e_list);
-
-	alloc_track_process(sgroup->gname, &sgroup->track_process, strvec);
+	alloc_track_process(current_vsyncg->gname, &current_vsyncg->track_process, strvec);
 }
 #endif
 
@@ -1103,65 +1088,60 @@ alloc_vrrp_group_track_process(const vector_t *strvec)
 void
 alloc_vrrp_group_track_bfd(const vector_t *strvec)
 {
-	vrrp_sgroup_t *sgroup = list_last_entry(&vrrp_data->vrrp_sync_group, vrrp_sgroup_t, e_list);
-
-	alloc_track_bfd(sgroup->gname, &sgroup->track_bfd, strvec);
+	alloc_track_bfd(current_vsyncg->gname, &current_vsyncg->track_bfd, strvec);
 }
 #endif
 
 void
 alloc_vrrp_vip(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-	ip_address_t *last_ipaddr = NULL, *tail_ipaddr;
+	ip_address_t *last_ipaddr = NULL, *new_ipaddr;
 	sa_family_t address_family;
 
-	if (!list_empty(&vrrp->vip))
-		last_ipaddr = list_last_entry(&vrrp->vip, ip_address_t, e_list);
+	if (!list_empty(&current_vrrp->vip))
+		last_ipaddr = list_last_entry(&current_vrrp->vip, ip_address_t, e_list);
 
-	alloc_ipaddress(&vrrp->vip, strvec, false);
+	if (!(new_ipaddr = alloc_ipaddress(strvec, false)))
+		return;
 
-	tail_ipaddr = list_last_entry(&vrrp->vip, ip_address_t, e_list);
-	if (!list_empty(&vrrp->vip) && tail_ipaddr != last_ipaddr) {
-		address_family = IP_FAMILY(tail_ipaddr);
+	if (last_ipaddr) {
+		address_family = IP_FAMILY(new_ipaddr);
 
-		if (vrrp->family == AF_UNSPEC)
-			vrrp->family = address_family;
-		else if (address_family != vrrp->family) {
-			report_config_error(CONFIG_GENERAL_ERROR, "(%s): address family must match VRRP instance [%s] - ignoring", vrrp->iname, strvec_slot(strvec, 0));
-			free_ipaddress(tail_ipaddr);
+		if (current_vrrp->family == AF_UNSPEC)
+			current_vrrp->family = address_family;
+		else if (address_family != current_vrrp->family) {
+			report_config_error(CONFIG_GENERAL_ERROR, "(%s): address family must match VRRP instance [%s] - ignoring", current_vrrp->iname, strvec_slot(strvec, 0));
+			free_ipaddress(new_ipaddr);
 			return;
 		}
-
-		vrrp->vip_cnt++;
 	}
+
+	list_add_tail(&new_ipaddr->e_list, &current_vrrp->vip);
+	current_vrrp->vip_cnt++;
 }
 
 void
 alloc_vrrp_evip(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
+	ip_address_t *new_ipaddr;
 
-	alloc_ipaddress(&vrrp->evip, strvec, false);
+	if ((new_ipaddr = alloc_ipaddress(strvec, false)))
+		list_add_tail(&new_ipaddr->e_list, &current_vrrp->evip);
 }
 
 void
 alloc_vrrp_vroute(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	alloc_route(&vrrp->vroutes, strvec, false);
+	alloc_route(&current_vrrp->vroutes, strvec, false);
 }
 
 void
 alloc_vrrp_vrule(const vector_t *strvec)
 {
-	vrrp_t *vrrp = list_last_entry(&vrrp_data->vrrp, vrrp_t, e_list);
-
-	alloc_rule(&vrrp->vrules, strvec, false);
+	alloc_rule(&current_vrrp->vrules, strvec, false);
 }
 
-void
+vrrp_script_t *
 alloc_vrrp_script(const char *sname)
 {
 	vrrp_script_t *new;
@@ -1179,11 +1159,12 @@ alloc_vrrp_script(const char *sname)
 	new->state = SCRIPT_STATE_IDLE;
 	new->rise = 1;
 	new->fall = 1;
-	list_add_tail(&new->e_list, &vrrp_data->vrrp_script);
+
+	return new;
 }
 
 #ifdef _WITH_TRACK_PROCESS_
-void
+vrrp_tracked_process_t *
 alloc_vrrp_process(const char *pname)
 {
 	vrrp_tracked_process_t *new;
@@ -1195,7 +1176,8 @@ alloc_vrrp_process(const char *pname)
 	new->quorum = 1;
 	new->quorum_max = UINT_MAX;
 	INIT_LIST_HEAD(&new->tracking_vrrp);
-	list_add_tail(&new->e_list, &vrrp_data->vrrp_track_processes);
+
+	return new;
 }
 #endif
 

configure.ac
@@ -787,7 +787,7 @@ AS_IF([test .$HAVE_WARNING_STRICT_OVERFLOW = .yes],
      )
 CFLAGS="$SAV_CFLAGS"
 
-dnl ---- [ Is function __attribute__((error(msg)) supported? ] ----
+dnl ---- [ Is function __attribute__((error(msg))) supported? ] ----
 AC_MSG_CHECKING([for function __attribute__((error(msg))) support])
 AC_COMPILE_IFELSE(
   [
@@ -795,7 +795,9 @@ AC_COMPILE_IFELSE(
       [[
         #include <stdio.h>
 
-        static int func(int i) __attribute__((error("deliberate error")))
+        static int func(int i) __attribute__((error("deliberate-error")));
+
+        static int func(int i)
 	{
 	  return i * 2;
 	}
@@ -815,6 +817,38 @@ AC_COMPILE_IFELSE(
     AC_MSG_RESULT([yes])
     AC_DEFINE([_HAVE_FUNCTION_ATTRIBUTE_ERROR_], [ 1 ], [Define to 1 if function __attribute__((error(msg))) supported])
   ])
+
+SAV_CFLAGS="$CFLAGS"
+CFLAGS="$SAV_CFLAGS -Werror=attributes"
+dnl ---- [ Is function __attribute__((warn_unused_result)) supported? ] ----
+AC_MSG_CHECKING([for function __attribute__((warn_unused_result)) support])
+AC_COMPILE_IFELSE(
+  [
+    AC_LANG_PROGRAM(
+      [[
+        #include <stdio.h>
+
+        static int func(int i) __attribute__((warn_unused_result));
+
+        static int func(int i)
+	{
+	  return i * 2;
+	}
+      ]],
+      [[
+	func(2);
+	printf("%d", 2);
+      ]]
+    )
+  ],
+  [
+    AC_MSG_RESULT([yes])
+    AC_DEFINE([_HAVE_FUNCTION_WARN_UNUSED_RESULTS_], [ 1 ], [Define to 1 if function __attribute__((warn_unused_results)) supported])
+  ],
+  [
+    AC_MSG_RESULT([no])
+  ])
+CFLAGS="$SAV_CFLAGS"
 
 dnl ---- [ Do we want stricter configuration checking? ] ----
 STRICT_CONFIG=No

keepalived/bfd/bfd_data.c
@@ -42,7 +42,7 @@ char *bfd_buffer;
  *	bfd_t functions
  */
 /* Initialize bfd_t */
-bool
+bfd_t *
 alloc_bfd(const char *name)
 {
 	bfd_t *bfd;
@@ -87,9 +87,7 @@ alloc_bfd(const char *name)
 	bfd->sands_exp = TIMER_NEVER;
 	bfd->sands_rst = TIMER_NEVER;
 
-	list_add_tail(&bfd->e_list, &bfd_data->bfd);
-
-	return true;
+	return bfd;
 }
 
 void

keepalived/bfd/bfd_parser.c
@@ -53,20 +53,19 @@ static unsigned long specified_event_processes;
 /* Allow for English spelling */
 static const char * neighbor_str = "neighbor";
 
+static void *current_bfd;
+
+
 static void
 bfd_handler(const vector_t *strvec)
 {
-	char *name;
-
 	global_data->have_bfd_config = true;
 
 	/* If we are not the bfd process, we don't need any more information */
 	if (!strvec)
 		return;
 
-	name = vector_slot(strvec, 1);
-
-	if (!alloc_bfd(name)) {
+	if (!(current_bfd = alloc_bfd(vector_slot(strvec, 1)))) {
 		skip_block(true);
 		return;
 	}
@@ -77,14 +76,11 @@ bfd_handler(const vector_t *strvec)
 static void
 bfd_nbrip_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	sockaddr_t nbr_addr;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!strcmp(vector_slot(strvec, 1), "neighbour_ip"))
 		neighbor_str = "neighbour";
@@ -95,6 +91,7 @@ bfd_nbrip_handler(const vector_t *strvec)
 			    " malformed %s address %s, ignoring instance",
 			    bfd->iname, neighbor_str, strvec_slot(strvec, 1));
 		free_bfd(bfd);
+		current_bfd = NULL;
 		skip_block(false);
 		return;
 	}
@@ -106,14 +103,11 @@ bfd_nbrip_handler(const vector_t *strvec)
 static void
 bfd_srcip_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	sockaddr_t src_addr;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (inet_stosockaddr(strvec_slot(strvec, 1), NULL, &src_addr)) {
 		report_config_error(CONFIG_GENERAL_ERROR,
@@ -129,14 +123,11 @@ bfd_srcip_handler(const vector_t *strvec)
 static void
 bfd_minrx_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	unsigned value;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!read_decimal_unsigned_strvec(strvec, 1, &value, BFD_MINRX_MIN * 1000, BFD_MINRX_MAX * 1000, 3, false)) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -157,14 +148,11 @@ bfd_minrx_handler(const vector_t *strvec)
 static void
 bfd_mintx_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	unsigned value;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!read_decimal_unsigned_strvec(strvec, 1, &value, BFD_MINTX_MIN * 1000, BFD_MINTX_MAX * 1000, 3, false)) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -185,14 +173,11 @@ bfd_mintx_handler(const vector_t *strvec)
 static void
 bfd_idletx_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	unsigned value;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!read_decimal_unsigned_strvec(strvec, 1, &value, BFD_IDLETX_MIN * 1000, BFD_IDLETX_MAX * 1000, 3, false)) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -213,14 +198,11 @@ bfd_idletx_handler(const vector_t *strvec)
 static void
 bfd_multiplier_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	unsigned value;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!read_unsigned_strvec(strvec, 1, &value, BFD_MULTIPLIER_MIN, BFD_MULTIPLIER_MAX, false))
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -234,12 +216,9 @@ bfd_multiplier_handler(const vector_t *strvec)
 static void
 bfd_passive_handler(__attribute__((unused)) const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	bfd->passive = true;
 }
@@ -247,14 +226,11 @@ bfd_passive_handler(__attribute__((unused)) const vector_t *strvec)
 static void
 bfd_ttl_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	unsigned value;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!read_unsigned_strvec(strvec, 1, &value, 1, BFD_TTL_MAX, false))
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -268,14 +244,11 @@ bfd_ttl_handler(const vector_t *strvec)
 static void
 bfd_maxhops_handler(const vector_t *strvec)
 {
-	bfd_t *bfd;
+	bfd_t *bfd = current_bfd;
 	int value;
 
 	assert(strvec);
 	assert(bfd_data);
-
-	bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-	assert(bfd);
 
 	if (!read_int_strvec(strvec, 1, &value, -1, BFD_TTL_MAX, false))
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -291,10 +264,14 @@ bfd_maxhops_handler(const vector_t *strvec)
 static void
 bfd_vrrp_end_handler(void)
 {
-	vrrp_tracked_bfd_t *tbfd = list_last_entry(&vrrp_data->vrrp_track_bfds, vrrp_tracked_bfd_t, e_list);
+	vrrp_tracked_bfd_t *tbfd = current_bfd;
 
-	if (specified_event_processes && !__test_bit(DAEMON_VRRP, &specified_event_processes))
+	if (specified_event_processes && !__test_bit(DAEMON_VRRP, &specified_event_processes)) {
 		free_vrrp_tracked_bfd(tbfd);
+		return;
+	}
+
+	list_add_tail(&tbfd->e_list, &vrrp_data->vrrp_track_bfds);
 }
 #endif
 
@@ -302,19 +279,21 @@ bfd_vrrp_end_handler(void)
 static void
 bfd_checker_end_handler(void)
 {
-	checker_tracked_bfd_t *cbfd = list_last_entry(&check_data->track_bfds, checker_tracked_bfd_t, e_list);
+	checker_tracked_bfd_t *cbfd = current_bfd;
 
-	if (specified_event_processes && !__test_bit(DAEMON_CHECKERS, &specified_event_processes))
+	if (specified_event_processes && !__test_bit(DAEMON_CHECKERS, &specified_event_processes)) {
 		free_checker_bfd(cbfd);
+		return;
+	}
+
+	list_add_tail(&cbfd->e_list, &check_data->track_bfds);
 }
 #endif
 
 static void
 bfd_end_handler(void)
 {
-	bfd_t *bfd = list_last_entry(&bfd_data->bfd, bfd_t, e_list);
-
-	assert(bfd);
+	bfd_t *bfd = current_bfd;
 
 	if (!bfd->nbr_addr.ss_family) {
 		report_config_error(CONFIG_GENERAL_ERROR,
@@ -377,6 +356,8 @@ bfd_end_handler(void)
 	bfd_checker_end_handler();
 #endif
 #endif
+
+	list_add_tail(&bfd->e_list, &bfd_data->bfd);
 }
 
 #ifdef _WITH_VRRP_
@@ -384,13 +365,10 @@ bfd_end_handler(void)
 static void
 bfd_vrrp_handler(const vector_t *strvec)
 {
-	const char *name;
-
 	if (!strvec)
 		return;
 
-	name = strvec_slot(strvec, 1);
-	alloc_vrrp_tracked_bfd(name, &vrrp_data->vrrp_track_bfds);
+	current_bfd = alloc_vrrp_tracked_bfd(strvec_slot(strvec, 1), &vrrp_data->vrrp_track_bfds);
 
 	specified_event_processes = 0;
 }
@@ -399,14 +377,11 @@ bfd_vrrp_handler(const vector_t *strvec)
 static void
 bfd_vrrp_weight_handler(const vector_t *strvec)
 {
-	vrrp_tracked_bfd_t *tbfd;
+	vrrp_tracked_bfd_t *tbfd = current_bfd;
 	int value;
 
 	assert(strvec);
 	assert(vrrp_data);
-
-	tbfd = list_last_entry(&vrrp_data->vrrp_track_bfds, vrrp_tracked_bfd_t, e_list);
-	assert(tbfd);
 
 	if (!read_int_strvec(strvec, 1, &value, -253, 253, true)) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Configuration error: BFD instance %s"
@@ -457,7 +432,8 @@ bfd_checker_handler(const vector_t *strvec)
 	INIT_LIST_HEAD(&cbfd->e_list);
 	INIT_LIST_HEAD(&cbfd->tracking_rs);
 	cbfd->bname = STRDUP(name);
-	list_add_tail(&cbfd->e_list, &check_data->track_bfds);
+
+	current_bfd = cbfd;
 
 	specified_event_processes = 0;
 }
@@ -494,21 +470,21 @@ init_bfd_keywords(bool active)
 	if (prog_type == PROG_TYPE_BFD || !active)
 #endif
 	{
-		install_keyword_root("bfd_instance", &bfd_handler, active);
-		install_sublevel_end_handler(bfd_end_handler);
+		install_keyword_root("bfd_instance", &bfd_handler, active, VPP &current_bfd);
+		install_level_end_handler(bfd_end_handler);
 		bfd_handlers = true;
 	}
 #ifndef _ONE_PROCESS_DEBUG_
 #ifdef _WITH_VRRP_
 	else if (prog_type == PROG_TYPE_VRRP) {
-		install_keyword_root("bfd_instance", &bfd_vrrp_handler, active);
-		install_sublevel_end_handler(bfd_vrrp_end_handler);
+		install_keyword_root("bfd_instance", &bfd_vrrp_handler, active, VPP &current_bfd);
+		install_level_end_handler(bfd_vrrp_end_handler);
 	}
 #endif
 #ifdef _WITH_LVS_
 	else if (prog_type == PROG_TYPE_CHECKER) {
-		install_keyword_root("bfd_instance", &bfd_checker_handler, active);
-		install_sublevel_end_handler(bfd_checker_end_handler);
+		install_keyword_root("bfd_instance", &bfd_checker_handler, active, VPP &current_bfd);
+		install_level_end_handler(bfd_checker_end_handler);
 	}
 #endif
 #endif

keepalived/check/check_api.c
@@ -52,12 +52,15 @@
 #include "bfd_daemon.h"
 #endif
 #include "track_file.h"
+#include "check_parser.h"
+
 
 /* Global vars */
 list_head_t checkers_queue;
 #ifdef _CHECKER_DEBUG_
 bool do_checker_debug;
 #endif
+checker_t *current_checker;
 
 /* free checker data */
 void
@@ -133,20 +136,18 @@ dump_connection_opts(FILE *fp, const void *data)
 }
 
 /* Queue a checker into the checkers_queue */
-checker_t *
+void
 queue_checker(const checker_funcs_t *funcs
 	      , thread_func_t launch
 	      , void *data
 	      , conn_opts_t *co
 	      , bool fd_required)
 {
-	virtual_server_t *vs = list_last_entry(&check_data->vs, virtual_server_t, e_list);
-	real_server_t *rs = list_last_entry(&vs->rs, real_server_t, e_list);
 	checker_t *checker;
 
 	/* Set default dst = RS, timeout = default */
 	if (co) {
-		co->dst = rs->addr;
+		co->dst = current_rs->addr;
 		co->connection_to = UINT_MAX;
 	}
 
@@ -154,8 +155,8 @@ queue_checker(const checker_funcs_t *funcs
 	INIT_LIST_HEAD(&checker->e_list);
 	checker->checker_funcs = funcs;
 	checker->launch = launch;
-	checker->vs = vs;
-	checker->rs = rs;
+	checker->vs = current_vs;
+	checker->rs = current_rs;
 	checker->data = data;
 	checker->co = co;
 	checker->enabled = true;
@@ -169,24 +170,22 @@ queue_checker(const checker_funcs_t *funcs
 	checker->default_delay_before_retry = 1 * TIMER_HZ;
 	checker->default_retry = 1 ;
 
-	/* queue the checker */
-	list_add_tail(&checker->e_list, &checkers_queue);
-
 	if (fd_required)
 		check_data->num_checker_fd_required++;
 
-	return checker;
+	current_checker = checker;
 }
 
 void
 dequeue_new_checker(void)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+// TODO - queue checker at end, not at start
+	if (!current_checker->is_up)
+		set_checker_state(current_checker, true);
 
-	if (!checker->is_up)
-		set_checker_state(checker, true);
+	free_checker(current_checker);
 
-	free_checker(checker);
+	current_checker = NULL;
 }
 
 bool
@@ -247,7 +246,7 @@ checker_set_dst_port(sockaddr_t *dst, uint16_t port)
 static void
 co_ip_handler(const vector_t *strvec)
 {
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
 
 	if (inet_stosockaddr(strvec_slot(strvec, 1), NULL, &co->dst))
 		report_config_error(CONFIG_GENERAL_ERROR, "Invalid connect_ip address %s - ignoring", strvec_slot(strvec, 1));
@@ -262,7 +261,7 @@ co_ip_handler(const vector_t *strvec)
 static void
 co_port_handler(const vector_t *strvec)
 {
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
 	unsigned port;
 
 	if (!read_unsigned_strvec(strvec, 1, &port, 1, 65535, true)) {
@@ -277,7 +276,8 @@ co_port_handler(const vector_t *strvec)
 static void
 co_srcip_handler(const vector_t *strvec)
 {
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
+
 	if (inet_stosockaddr(strvec_slot(strvec, 1), NULL, &co->bindto))
 		report_config_error(CONFIG_GENERAL_ERROR, "Invalid bindto address %s - ignoring", strvec_slot(strvec, 1));
 	else if (co->dst.ss_family != AF_UNSPEC &&
@@ -291,7 +291,7 @@ co_srcip_handler(const vector_t *strvec)
 static void
 co_srcport_handler(const vector_t *strvec)
 {
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
 	unsigned port;
 
 	if (!read_unsigned_strvec(strvec, 1, &port, 1, 65535, true)) {
@@ -307,7 +307,7 @@ static void
 co_srcif_handler(const vector_t *strvec)
 {
 	// This is needed for link local IPv6 bindto address
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
 
 	if (strlen(strvec_slot(strvec, 1)) > sizeof(co->bind_if) - 1) {
 		report_config_error(CONFIG_GENERAL_ERROR, "Interface name %s is too long - ignoring", strvec_slot(strvec, 1));
@@ -320,7 +320,7 @@ co_srcif_handler(const vector_t *strvec)
 static void
 co_timeout_handler(const vector_t *strvec)
 {
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
 	unsigned long timer;
 
 	if (!read_timer(strvec, 1, &timer, 1, UINT_MAX, true)) {
@@ -335,7 +335,7 @@ co_timeout_handler(const vector_t *strvec)
 static void
 co_fwmark_handler(const vector_t *strvec)
 {
-	conn_opts_t *co = CHECKER_GET_CO();
+	conn_opts_t *co = current_checker->co;
 	unsigned fwmark;
 
 	if (!read_unsigned_strvec(strvec, 1, &fwmark, 0, UINT_MAX, true)) {
@@ -349,7 +349,7 @@ co_fwmark_handler(const vector_t *strvec)
 static void
 retry_handler(const vector_t *strvec)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+	checker_t *checker = current_checker;
 	unsigned retry;
 
 	if (!read_unsigned_strvec(strvec, 1, &retry, 0, UINT_MAX, true)) {
@@ -363,7 +363,7 @@ retry_handler(const vector_t *strvec)
 static void
 delay_before_retry_handler(const vector_t *strvec)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+	checker_t *checker = current_checker;
 	unsigned long delay;
 
 	if (!read_timer(strvec, 1, &delay, 0, 0, true)) {
@@ -378,7 +378,7 @@ delay_before_retry_handler(const vector_t *strvec)
 static void
 warmup_handler(const vector_t *strvec)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+	checker_t *checker = current_checker;
 	unsigned long warmup;
 
 	if (!read_timer(strvec, 1, &warmup, 0, 0, true)) {
@@ -392,7 +392,7 @@ warmup_handler(const vector_t *strvec)
 static void
 delay_handler(const vector_t *strvec)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+	checker_t *checker = current_checker;
 	unsigned long delay_loop;
 
 	if (!read_timer(strvec, 1, &delay_loop, 1, 0, true)) {
@@ -406,7 +406,7 @@ delay_handler(const vector_t *strvec)
 static void
 alpha_handler(const vector_t *strvec)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+	checker_t *checker = current_checker;
 	int res = true;
 
 	if (vector_size(strvec) >= 2) {
@@ -421,7 +421,7 @@ alpha_handler(const vector_t *strvec)
 static void
 log_all_failures_handler(const vector_t *strvec)
 {
-	checker_t *checker = CHECKER_GET_CURRENT();
+	checker_t *checker = current_checker;
 	int res = true;
 
 	if (vector_size(strvec) >= 2) {

commit 7a983354fe866cdd8237c66e84d281973796c7a4

vrrp: fix ipv6 vrrp in fault state because no ipv4 address
Setting an IPv6 VRRP virtual address on an interface that has no IPv4
address results in a persistent FAULT state:

> Mar 21 11:23:25 ubuntu2204 Keepalived_vrrp[95135]: (vrrp) entering FAULT state (no IPv4 address for interface)

Virtual addresses are not correctly parsed in alloc_vrrp_vip().
vrrp_complete_instance() considers that the IPv4 address family is
set by default if no virtual addresses are detected.

>         /* Default to IPv4. This can only happen if no VIPs are specified. */
>         if (vrrp->family == AF_UNSPEC)
>                 vrrp->family = AF_INET;

Fix the virtual address parsing.

Fixes: 516032ec39 ("config parser: Fix segfault caused by extra '}' and other parser fixes")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

keepalived/vrrp/vrrp_data.c
@@ -1115,16 +1115,14 @@ alloc_vrrp_vip(const vector_t *strvec)
 	if (!(new_ipaddr = alloc_ipaddress(strvec, false)))
 		return;
 
-	if (last_ipaddr) {
-		address_family = IP_FAMILY(new_ipaddr);
+	address_family = IP_FAMILY(new_ipaddr);
 
-		if (current_vrrp->family == AF_UNSPEC)
-			current_vrrp->family = address_family;
-		else if (address_family != current_vrrp->family) {
-			report_config_error(CONFIG_GENERAL_ERROR, "(%s): address family must match VRRP instance [%s] - ignoring", current_vrrp->iname, strvec_slot(strvec, 0));
-			free_ipaddress(new_ipaddr);
-			return;
-		}
+	if (current_vrrp->family == AF_UNSPEC)
+		current_vrrp->family = address_family;
+	else if (address_family != current_vrrp->family) {
+		report_config_error(CONFIG_GENERAL_ERROR, "(%s): address family must match VRRP instance [%s] - ignoring", current_vrrp->iname, strvec_slot(strvec, 0));
+		free_ipaddress(new_ipaddr);
+		return;
 	}
 
 	list_add_tail(&new_ipaddr->e_list, &current_vrrp->vip);