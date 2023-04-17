acassen/keepalived

commit 74b203b70ce11e7b36dbb88cbccd5405106de0cb

vrrp: restore the vmac ipv6 link-local after flapping
The user is not supposed to shutdown a vmac interface created by
keepalived. However, it can mistakenly happen. When the link is
re-established, the link-local has disappear (the kernel removes all
IPv6 addresses on link down except if keep_addr_on_down sysctl is on)
and sending VRRP packet is no nore possible.

Restore the IPv6 Link-Local after a VMAC interface flapping. A
Link-Local is not set when the VRRP packets are sent from the base
interface (vmac-xmit-base).

Note that the IPv6 Virtual Addresses are also removed on link down which
is the desired behavior. Enabling keep_addr_on_down sysctl would keep
the link-local without this patch but would break this behavior.

Original patch signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/core/keepalived_netlink.c
@@ -1176,6 +1176,10 @@ netlink_if_address_filter(__attribute__((unused)) struct sockaddr_nl *snl, struc
 							 vrrp->ifp) &&
 						 vrrp->family == ifa->ifa_family &&
 						 vrrp->saddr.ss_family != AF_UNSPEC &&
+						 (vrrp->family != AF_INET6 ||	/* For an IPv6 VMAC if down removes the link local address */
+						  !__test_bit(VRRP_VMAC_BIT, &vrrp->flags) ||
+						  __test_bit(VRRP_VMAC_XMITBASE_BIT, &vrrp->flags) ||
+						  IF_ISUP(ifp)) &&
 						 (!__test_bit(VRRP_FLAG_SADDR_FROM_CONFIG, &vrrp->flags) || is_tracking_saddr)) {
 						down_instance(vrrp);
 						vrrp->saddr.ss_family = AF_UNSPEC;
@@ -1590,12 +1594,31 @@ static void
 process_interface_flags_change(interface_t *ifp, unsigned ifi_flags)
 {
 	bool now_up = FLAGS_UP(ifi_flags);
+#ifdef _HAVE_VRRP_VMAC_
+	tracking_obj_t *top;
+	vrrp_t *vrrp;
+#endif
 
 	ifp->ifi_flags = ifi_flags;
 
 	if (!list_empty(&ifp->tracking_vrrp)) {
 		log_message(LOG_INFO, "Netlink reports %s %s", ifp->ifname, now_up ? "up" : "down");
 
+#ifdef _HAVE_VRRP_VMAC_
+		if (ifp->vmac_type &&
+		    ifp->is_ours &&
+		    ifp->hw_addr[sizeof(ll_addr) - 2] == 0x02 &&   /* Is it an IPv6 vmac? */
+		    now_up &&
+		    IN6_IS_ADDR_UNSPECIFIED(&ifp->sin6_addr)) {
+			list_for_each_entry(top, &ifp->tracking_vrrp, e_list) {
+				vrrp = top->obj.vrrp;
+				if (!__test_bit(VRRP_VMAC_XMITBASE_BIT, &vrrp->flags)) {
+					set_link_local_address(vrrp);
+					break;
+				}
+			}
+		}
+#endif
 		process_if_status_change(ifp);
 	}
 

commit 45d0e285fc8d47eb30b8827d0ceea9ecc33f60a3

vrrp: fix compilation error when VRRP_VMAC is disabled if VRRP_VMAC is not enabled, VRRP_VMAC_BIT AND VRRP_VMAC_XMITBASE_BIT are invalid. However, VRRP_VMAC_BIT AND VRRP_VMAC_XMITBASE_BIT are used without checking _HAVE_VRRP_VMAC_ in netlink_if_address_filter().
fixes: 74b203b ("vrrp:restore the vmac ipv6 link_local after flapping")

keepalived/core/keepalived_netlink.c
@@ -1176,10 +1176,12 @@ netlink_if_address_filter(__attribute__((unused)) struct sockaddr_nl *snl, struc
 							 vrrp->ifp) &&
 						 vrrp->family == ifa->ifa_family &&
 						 vrrp->saddr.ss_family != AF_UNSPEC &&
+#ifdef _HAVE_VRRP_VMAC_
 						 (vrrp->family != AF_INET6 ||	/* For an IPv6 VMAC if down removes the link local address */
 						  !__test_bit(VRRP_VMAC_BIT, &vrrp->flags) ||
 						  __test_bit(VRRP_VMAC_XMITBASE_BIT, &vrrp->flags) ||
 						  IF_ISUP(ifp)) &&
+#endif
 						 (!__test_bit(VRRP_FLAG_SADDR_FROM_CONFIG, &vrrp->flags) || is_tracking_saddr)) {
 						down_instance(vrrp);
 						vrrp->saddr.ss_family = AF_UNSPEC;