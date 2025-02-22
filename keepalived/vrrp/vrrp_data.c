acassen/keepalived

commit cb95b7f68798945794738b93cfc4d2d5323fc4ae

vrrp: detect and reject duplicate unicast_peers in configuration
Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_data.c
@@ -1015,6 +1015,7 @@ alloc_vrrp_unicast_peer(const vector_t *strvec)
 	unicast_peer_t *peer;
 	unsigned ttl;
 	unsigned i;
+	unicast_peer_t *existing_peer;
 
 	/* Allocate new unicast peer */
 	PMALLOC(peer);
@@ -1054,6 +1055,16 @@ alloc_vrrp_unicast_peer(const vector_t *strvec)
 				break;
 			}
 			__set_bit(VRRP_FLAG_CHECK_UNICAST_SRC, &current_vrrp->flags);
+		}
+	}
+
+	/* Check this unicast peer is not already configured */
+	list_for_each_entry(existing_peer, &current_vrrp->unicast_peer, e_list) {
+		if ((current_vrrp->family == AF_INET && peer->address.in.sin_addr.s_addr == existing_peer->address.in.sin_addr.s_addr) ||
+		    (current_vrrp->family == AF_INET6 && !memcmp(&peer->address.in6.sin6_addr, &existing_peer->address.in6.sin6_addr, sizeof(peer->address.in6.sin6_addr)))) {
+			report_config_error(CONFIG_GENERAL_ERROR, "(%s) %s - duplicate unicast_peer", current_vrrp->iname, strvec_slot(strvec, 0));
+			FREE(peer);
+			return;
 		}
 	}
 

commit 26c9c2a7df38f1864b8b559a3e2bd245372308c4

vrrp: fix compilation error caused by previous patch
Commit cb95b7f - "vrrp: detect and reject duplicate unicast_peers in
configuration" used sockaddr_t incorrectly.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_data.c
@@ -1060,8 +1060,11 @@ alloc_vrrp_unicast_peer(const vector_t *strvec)
 
 	/* Check this unicast peer is not already configured */
 	list_for_each_entry(existing_peer, &current_vrrp->unicast_peer, e_list) {
-		if ((current_vrrp->family == AF_INET && peer->address.in.sin_addr.s_addr == existing_peer->address.in.sin_addr.s_addr) ||
-		    (current_vrrp->family == AF_INET6 && !memcmp(&peer->address.in6.sin6_addr, &existing_peer->address.in6.sin6_addr, sizeof(peer->address.in6.sin6_addr)))) {
+		if ((current_vrrp->family == AF_INET && ((struct sockaddr_in *)&peer->address)->sin_addr.s_addr == ((struct sockaddr_in *)&existing_peer->address)->sin_addr.s_addr) ||
+		    (current_vrrp->family == AF_INET6 &&
+		     !memcmp(&((struct sockaddr_in6 *)&peer->address)->sin6_addr,
+			     &((struct sockaddr_in6 *)&existing_peer->address)->sin6_addr,
+			     sizeof(((struct sockaddr_in6 *)&peer->address)->sin6_addr)))) {
 			report_config_error(CONFIG_GENERAL_ERROR, "(%s) %s - duplicate unicast_peer", current_vrrp->iname, strvec_slot(strvec, 0));
 			FREE(peer);
 			return;