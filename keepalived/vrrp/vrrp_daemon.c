acassen/keepalived

commit 58483b22c890e904c16653b68aa88796c940fa7e

vrrp: Handle a reload before vrrp_delayed_start has expired
If keepalived reloaded its configuration before a specified
vrrp_startup_delay had expired, the startup_delay was never being
timed out, and so all received adverts would be discarded.

The commit caused the startup_delay timer to be reinstated after a
reload if the timer has not yet expired.

Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

keepalived/vrrp/vrrp_daemon.c
@@ -608,10 +608,24 @@ start_vrrp(data_t *prev_global_data)
 		/* Init & start the VRRP packet dispatcher */
 		thread_add_event(master, vrrp_dispatcher_init, NULL, 0);
 
-		if (!reload && global_data->vrrp_startup_delay) {
-			vrrp_delayed_start_time = timer_add_long(time_now, global_data->vrrp_startup_delay);
-			thread_add_timer(master, delayed_start_clear_thread, NULL, global_data->vrrp_startup_delay);
-			log_message(LOG_INFO, "Delaying startup for %g seconds", global_data->vrrp_startup_delay / TIMER_HZ_DOUBLE);
+		if (global_data->vrrp_startup_delay) {
+			if (!reload) {
+				vrrp_delayed_start_time = timer_add_long(time_now, global_data->vrrp_startup_delay);
+				thread_add_timer(master, delayed_start_clear_thread, NULL, global_data->vrrp_startup_delay);
+				log_message(LOG_INFO, "Delaying startup for %g seconds", global_data->vrrp_startup_delay / TIMER_HZ_DOUBLE);
+			} else if (vrrp_delayed_start_time.tv_sec) {
+				if (time_now.tv_sec > vrrp_delayed_start_time.tv_sec ||
+				    (time_now.tv_sec == vrrp_delayed_start_time.tv_sec &&
+				     time_now.tv_usec >= vrrp_delayed_start_time.tv_usec))
+					vrrp_delayed_start_time.tv_sec = 0;
+				else {
+					unsigned delay_left;
+					delay_left = (vrrp_delayed_start_time.tv_sec - time_now.tv_sec) * 1000000UL
+							+ vrrp_delayed_start_time.tv_usec - time_now.tv_usec;
+					thread_add_timer(master, delayed_start_clear_thread, NULL, delay_left);
+					log_message(LOG_INFO, "Delaying startup for a further %g seconds", delay_left / TIMER_HZ_DOUBLE);
+				}
+			}
 		}
 
 		if (!reload && global_data->disable_local_igmp)
@@ -979,6 +993,7 @@ register_vrrp_thread_addresses(void)
 	register_thread_address("start_vrrp_termination_thread", start_vrrp_termination_thread);
 	register_thread_address("send_reload_advert_thread", send_reload_advert_thread);
 #endif
+	register_thread_address("delayed_start_clear_thread", delayed_start_clear_thread);
 	register_thread_address("vrrp_shutdown_backstop_thread", vrrp_shutdown_backstop_thread);
 	register_thread_address("vrrp_shutdown_timer_thread", vrrp_shutdown_timer_thread);
 
@@ -1181,6 +1196,5 @@ register_vrrp_parent_addresses(void)
 	register_thread_address("vrrp_respawn_thread", vrrp_respawn_thread);
 	register_thread_address("delayed_restart_vrrp_child_thread", delayed_restart_vrrp_child_thread);
 #endif
-	register_thread_address("delayed_start_clear_thread", delayed_start_clear_thread);
 }
 #endif

commit 1056f7472fa55d39d3b63c36945b8244702b2a1b

vrrp: handle a reload with no more startup_delay
During the vrrp_startup_delay time, if keepalived is reloaded with
no more startup_delay, the startup_delay is never timed out and all
received adverts is discarded.

The commit causes the startup_delay timer to be reinstated after a
reload with no more startup_delay if the timer has not yet expired.

Fixes: 58483b22c8 ("vrrp: Handle a reload before vrrp_delayed_start has expired")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

keepalived/vrrp/vrrp_daemon.c
@@ -609,23 +609,24 @@ start_vrrp(data_t *prev_global_data)
 		/* Init & start the VRRP packet dispatcher */
 		thread_add_event(master, vrrp_dispatcher_init, NULL, 0);
 
-		if (global_data->vrrp_startup_delay) {
-			if (!reload) {
+		if (!reload) {
+			if (global_data->vrrp_startup_delay) {
 				vrrp_delayed_start_time = timer_add_long(time_now, global_data->vrrp_startup_delay);
 				thread_add_timer(master, delayed_start_clear_thread, NULL, global_data->vrrp_startup_delay);
 				log_message(LOG_INFO, "Delaying startup for %g seconds", global_data->vrrp_startup_delay / TIMER_HZ_DOUBLE);
-			} else if (vrrp_delayed_start_time.tv_sec) {
-				if (time_now.tv_sec > vrrp_delayed_start_time.tv_sec ||
-				    (time_now.tv_sec == vrrp_delayed_start_time.tv_sec &&
-				     time_now.tv_usec >= vrrp_delayed_start_time.tv_usec))
-					vrrp_delayed_start_time.tv_sec = 0;
-				else {
-					unsigned delay_left;
-					delay_left = (vrrp_delayed_start_time.tv_sec - time_now.tv_sec) * 1000000UL
-							+ vrrp_delayed_start_time.tv_usec - time_now.tv_usec;
-					thread_add_timer(master, delayed_start_clear_thread, NULL, delay_left);
-					log_message(LOG_INFO, "Delaying startup for a further %g seconds", delay_left / TIMER_HZ_DOUBLE);
-				}
+			} else
+				vrrp_delayed_start_time.tv_sec = 0;
+		} else if (vrrp_delayed_start_time.tv_sec) {
+			if (time_now.tv_sec > vrrp_delayed_start_time.tv_sec ||
+				(time_now.tv_sec == vrrp_delayed_start_time.tv_sec &&
+				 time_now.tv_usec >= vrrp_delayed_start_time.tv_usec))
+				vrrp_delayed_start_time.tv_sec = 0;
+			else {
+				unsigned delay_left;
+				delay_left = (vrrp_delayed_start_time.tv_sec - time_now.tv_sec) * 1000000UL
+						+ vrrp_delayed_start_time.tv_usec - time_now.tv_usec;
+				thread_add_timer(master, delayed_start_clear_thread, NULL, delay_left);
+				log_message(LOG_INFO, "Delaying startup for a further %g seconds", delay_left / TIMER_HZ_DOUBLE);
 			}
 		}
 