14cb56d084ad93b9f3cbdfa387f3d580bbba86a0
Quentin Armitage <quentin@armitage.org.uk>
2026-08-13 21:22:44 +0100

    vrrp: Tidy up "Make JSON create atomic" patch
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>

--- keepalived/vrrp/vrrp_json.c
+++ keepalived/vrrp/vrrp_json.c
@@ -368,7 +368,6 @@ vrrp_print_json(void)
 	FILE *fp;
 	const char *filename;
 	char file_tmp_name[PATH_MAX];
-	int sav_errno;
 
 	if (list_empty(&vrrp_data->vrrp))
 		return;
@@ -377,34 +376,24 @@ vrrp_print_json(void)
 
 	/* write to a temporary file and rename it afterwards to avoid dealing with filesystem locks */
 	strcpy_safe(file_tmp_name, filename);
-	if (strlen(filename) + 6 < sizeof(file_tmp_name))
+	if (strlen(file_tmp_name) + 6 < sizeof(file_tmp_name))
 		strcat(file_tmp_name, "XXXXXX");
 	else
 		strcpy(file_tmp_name + sizeof(file_tmp_name) - 6 - 1, "XXXXXX");
 
 	fd = mkostemp(file_tmp_name, O_CLOEXEC);
-	if (fd == -1) {
-		sav_errno = errno;
+	if (fd == -1)
 		log_message(LOG_INFO, "Unable to create temporary json file '%s' - errno %d (%m)", file_tmp_name, errno);
-		errno = sav_errno;
-		FREE_CONST(filename);
-		return;
-	} else
-		close(fd);
-
-	fp = fopen_safe(file_tmp_name, "ae");
-	if (fp) {
-		vrrp_json_dump(fp);
-		fclose(fp);
-		if (rename(file_tmp_name, filename)) {
-			sav_errno = errno;
-			log_message(LOG_INFO, "Failed to rename %s to %s - errno %d (%m)", file_tmp_name, filename, errno);
-			errno = sav_errno;
-			FREE_CONST(filename);
-			return;
-		}
-	} else
-		log_message(LOG_INFO, "Can't create %s (%d: %m)", filename, errno);
+	else {
+		fp = fdopen(fd, "w+e");
+		if (fp) {
+			vrrp_json_dump(fp);
+			fclose(fp);
+			if (rename(file_tmp_name, filename))
+				log_message(LOG_INFO, "Failed to rename %s to %s - errno %d (%m)", file_tmp_name, filename, errno);
+		} else
+			log_message(LOG_INFO, "Can't create %s (%d: %m)", filename, errno);
+	}
 
 	FREE_CONST(filename);
 }
